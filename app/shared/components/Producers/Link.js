890c82a908bec37f3307455e7f445860b3c3e106
Aaron Cox <aaron.cox@greymass.com>
2018-05-21 03:34:20 -0400

    Producer Links now have a warning modal

--- app/shared/components/Producers/Link.js
+++ app/shared/components/Producers/Link.js
@@ -1,9 +1,12 @@
 // @flow
+import React, { Component } from 'react';
+import { Button, Header, Modal } from 'semantic-ui-react';
+import { I18n } from 'react-i18next';
+
 const { shell } = require('electron');
 
-import React, { Component } from 'react';
-
 export default class ProducersLink extends Component<Props> {
+  state = { open: false }
 
   isSafeish = (url) => url.startsWith('http:') || url.startsWith('https:')
 
@@ -12,17 +15,69 @@ export default class ProducersLink extends Component<Props> {
     if (this.isSafeish(url)) {
       shell.openExternal(url);
     }
+    this.setState({
+      open: false
+    });
   }
 
+  openWarning() {
+    this.setState({ open: true });
+  }
+
+  close = () => this.setState({ open: false })
+
   render() {
-    let element = this.props.producer.url;
+    const {
+      open
+    } = this.state;
+    let link = this.props.producer.url;
     if (this.isSafeish(this.props.producer.url)) {
-      element = (
-        <a href="#" onClick={this.openLink.bind(this)}>
+      link = (
+        <a href="#" onClick={this.openWarning.bind(this)}>
           {this.props.producer.url}
         </a>
       );
     }
-    return element;
+    return (
+      <I18n ns="producers">
+        {
+          (t) => (
+            <div>
+              {link}
+              <Modal size="tiny" open={open} onClose={this.close}>
+                <Modal.Header>
+                  {t('producer_link_warning_title')}
+                </Modal.Header>
+                <Modal.Content>
+                  <p>
+                    {t('producer_link_warning_body_1')}
+                  </p>
+                  <Header>
+                    {t('producer_link_warning_body_2')}
+                  </Header>
+                  <p>
+                    {t('producer_link_warning_body_3')}
+                  </p>
+                </Modal.Content>
+                <Modal.Actions>
+                  <Button
+                    content={t('cancel')}
+                    onClick={this.close}
+                    negative
+                  />
+                  <Button
+                    positive
+                    onClick={this.openLink.bind(this)}
+                    icon="external"
+                    labelPosition="right"
+                    content={t('confirm')}
+                  />
+                </Modal.Actions>
+              </Modal>
+            </div>
+          )
+        }
+      </I18n>
+    );
   }
 }
