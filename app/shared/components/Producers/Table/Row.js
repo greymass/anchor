09295c9fcbc4707fa3dba48cc29f470f621849c9
Aaron Cox <aaron.cox@greymass.com>
2018-05-25 02:22:30 -0400

    Simplified producer status (most voters don't need super deep info)

--- app/shared/components/Producers/Table/Row.js
+++ app/shared/components/Producers/Table/Row.js
@@ -1,12 +1,45 @@
 // @flow
 import React, { Component } from 'react';
 import { I18n } from 'react-i18next';
-import { Button, Header, Label, Progress, Table } from 'semantic-ui-react';
+import { Button, Header, Label, Popup, Progress, Table } from 'semantic-ui-react';
+
+import TimeAgo from 'react-timeago';
 
 import ProducersLink from '../Link';
 import ProducersVoteWeight from '../Vote/Weight';
 
+const statusCodes = {
+  enabled: {
+    status: 'enabled',
+    color: 'green',
+    message: 'producer_production_enabled'
+  },
+  disabled: {
+    status: 'disabled',
+    color: 'yellow',
+    message: 'producer_production_disabled'
+  },
+  never: {
+    status: 'never',
+    color: 'grey',
+    message: 'producer_production_never'
+  }
+};
+
 export default class ProducersTableRow extends Component<Props> {
+  getProductionStatus = (lastProduced) => {
+    const timeAgo = Date.now() - lastProduced;
+    // Account for two rounds worth of time with a 5 second drift (131 seconds)
+    if (timeAgo < ((1000 * 21 * 6 * 0.5 * 2) + 5000)) {
+      return 'enabled';
+    // If the last produced is less than or equal to the epoch, it's never produced
+    } else if (lastProduced <= 946684800000) {
+      return 'never';
+    }
+    // Beyond 131 seconds - they aren't producing
+    return 'disabled';
+  }
+
   render() {
     const {
       addProducer,
@@ -22,7 +55,7 @@ export default class ProducersTableRow extends Component<Props> {
     const isActive = (Date.now() - lastProduced) < 1000;
     const isSelected = (selected.indexOf(producer.owner) !== -1);
     const votePercent = (totalVoteWeight)
-      ? ((parseInt(producer.total_votes, 10) / parseInt(totalVoteWeight, 10)) * 100).toFixed(0)
+      ? ((parseInt(producer.total_votes, 10) / parseInt(totalVoteWeight, 10)) * 100).toFixed(2)
       : 0;
     const voteFormatted = (producer.total_votes > 0)
       ? (
@@ -31,12 +64,12 @@ export default class ProducersTableRow extends Component<Props> {
         />
       )
       : 'None';
-
+    const producerStatusCode = this.getProductionStatus(lastProduced);
     return (
       <I18n ns="producers">
         {
           (t) => (
-            <Table.Row key={producer.owner} positive={isActive}>
+            <Table.Row positive={isActive}>
               <Table.Cell textAlign="center">
                 <Button
                   color={isSelected ? 'blue' : 'grey'}
@@ -71,24 +104,30 @@ export default class ProducersTableRow extends Component<Props> {
                 />
               </Table.Cell>
               <Table.Cell>
-                {(lastProduced <= epoch)
-                  ? (
+                <Popup
+                  content={
+                    (statusCodes[producerStatusCode].status === 'never')
+                    ? 'Never'
+                    : (
+                      <span>
+                        Last Block:
+                        {' '}
+                        <TimeAgo date={lastProduced} />
+                      </span>
+                    )
+                  }
+                  inverted
+                  position="top center"
+                  trigger={(
                     <Label
                       basic
-                      color="grey"
-                      content={t('producer_produced_never')}
+                      color={statusCodes[producerStatusCode].color}
+                      content={t(statusCodes[producerStatusCode].message)}
                       size="small"
                     />
-                  )
-                  : (
-                    <Label
-                      basic
-                      color="green"
-                      content={t('producer_produced_recently')}
-                      size="small"
-                    />
-                  )
-                }
+                  )}
+                />
+
               </Table.Cell>
             </Table.Row>
           )
