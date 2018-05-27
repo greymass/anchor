643f48402c35d9f1439d9edaad72be4c0d9c421b
Aaron Cox <aaron.cox@greymass.com>
2018-05-27 14:34:00 -0400

    confirm prompt autoFocus + submit on enter

--- app/shared/components/Wallet/Panel/Form/Modal/Confirm.js
+++ app/shared/components/Wallet/Panel/Form/Modal/Confirm.js
@@ -8,7 +8,17 @@ export default class WalletPanelFormModalConfirm extends Component<Props> {
     password: ''
   }
 
-  compare = (e, { value }) => this.setState({ password: value })
+  compare = (e, { value }) => this.setState({
+    matches: (value === this.props.password),
+    password: value
+  })
+
+  onKeyPress = (e) => {
+    const { matches } = this.state;
+    if (matches && e.key === 'Enter') {
+      this.props.onSubmit();
+    }
+  }
 
   render() {
     const {
@@ -42,11 +52,13 @@ export default class WalletPanelFormModalConfirm extends Component<Props> {
               <Modal.Content>
                 <h3>{t('wallet_panel_password_confirm_description')}</h3>
                 <Form.Field
+                  autoFocus
                   control={Input}
                   fluid
                   icon={matches ? 'checkmark' : 'x'}
                   label={t('wallet_panel_password_label')}
                   onChange={this.compare}
+                  onKeyPress={this.onKeyPress}
                   type="password"
                 />
               </Modal.Content>
