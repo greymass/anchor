bcb23ae2fdb7a81e795bb9b414c8502480885cb7
dafuga <daniel.fugere@ballistiq.com>
2018-05-28 20:23:06 -0700

    Made Staking Form easier to understand and use

--- app/shared/actions/stake.js
+++ app/shared/actions/stake.js
@@ -12,9 +12,14 @@ export function setStake(account, net_amount, cpu_amount) {
 export function setStakeWithValidation(balance, account, net_amount, cpu_amount) {
   return (dispatch: () => void) => {
     const nextStake = {
-      net_amount: net_amount,
-      cpu_amount: cpu_amount
+      net_amount: (net_amount + account.net_weight/10000),
+      cpu_amount: (cpu_amount + account.cpu_weight/10000)
     };
+
+    setTimeout(function(){
+      dispatch({type: types.VALIDATE_STAKE_NULL})
+    }, 15000);
+    
 
     if (dispatch(validate.validateStake(nextStake, balance))){
       return dispatch(delegatebw(account.account_name, account.account_name, net_amount, cpu_amount));
