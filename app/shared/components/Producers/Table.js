faf8b4da807c73753e0e3998c787eb7dab9f2c0c
Aaron Cox <aaron.cox@greymass.com>
2018-05-29 02:12:46 -0400

    reducing the amount of data from producers + sorting

--- app/shared/components/Producers/Table.js
+++ app/shared/components/Producers/Table.js
@@ -1,4 +1,5 @@
 // @flow
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { I18n } from 'react-i18next';
 
@@ -11,12 +12,33 @@ export default class ProducersTable extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
+      column: 'votes',
+      data: _.sortBy(props.producers.list, ['votes']).reverse(),
+      direction: 'descending',
       filter: false
     };
   }
 
   onSearchChange = (e, { value }) => {
     this.setState({ filter: value });
+  }
+
+  handleSort = clickedColumn => () => {
+    const { column, data, direction } = this.state;
+
+    if (column !== clickedColumn) {
+      this.setState({
+        column: clickedColumn,
+        data: _.sortBy(data, [clickedColumn]),
+        direction: 'ascending',
+      });
+      return;
+    }
+
+    this.setState({
+      data: data.reverse(),
+      direction: direction === 'ascending' ? 'descending' : 'ascending',
+    });
   }
 
   render() {
@@ -26,6 +48,9 @@ export default class ProducersTable extends Component<Props> {
       selected
     } = this.props;
     const {
+      column,
+      data,
+      direction,
       filter
     } = this.state;
     const {
@@ -64,28 +89,47 @@ export default class ProducersTable extends Component<Props> {
                   />
                 </Grid.Column>
               </Grid>
-              <Table color="violet" size="small" style={{ borderRadius: 0 }}>
+              <Table
+                color="violet"
+                size="small"
+                sortable
+                style={{ borderRadius: 0 }}
+              >
                 <Table.Header>
                   <Table.Row>
                     <Table.HeaderCell collapsing />
-                    <Table.HeaderCell textAlign="center" collapsing>
+                    <Table.HeaderCell
+                      collapsing
+                      onClick={this.handleSort('votes')}
+                      sorted={column === 'votes' ? direction : null}
+                      textAlign="center"
+                    >
                       {t('block_producer_position')}
                     </Table.HeaderCell>
-                    <Table.HeaderCell>
+                    <Table.HeaderCell
+                      onClick={this.handleSort('owner')}
+                      sorted={column === 'owner' ? direction : null}
+                    >
                       {t('block_producer')}
                     </Table.HeaderCell>
-                    <Table.HeaderCell>
+                    <Table.HeaderCell
+                      onClick={this.handleSort('votes')}
+                      sorted={column === 'votes' ? direction : null}
+                    >
                       {t('block_producer_total_votes')}
                     </Table.HeaderCell>
-                    <Table.HeaderCell collapsing>
+                    <Table.HeaderCell
+                      collapsing
+                      onClick={this.handleSort('last_produced_block_time')}
+                      sorted={column === 'last_produced_block_time' ? direction : null}
+                    >
                       {t('block_producer_last_production_status')}
                     </Table.HeaderCell>
                   </Table.Row>
                 </Table.Header>
                 <Table.Body>
-                  {producers
-                    .list.filter(producer => (filter ? producer.owner.includes(filter) : true))
-                    .sort((a, b) => parseInt(b.total_votes, 10) - parseInt(a.total_votes, 10))
+                  {data.filter(producer => (filter ? producer.owner.includes(filter) : true))
+                    // .sort((a, b) => parseInt(b.total_votes, 10) - parseInt(a.total_votes, 10))
                     .map((producer, idx) => (
                       <ProducersTableRow
                         addProducer={this.props.addProducer}
