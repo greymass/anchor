611953fe4194e7e2e99a24a1d16e82735fed63c7
Aaron Cox <aaron.cox@greymass.com>
2018-06-01 18:15:31 -0400

    design pass on the wallet area

--- app/shared/components/Wallet/Panel/Unlocked.js
+++ app/shared/components/Wallet/Panel/Unlocked.js
@@ -1,15 +1,26 @@
 // @flow
 import React, { Component } from 'react';
 import { I18n } from 'react-i18next';
-import { Button } from 'semantic-ui-react';
+import { Accordion, Button, Header, Menu, Segment } from 'semantic-ui-react';
 
 import WalletPanelFormNode from './Form/Node';
 import WalletPanelButtonLock from './Button/Lock';
 import WalletPanelButtonRemove from './Button/Remove';
 import WalletPanelButtonStake from './Button/Stake';
 
-export default class WalletPanelLocked extends Component<Props> {
+export default class WalletPanelUnlocked extends Component<Props> {
+  state = { activeIndex: 0 }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
   render() {
+    const { activeIndex } = this.state
     const {
       actions,
       accounts,
@@ -31,22 +42,53 @@ export default class WalletPanelLocked extends Component<Props> {
                   />
                 )
                 : (
-                  <Button.Group vertical>
+                  <Segment vertical>
                     <WalletPanelButtonLock
                       lockWallet={actions.lockWallet}
                     />
-                    <WalletPanelButtonRemove
-                      removeWallet={actions.removeWallet}
-                    />
-                    <WalletPanelButtonStake
-                      actions={actions}
-                      accounts={accounts}
-                      balances={balances}
-                      validate={validate}
-                      settings={settings}
-                      system={system}
-                    />
-                  </Button.Group>
+                    <Accordion
+                      as={Menu}
+                      fluid
+                      vertical
+                    >
+                      <Menu.Item>
+                        <Accordion.Title
+                          active={activeIndex === 0}
+                          content={t('wallet_actions')}
+                          index={0}
+                          onClick={this.handleClick}
+                        />
+                        <Accordion.Content active={activeIndex === 0}>
+                          <Segment basic>
+                            <WalletPanelButtonStake
+                              actions={actions}
+                              accounts={accounts}
+                              balances={balances}
+                              validate={validate}
+                              settings={settings}
+                              system={system}
+                            />
+                          </Segment>
+                        </Accordion.Content>
+                      </Menu.Item>
+
+                      <Menu.Item>
+                        <Accordion.Title
+                          active={activeIndex === 1}
+                          content={t('wallet_actions_dangerous')}
+                          index={1}
+                          onClick={this.handleClick}
+                        />
+                        <Accordion.Content active={activeIndex === 1}>
+                          <Segment basic>
+                            <WalletPanelButtonRemove
+                              removeWallet={actions.removeWallet}
+                            />
+                          </Segment>
+                        </Accordion.Content>
+                      </Menu.Item>
+                    </Accordion>
+                  </Segment>
                 )
               }
             </div>
