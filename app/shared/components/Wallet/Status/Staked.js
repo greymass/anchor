23d0d7586857d2bb3401d06185b910fb8aad2d67
Aaron Cox <aaron.cox@greymass.com>
2018-06-01 18:14:59 -0400

    tweaks to UI and component for staking

--- app/shared/components/Wallet/Status/Staked.js
+++ app/shared/components/Wallet/Status/Staked.js
@@ -5,59 +5,49 @@ import { I18n } from 'react-i18next';
 
 export default class WalletStatusStaked extends Component<Props> {
   render() {
-    const { account, settings, balance } = this.props;
-    const server = settings.node;
-
-    let element = `No account stake data loaded yet (connected to: ${server})...`;
-
-    if (account.account_name) {
-      const coinsStakedToNet = account.coins_staked_to_net;
-      const coinsStakedToCpu = account.coins_staked_to_cpu;
-      const totalStaked = coinsStakedToNet + coinsStakedToCpu;
-      const amountNotStaked = balance.EOS;
-
-      element = (
-        <I18n ns="stake">
-          {
-            (t) => (
-              <Segment vertical basic>
-                <Header size="small">
-                  {t('staked_balances')}
-                </Header>
-                <Table
-                  attached="bottom"
-                  definition
-                  unstackable
-                >
-                  <Table.Body>
-                    <Table.Row>
-                      <Table.Cell>{t('total_staked')}</Table.Cell>
-                      <Table.Cell>{totalStaked.toFixed(4)}</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                      <Table.Cell>{t('net_staked')}</Table.Cell>
-                      <Table.Cell>{coinsStakedToNet.toFixed(4)}</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                      <Table.Cell>{t('cpu_staked')}</Table.Cell>
-                      <Table.Cell>{coinsStakedToCpu.toFixed(4)}</Table.Cell>
-                    </Table.Row>
-                    <Table.Row>
-                      <Table.Cell>{t('amount_not_staked')}</Table.Cell>
-                      <Table.Cell>{amountNotStaked.toFixed(4)}</Table.Cell>
-                    </Table.Row>
-                  </Table.Body>
-                </Table>
-              </Segment>
-            )
-          }
-        </I18n>
-      );
-    }
+    const { accounts, balances, settings } = this.props;
+    const account = accounts[settings.account] || {};
+    const balance = balances[settings.account] || {};
+    const stakedToNet = parseFloat(account.coins_staked_to_net).toFixed(4);
+    const stakedToCpu = parseFloat(account.coins_staked_to_cpu).toFixed(4);
+    const totalStaked = parseFloat(stakedToNet + stakedToCpu).toFixed(4);
+    const amountNotStaked = parseFloat(balance.EOS).toFixed(4);
     return (
-      <Segment basic>
-        {element}
-      </Segment>
+      <I18n ns="stake">
+        {
+          (t) => (
+            <Segment vertical basic>
+              <Header size="small">
+                {t('staked_balances')}
+              </Header>
+              <Table
+                attached="bottom"
+                definition
+                unstackable
+              >
+                <Table.Body>
+                  <Table.Row>
+                    <Table.Cell width={5}>{t('total_staked')}</Table.Cell>
+                    <Table.Cell>{totalStaked} EOS</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>{t('net_staked')}</Table.Cell>
+                    <Table.Cell>{stakedToNet} EOS</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>{t('cpu_staked')}</Table.Cell>
+                    <Table.Cell>{stakedToCpu} EOS</Table.Cell>
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.Cell>{t('amount_not_staked')}</Table.Cell>
+                    <Table.Cell>{amountNotStaked} EOS</Table.Cell>
+                  </Table.Row>
+                </Table.Body>
+              </Table>
+            </Segment>
+          )
+        }
+      </I18n>
     );
   }
 }
