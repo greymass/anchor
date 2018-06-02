5873e94d0edaa17014cc260aac321519c8e03fff
Aaron Cox <aaron.cox@greymass.com>
2018-06-01 20:25:01 -0400

    confirmation on remove wallet

--- app/shared/components/Wallet/Panel/Button/Remove.js
+++ app/shared/components/Wallet/Panel/Button/Remove.js
@@ -1,21 +1,39 @@
 // @flow
 import React, { Component } from 'react';
 import { I18n } from 'react-i18next';
-import { Button } from 'semantic-ui-react';
+import { Button, Confirm } from 'semantic-ui-react';
 
 export default class WalletPanelButtonLogout extends Component<Props> {
+  state = { open: false }
+
+  open = () => this.setState({ open: true })
+  close = () => this.setState({ open: false })
+
+  confirmRemoveWallet = () => {
+    this.setState({ open: false }, () => {
+      this.props.removeWallet();
+    });
+  }
+
   render() {
     return (
       <I18n ns="wallet">
         {
           (t) => (
-            <Button
-              color="red"
-              content={t('wallet_panel_wallet_remove')}
-              fluid
-              icon="trash"
-              onClick={this.props.removeWallet}
-            />
+            <span>
+              <Button
+                color="red"
+                content={t('wallet_panel_wallet_remove')}
+                fluid
+                icon="trash"
+                onClick={this.open}
+              />
+              <Confirm
+                open={this.state.open}
+                onCancel={this.close}
+                onConfirm={this.confirmRemoveWallet}
+              />
+            </span>
           )
         }
       </I18n>
