3b0c838233f1f74c567a7a72aba817f7ce9b5067
Aaron Cox <aaron.cox@greymass.com>
2018-06-01 22:35:29 -0400

    Improved loading + success pages

--- app/shared/components/Wallet/Panel/Form/Stake/SuccessMessage.js
+++ app/shared/components/Wallet/Panel/Form/Stake/SuccessMessage.js
@@ -1,37 +1,58 @@
 // @flow
 import React, { Component } from 'react';
 import { I18n } from 'react-i18next';
-import { Message } from 'semantic-ui-react';
+import { Button, Header, Icon, Modal, Segment } from 'semantic-ui-react';
 
 export default class WalletPanelFormStakeSuccessMessage extends Component<Props> {
   render() {
     const {
-      system,
+      onClose,
+      system
     } = this.props;
-
     return (
       <I18n ns="stake">
         {
           (t) => (
             <div>
-              {(system.DELEGATEBW === 'SUCCESS' || system.UNDELEGATEBW === 'SUCCESS')
+              {((system.DELEGATEBW === 'SUCCESS') || (system.UNDELEGATEBW === 'SUCCESS'))
                 ? (
-                  <Message positive>
-                    {(system.DELEGATEBW === 'SUCCESS')
-                      ? (
-                        <p>{`${t('stake_success')} tx # ${system.DELEGATEBW_LAST_TRANSACTION.transaction_id}`}</p>
-                      )
-                      : ''
-                    }
-                    {(system.UNDELEGATEBW === 'SUCCESS')
-                      ? (
-                        <p>{`${t('unstake_success')} tx # ${system.UNDELEGATEBW_LAST_TRANSACTION.transaction_id}`}</p>
-                      )
-                      : ''
-                    }
-                  </Message>
-                )
-                : ''
+                  <div>
+                    <Modal.Content>
+                      {(system.DELEGATEBW === 'SUCCESS')
+                        ? (
+                          <div>
+                            <h3>
+                              {t('stake_success')}
+                            </h3>
+                            <Segment basic padded textAlign="center">
+                              <pre>{system.DELEGATEBW_LAST_TRANSACTION.transaction_id}</pre>
+                            </Segment>
+                          </div>
+                        )
+                        : ''
+                      }
+                      {(system.UNDELEGATEBW === 'SUCCESS')
+                        ? (
+                          <div>
+                            <h3>
+                              {t('unstake_success')}
+                            </h3>
+                            <Segment basic padded textAlign="center">
+                              <pre>{system.UNDELEGATEBW_LAST_TRANSACTION.transaction_id}</pre>
+                            </Segment>
+                          </div>
+                        )
+                        : ''
+                      }
+                    </Modal.Content>
+                    <Modal.Actions>
+                      <Button color="green" onClick={onClose}>
+                        <Icon name="checkmark" /> {t('close')}
+                      </Button>
+                    </Modal.Actions>
+                  </div>
+                ):
+                ''
               }
             </div>
           )
