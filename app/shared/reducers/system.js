5b228218273c1f4d65ef05d82a2c5b9079f7f9d1
Aaron Cox <aaron.cox@greymass.com>
2018-06-06 19:16:31 -0400

    better error handlers

--- app/shared/reducers/system.js
+++ app/shared/reducers/system.js
@@ -15,10 +15,29 @@ export default function system(state = {}, action) {
 
   const [, requestName, requestState] = matches;
 
-  return {
+  const errField = `${requestName}_LAST_ERROR`;
+  const txField = `${requestName}_LAST_TRANSACTION`;
+
+  const newState = {
     ...state,
     [requestName]: requestState,
-    [`${requestName}_LAST_ERROR`]: (action.payload) ? action.payload.err : '',
-    [`${requestName}_LAST_TRANSACTION`]: (action.payload) ? action.payload.tx : ''
+    errField: '',
+    txField: ''
   };
+
+  if (action.payload) {
+    // Attempt to process any errors returned
+    if (action.payload.err) {
+      try {
+        newState[errField] = JSON.parse(action.payload.err);
+      } catch (e) {
+        newState[errField] = action.payload.err;
+      }
+    }
+    // Attach any returned transactions
+    if (action.payload.tx) {
+      newState[txField] = action.payload.tx;
+    }
+  }
+  return newState;
 }
