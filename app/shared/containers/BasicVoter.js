cc1508cdcf954420a2486bdfeacb9f7930d79f5d
Aaron Cox <aaron.cox@greymass.com>
2018-06-13 19:24:11 -0400

    getCurrencyStats + maintaining it in state

--- app/shared/containers/BasicVoter.js
+++ app/shared/containers/BasicVoter.js
@@ -83,11 +83,13 @@ class BasicVoterContainer extends Component<Props> {
       validate
     } = this.props;
     const {
+      getCurrencyStats,
       getGlobals,
       getInfo,
       getAccount
     } = actions;
     if (validate.NODE === 'SUCCESS') {
+      getCurrencyStats();
       getGlobals();
       getInfo();
       if (validate.ACCOUNT === 'SUCCESS') {
