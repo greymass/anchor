80c3e7764d7abae4fa48882ac5339d68c09da0d5
Aaron Cox <aaron.cox@greymass.com>
2018-06-14 21:23:24 -0400

    removed connection form

--- app/shared/components/Sidebar/Connection.js
+++ app/shared/components/Sidebar/Connection.js
@@ -2,19 +2,14 @@
 import React, { Component } from 'react';
 import { Segment } from 'semantic-ui-react';
 
-import FormConnectionContainer from '../../containers/Form/Connection';
-
-const { shell } = require('electron');
-
 export default class SidebarConnection extends Component<Props> {
-  openLink = (url) => shell.openExternal(url);
   render() {
     return (
       <Segment
         size="small"
         stacked
       >
-        <FormConnectionContainer />
+        setup
       </Segment>
     );
   }
