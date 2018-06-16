294e8b0d074c3d74222674264b0d60426994f971
Aaron Cox <aaron@greymass.com>
2018-06-16 15:00:19 -0400

    Bit of formatting + wording + countdown timer

--- app/shared/components/Wallet/Status/Balances.js
+++ app/shared/components/Wallet/Status/Balances.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 import { Header, Segment, Table } from 'semantic-ui-react';
 import { forEach } from 'lodash';
+import TimeAgo from 'react-timeago';
 
 class WalletStatusBalances extends Component<Props> {
   render() {
@@ -21,8 +22,8 @@ class WalletStatusBalances extends Component<Props> {
     const totalBeingUnstaked = (parseFloat(refund_request.net_amount) + parseFloat(refund_request.cpu_amount));
     const tokens = (balances && balances[settings.account]) ? balances[settings.account] : { EOS: 0 };
     const totalTokens = totalStaked + totalBeingUnstaked + tokens.EOS;
-
-    const spanStyle = { margin: '1em' };
+    const refundDate = new Date(`${refund_request.request_time}z`);
+    refundDate.setHours(refundDate.getHours() + 72);
 
     const rows = [
       (
@@ -31,21 +32,26 @@ class WalletStatusBalances extends Component<Props> {
             EOS
           </Table.Cell>
           <Table.Cell width={10}>
-            <span style={spanStyle}>
-              {(tokens.EOS) ? tokens.EOS.toFixed(4) : '0.0000'} EOS {t('wallet_status_liquid')}
-            </span>
-            |
-            <span style={spanStyle}>
-              {totalStaked.toFixed(4)} EOS {t('wallet_status_resources_staked')}
-            </span>
-            |
-            <span style={spanStyle}>
-              {totalBeingUnstaked.toFixed(4)} EOS {t('wallet_status_resources_being_unstaked')}
-            </span>
-            |
-            <span style={spanStyle}>
-              {totalTokens.toFixed(4)} EOS {t('wallet_status_total_balance')}
-            </span>
+            <Table size="small">
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell width={4}>{t('wallet_status_liquid')}</Table.Cell>
+                  <Table.Cell>{(tokens.EOS) ? tokens.EOS.toFixed(4) : '0.0000'} EOS</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>{t('wallet_status_resources_staked')}</Table.Cell>
+                  <Table.Cell>{totalStaked.toFixed(4)} EOS </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>{t('wallet_status_resources_being_unstaked')} </Table.Cell>
+                  <Table.Cell>{totalBeingUnstaked.toFixed(4)} EOS (<TimeAgo date={refundDate} />)</Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>{t('wallet_status_total_balance')}</Table.Cell>
+                  <Table.Cell>{totalTokens.toFixed(4)} EOS</Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
           </Table.Cell>
         </Table.Row>
       )
