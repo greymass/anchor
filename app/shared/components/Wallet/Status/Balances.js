ccb2ed8e98a6c2248309d5a303c83dec1dedd795
dafuga <daniel.fugere@ballistiq.com>
2018-06-16 03:32:21 -0400

    Added amount being unstaked and total balance to dashboard

--- app/shared/components/Wallet/Status/Balances.js
+++ app/shared/components/Wallet/Status/Balances.js
@@ -14,20 +14,37 @@ class WalletStatusBalances extends Component<Props> {
     } = this.props;
     const account = accounts[settings.account] || {};
     const {
-      self_delegated_bandwidth
+      self_delegated_bandwidth,
+      refund_request
     } = account;
     const totalStaked = (parseFloat(self_delegated_bandwidth.cpu_weight) + parseFloat(self_delegated_bandwidth.net_weight));
+    const totalBeingUnstaked = (parseFloat(refund_request.net_amount) + parseFloat(refund_request.cpu_amount));
     const tokens = (balances && balances[settings.account]) ? balances[settings.account] : { EOS: 0 };
+    const totalTokens = totalStaked + totalBeingUnstaked + tokens.EOS;
+
+    const spanStyle = { margin: '1em' };
+
     const rows = [
       (
         <Table.Row key="EOS">
-          <Table.Cell width={5}>
+          <Table.Cell width={2}>
             EOS
           </Table.Cell>
-          <Table.Cell>
-            {(tokens.EOS) ? tokens.EOS.toFixed(4) : '0.0000'}
-            <span style={{ marginLeft: '0.25em' }}>
-              (+{totalStaked.toFixed(4)} EOS {t('wallet_status_resources_staked')})
+          <Table.Cell width={10}>
+            <span style={spanStyle}>
+              {(tokens.EOS) ? tokens.EOS.toFixed(4) : '0.0000'} EOS {t('wallet_status_liquid')}
+            </span>
+            |
+            <span style={spanStyle}>
+              {totalStaked.toFixed(4)} EOS {t('wallet_status_resources_staked')}
+            </span>
+            |
+            <span style={spanStyle}>
+              {totalBeingUnstaked.toFixed(4)} EOS {t('wallet_status_resources_being_unstaked')}
+            </span>
+            |
+            <span style={spanStyle}>
+              {totalTokens.toFixed(4)} EOS {t('wallet_status_total_balance')}
             </span>
           </Table.Cell>
         </Table.Row>
