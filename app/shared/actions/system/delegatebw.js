85fdb7333b0475561a375ecc062266700f616ce5
dafuga <daniel.fugere@ballistiq.com>
2018-06-21 01:51:42 -0400

    merging delegatebw and undelegatebw into one transaction

--- app/shared/actions/system/delegatebw.js
+++ app/shared/actions/system/delegatebw.js
@@ -12,17 +12,8 @@ export function delegatebw(delegator, receiver, netAmount, cpuAmount) {
       type: types.SYSTEM_DELEGATEBW_PENDING
     });
 
-    const stakeNetAmount = netAmount || 0;
-    const stakeCpuAmount = cpuAmount || 0;
-
     return eos(connection).transaction(tr => {
-      tr.delegatebw({
-        from: delegator,
-        receiver,
-        stake_net_quantity: `${stakeNetAmount.toFixed(4)} EOS`,
-        stake_cpu_quantity: `${stakeCpuAmount.toFixed(4)} EOS`,
-        transfer: 0
-      });
+      tr.delegatebw(delegatebwParams(delegator, receiver, netAmount, cpuAmount));
     }).then((tx) => {
       dispatch(AccountActions.getAccount(delegator));
       return dispatch({
@@ -33,6 +24,19 @@ export function delegatebw(delegator, receiver, netAmount, cpuAmount) {
       payload: { err },
       type: types.SYSTEM_DELEGATEBW_FAILURE
     }));
+  };
+}
+
+export function delegatebwParams(delegator, receiver, netAmount, cpuAmount) {
+  const stakeNetAmount = netAmount || 0;
+  const stakeCpuAmount = cpuAmount || 0;
+
+  return {
+    from: delegator,
+    receiver,
+    stake_net_quantity: `${stakeNetAmount.toFixed(4)} EOS`,
+    stake_cpu_quantity: `${stakeCpuAmount.toFixed(4)} EOS`,
+    transfer: 0
   };
 }
 
