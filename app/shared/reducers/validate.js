9476342d0791f28a5ca70127751041e0f7cc3e6d
dafuga <daniel.fugere@ballistiq.com>
2018-06-21 21:35:02 -0400

    removed success and error components from stake form

--- app/shared/reducers/validate.js
+++ app/shared/reducers/validate.js
@@ -15,31 +15,9 @@ export default function validate(state = {}, action) {
 
   const [, requestName, requestState] = matches;
 
-  const errField = `${requestName}_LAST_ERROR`;
-  const txField = `${requestName}_LAST_TRANSACTION`;
-
-  const newState = {
+  return {
     ...state,
     [requestName]: requestState,
-    [errField]: '',
-    [txField]: ''
+    [`${requestName}_ERROR`]: action.payload && action.payload.error
   };
-
-  if (action.payload) {
-    // Attempt to process any errors returned
-
-    if (action.payload.err) {
-      try {
-        newState[errField] = JSON.parse(action.payload.err);
-      } catch (e) {
-        newState[errField] = action.payload.err;
-      }
-    }
-
-    // Attach any returned transactions
-    if (action.payload.tx) {
-      newState[txField] = action.payload.tx;
-    }
-  }
-  return newState;
 }
