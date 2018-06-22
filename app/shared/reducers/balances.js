b03160606af994281a2600167cbc3baeaa266b13
Aaron Cox <aaron@greymass.com>
2018-06-22 17:51:05 -0400

    custom balance tracking

--- app/shared/reducers/balances.js
+++ app/shared/reducers/balances.js
@@ -1,36 +1,29 @@
 import * as types from '../actions/types';
 
-export default function balances(state = {}, action) {
+const initialState = {};
+
+export default function balances(state = initialState, action) {
   switch (action.type) {
     case types.CLEAR_ACCOUNT_CACHE:
+    case types.CLEAR_BALANCE_CACHE:
     case types.RESET_ALL_STATES: {
       return {};
     }
     case types.GET_ACCOUNT_BALANCE_FAILURE: {
-      return {
-        [action.payload.account_name]: false
-      };
+      return state;
     }
     case types.GET_ACCOUNT_BALANCE_REQUEST: {
       return state;
     }
     case types.GET_ACCOUNT_BALANCE_SUCCESS: {
+      const { account_name, tokens } = action.payload;
       return Object.assign({}, state, {
         __updated: Date.now(),
-        [action.payload.account_name]: formatBalances(action.payload.balances)
+        [account_name]: Object.assign({}, state[account_name], tokens)
       });
     }
     default: {
       return state;
     }
   }
-}
-
-function formatBalances(balances) {
-  const formatted = {};
-  for (let i = 0; i < balances.length; i += 1) {
-    const [amount, symbol] = balances[i].split(' ');
-    formatted[symbol] = parseFloat(amount);
-  }
-  return formatted;
 }
