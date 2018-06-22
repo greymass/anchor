296e23f9bcc7a3d850b061604819203a84485b1f
Aaron Cox <aaron@greymass.com>
2018-06-22 14:27:53 -0400

    Use the state key instead of props for resetting

--- app/shared/containers/Welcome/Key.js
+++ app/shared/containers/Welcome/Key.js
@@ -117,6 +117,7 @@ class WelcomeKeyContainer extends Component<Props> {
       account
     } = settings;
     const {
+      key,
       visible
     } = this.state;
     let currentPublic;
@@ -254,7 +255,7 @@ class WelcomeKeyContainer extends Component<Props> {
               <Form.Field
                 autoFocus
                 control={Input}
-                defaultValue={keys.key}
+                defaultValue={key}
                 icon={(validate.KEY === 'SUCCESS') ? 'checkmark' : 'x'}
                 loading={(validate.KEY === 'PENDING')}
                 name="key"
