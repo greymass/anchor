3a6a6254a49c83bb22cf71ac519b05279b880518
Aaron Cox <aaron@greymass.com>
2018-06-23 11:56:28 -0400

    Moved validation in the form so all fields can be validated at the same time

--- app/shared/components/Wallet/Panel/Form/Stake/Input.js
+++ app/shared/components/Wallet/Panel/Form/Stake/Input.js
@@ -9,71 +9,10 @@ import FormFieldToken from '../../../../Global/Form/Field/Token';
 class WalletPanelFormStakeInput extends Component<Props> {
   onChange = (e, { name, value }) => {
     const {
-      onChange,
-      onError
+      onChange
     } = this.props;
-
     const parsed = new Decimal(value.split(' ')[0]).toFixed(4);
-
-    this.setState({
-      value: parsed
-    }, () => {
-      const error = this.errorsInForm();
-      if (error) {
-        onError(error);
-      } else {
-        onChange(name, parsed);
-      }
-    });
-  }
-
-  errorsInForm = () => {
-    const {
-      cpuOriginal,
-      decimalCpuAmount,
-      decimalNetAmount,
-      EOSbalance,
-      netOriginal
-    } = this.props;
-
-    const {
-      value
-    } = this.state;
-
-    let cpuAmount = decimalCpuAmount;
-    let netAmount = decimalNetAmount;
-
-    if (this.props.name === 'cpuAmount') {
-      cpuAmount = value;
-    } else {
-      netAmount = value;
-    }
-
-    const decimalRegex = /^\d+(\.\d{1,4})?$/;
-
-    if (!decimalRegex.test(cpuAmount) || !decimalRegex.test(netAmount)) {
-      return 'not_valid_stake_amount';
-    }
-
-    cpuAmount = Decimal(cpuAmount);
-    netAmount = Decimal(netAmount);
-
-    if (cpuOriginal.equals(cpuAmount) && netOriginal.equals(netAmount)) {
-      return true;
-    }
-
-    if (!cpuAmount.greaterThan(0) || !netAmount.greaterThan(0)) {
-      return 'no_stake_left';
-    }
-
-    const cpuChange = cpuAmount.minus(cpuOriginal);
-    const netChange = netAmount.minus(netOriginal);
-
-    if (Math.max(0, cpuChange) + Math.max(0, netChange) > EOSbalance) {
-      return 'not_enough_balance';
-    }
-
-    return false;
+    onChange(name, parsed);
   }
 
   render() {
