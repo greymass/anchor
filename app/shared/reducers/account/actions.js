66e022804ccdd17b926597b6632df9f7861b72b0
dafuga <daniel.fugere@ballistiq.com>
2018-06-28 00:16:36 +0200

    made action rows more pretty

--- app/shared/reducers/account/actions.js
+++ app/shared/reducers/account/actions.js
@@ -1,4 +1,4 @@
-import * as types from '../actions/types';
+import * as types from '../../actions/types';
 
 const initialState = {
   lastError: {},
@@ -12,23 +12,27 @@ export default function actions(state = initialState, action) {
     case types.RESET_ALL_STATES: {
       return Object.assign({}, initialState);
     }
-    case types.CLEAR_ACTIONS_CACHE: {
+    case types.CLEAR_ACCOUNT_ACTIONS_CACHE: {
       return Object.assign({}, state, {
         list: []
       });
     }
-    case types.GET_ACTIONS_FAILURE: {
+    case types.GET_ACCOUNT_ACTIONS_FAILURE: {
       return Object.assign({}, state, {
+        lastError: action.payload.error,
         list: []
       });
     }
-    case types.GET_ACTIONS_SUCCESS: {
+    case types.GET_ACCOUNT_ACTIONS_SUCCESS: {
+      const actionsList = action.payload.results.actions;
+
       return Object.assign({}, state, {
-        __updated: Date.now(),
-        list: action.payload.list
+        updated: Date.now(),
+        list: actionsList,
+        lastTransaction: actionsList[actionsList.length - 1]
       });
     }
-    case types.GET_ACTIONS_REQUEST:
+    case types.GET_ACCOUNT_ACTIONS_REQUEST:
     default: {
       return state;
     }
