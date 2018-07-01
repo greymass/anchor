9f7d57f70708a036d72e428ba916ab2089b0d9f4
dafuga <daniel.fugere@ballistiq.com>
2018-07-01 20:42:55 +0200

    added system regproxy and unregproxy actions

---
+++ app/shared/actions/system/regproxy.js
@@ -0,0 +1,31 @@
+import * as types from '../types';
+
+import eos from '../helpers/eos';
+
+export function regproxy() {
+  return (dispatch: () => void, getState) => {
+    const {
+      connection,
+      wallet
+    } = getState();
+    const { account } = wallet;
+
+    dispatch({
+      type: types.SYSTEM_REGPROXY_PENDING
+    });
+    return eos(connection).regproxy({
+      proxy: account,
+      isproxy: true
+    }).then((tx) => dispatch({
+      payload: { tx },
+      type: types.SYSTEM_REGPROXY_SUCCESS
+    })).catch((err) => dispatch({
+      payload: { err },
+      type: types.SYSTEM_REGPROXY_FAILURE
+    }));
+  };
+}
+
+export default {
+  regproxy
+};
