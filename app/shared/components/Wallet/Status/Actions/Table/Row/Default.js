2e22152673d24274da38f3b8cb3e016bc7d93569
dafuga <daniel.fugere@ballistiq.com>
2018-07-02 12:39:03 +0100

    refactored row switch statement into individual components

---
+++ app/shared/components/Wallet/Status/Actions/Table/Row/Default.js
@@ -0,0 +1,37 @@
+// @flow
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+import { Icon } from 'semantic-ui-react';
+
+class WalletStatusActionsTableRowDefault extends Component<Props> {
+  render() {
+    const {
+      name,
+      data,
+      t
+    } = this.props;
+
+    const iconStyle = {
+      margin: '5px',
+      marginRight: '20px',
+      float: 'left',
+      verticalAlign: 'top'
+    };
+
+    return (
+      <div>
+        <Icon
+          name="arrow circle up"
+          size="large"
+          style={iconStyle}
+        />
+        <div>
+          {`${t('actions_table_row_default_header_one')} ${name} ${t('actions_table_row_default_header_two')} ${data.join(' | ')}.`}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default translate('actions')(WalletStatusActionsTableRowDefault);
+
