64e5a48d1eb7529e47f761f311ea7f443de1b769
dafuga <daniel.fugere@ballistiq.com>
2018-07-03 08:32:09 -0700

    addded register and unregister buttons to tools

---
+++ app/shared/components/Tools/Button/unregister.js
@@ -0,0 +1,71 @@
+// @flow
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+
+import GlobalTransactionModal from '../../Global/Transaction/Modal';
+import ToolsModalUnregister from '../Modal/Register';
+
+type Props = {
+  actions: {
+    clearSystemState: () => void
+  },
+  accounts: {},
+  balances: {},
+  settings: {},
+  validate: {},
+  system: {}
+};
+
+class ToolsButtonUnregister extends Component<Props> {
+  props: Props;
+
+  state = {
+    open: false
+  }
+
+  render() {
+    const {
+      actions,
+      accounts,
+      balances,
+      settings,
+      validate,
+      system,
+      t
+    } = this.props;
+
+    const {
+      open
+    } = this.state;
+
+    return (
+      <GlobalTransactionModal
+        actionName="PROXY"
+        actions={actions}
+        button={{
+          color: 'blue',
+          content: t('stake_button_cta'),
+          icon: 'microchip'
+        }}
+        content={(
+          <ToolsModalUnregister
+            account={accounts[settings.account]}
+            key="StakeForm"
+            settings={settings}
+            actions={actions}
+            onClose={this.onClose}
+            validate={validate}
+            balance={balances[settings.account]}
+            system={system}
+          />
+        )}
+        icon="microchip"
+        title={t('update_staked_coins')}
+        settings={settings}
+        system={system}
+      />
+    );
+  }
+}
+
+export default translate('stake')(ToolsButtonUnregister);
