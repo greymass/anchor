327417b23a680d9155361aa6de224c6a33aa6275
Aaron Cox <aaron@greymass.com>
2018-07-05 20:36:11 -0400

    Added loading dimmer

--- app/shared/components/Wallet/Status/Actions.js
+++ app/shared/components/Wallet/Status/Actions.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { Component } from 'react';
-import { Header, Grid, Loader, Segment, Visibility } from 'semantic-ui-react';
+import { Dimmer, Header, Grid, Loader, Segment, Visibility } from 'semantic-ui-react';
 import { translate } from 'react-i18next';
 import range from 'lodash/range';
 
@@ -110,43 +110,52 @@ class WalletStatusActions extends Component<Props> {
 
     return (
       <div ref={this.handleContextRef}>
-        {(actionHistory && actionHistory.list) ? (
-          <Grid.Column width={10}>
-            {(actionHistory.list.length > 0)
-             ? [(
-               <Visibility
-                 continuous
-                 key="ActionsTable"
-                 fireOnMount
-                 onBottomVisible={this.loadMore}
-                 once={false}
-               >
-                 <ActionsTable
-                   amount={amount}
-                   actionHistory={actionHistory}
-                   attached="top"
-                   chain={chain}
-                   settings={settings}
-                 />
-               </Visibility>
-             ), (
-               (amount > actionHistory.list.length && !this.reachedEndOfActions())
-               ? (
-                 <Segment key="ActionsTableLoading" clearing padded vertical>
-                   <Loader active />
+        {(actionHistory && actionHistory.list)
+          ? (
+            <Grid.Column width={10}>
+              {(actionHistory.list.length > 0)
+               ? [(
+                 <Visibility
+                   continuous
+                   key="ActionsTable"
+                   fireOnMount
+                   onBottomVisible={this.loadMore}
+                   once={false}
+                 >
+                   <ActionsTable
+                     amount={amount}
+                     actionHistory={actionHistory}
+                     attached="top"
+                     chain={chain}
+                     settings={settings}
+                   />
+                 </Visibility>
+               ), (
+                 (amount > actionHistory.list.length && !this.reachedEndOfActions())
+                 ? (
+                   <Segment key="ActionsTableLoading" clearing padded vertical>
+                     <Loader active />
+                   </Segment>
+                 ) : false
+               )]
+               : (
+                 <Segment attached="bottom" stacked>
+                   <Header textAlign="center">
+                     {t('actions_table_none')}
+                   </Header>
                  </Segment>
-               ) : false
-             )]
-             : (
-               <Segment attached="bottom" stacked>
-                 <Header textAlign="center">
-                   {t('actions_table_none')}
-                 </Header>
-               </Segment>
-             )
-            }
-          </Grid.Column>
-        ) : ''}
+               )
+              }
+            </Grid.Column>
+          )
+          : (
+            <Segment clearing style={{ minHeight: '100px' }}>
+              <Dimmer active>
+                <Loader>{t('loading')}</Loader>
+              </Dimmer>
+            </Segment>
+          )
+        }
       </div>
     );
   }
