daeb0691283b64e2e78db68939eec4a45a44254e
Aaron Cox <aaron@greymass.com>
2018-07-12 19:34:51 -0400

    version bump

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-eos-voter-0.3.2-amd64.deb"
-shasum -b -a 512 linux-eos-voter-0.3.2-amd64.deb
-echo "shasum -b -a 512 linux-eos-voter-0.3.2-amd64.snap"
-shasum -b -a 512 linux-eos-voter-0.3.2-amd64.snap
-echo "shasum -b -a 512 linux-eos-voter-0.3.2-arm64.deb"
-shasum -b -a 512 linux-eos-voter-0.3.2-arm64.deb
-echo "shasum -b -a 512 linux-eos-voter-0.3.2-armv7l.deb"
-shasum -b -a 512 linux-eos-voter-0.3.2-armv7l.deb
-echo "shasum -b -a 512 linux-eos-voter-0.3.2-i386.deb"
-shasum -b -a 512 linux-eos-voter-0.3.2-i386.deb
-echo "shasum -b -a 512 linux-eos-voter-0.3.2-x86_64.AppImage"
-shasum -b -a 512 linux-eos-voter-0.3.2-x86_64.AppImage
-echo "shasum -b -a 512 mac-eos-voter-0.3.2.dmg"
-shasum -b -a 512 mac-eos-voter-0.3.2.dmg
-echo "shasum -b -a 512 mac-eos-voter-0.3.2.zip"
-shasum -b -a 512 mac-eos-voter-0.3.2.zip
-echo "shasum -b -a 512 win-eos-voter-0.3.2.exe"
-shasum -b -a 512 win-eos-voter-0.3.2.exe
+echo "shasum -b -a 512 linux-eos-voter-0.3.3-amd64.deb"
+shasum -b -a 512 linux-eos-voter-0.3.3-amd64.deb
+echo "shasum -b -a 512 linux-eos-voter-0.3.3-amd64.snap"
+shasum -b -a 512 linux-eos-voter-0.3.3-amd64.snap
+echo "shasum -b -a 512 linux-eos-voter-0.3.3-arm64.deb"
+shasum -b -a 512 linux-eos-voter-0.3.3-arm64.deb
+echo "shasum -b -a 512 linux-eos-voter-0.3.3-armv7l.deb"
+shasum -b -a 512 linux-eos-voter-0.3.3-armv7l.deb
+echo "shasum -b -a 512 linux-eos-voter-0.3.3-i386.deb"
+shasum -b -a 512 linux-eos-voter-0.3.3-i386.deb
+echo "shasum -b -a 512 linux-eos-voter-0.3.3-x86_64.AppImage"
+shasum -b -a 512 linux-eos-voter-0.3.3-x86_64.AppImage
+echo "shasum -b -a 512 mac-eos-voter-0.3.3.dmg"
+shasum -b -a 512 mac-eos-voter-0.3.3.dmg
+echo "shasum -b -a 512 mac-eos-voter-0.3.3.zip"
+shasum -b -a 512 mac-eos-voter-0.3.3.zip
+echo "shasum -b -a 512 win-eos-voter-0.3.3.exe"
+shasum -b -a 512 win-eos-voter-0.3.3.exe
 
 cd ..
