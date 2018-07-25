36ca2037e88defa426d702d9cc2cb388398385ee
Aaron Cox <aaron@greymass.com>
2018-07-25 18:55:15 -0400

    Added optional generation to public key form

--- app/shared/components/Global/Form/Field/Key/Public.js
+++ app/shared/components/Global/Form/Field/Key/Public.js
@@ -1,24 +1,53 @@
 // @flow
 import React, { Component } from 'react';
-import { I18n } from 'react-i18next';
+import { translate } from 'react-i18next';
 import { Form, Input } from 'semantic-ui-react';
 
 import debounce from 'lodash/debounce';
 
 const ecc = require('eosjs-ecc');
 
-export default class GlobalFormFieldKeyPublic extends Component<Props> {
+class GlobalFormFieldKeyPublic extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
+      generated: '',
       value: props.defaultValue
     };
   }
-
+  generate = (e) => {
+    const {
+      name,
+      setPrivateKey
+    } = this.props;
+    e.preventDefault();
+    ecc
+      .randomKey()
+      .then((key) => {
+        const publicKey = ecc.privateToPublic(key);
+        // Set the value in the parent form with the provided name
+        this.onChange(null, {
+          name,
+          value: publicKey
+        });
+        // Also pass the private key back if a method was passed
+        if (setPrivateKey) {
+          setPrivateKey(name, publicKey, key);
+        }
+        // Set the local state
+        this.setState({
+          generated: publicKey,
+        });
+        return publicKey;
+      })
+      .catch(() => {
+        // no catch
+      });
+    return false;
+  }
   onChange = debounce((e, { name, value }) => {
     const parsed = value.trim();
     const valid = ecc.isValidPublic(parsed);
-
     this.setState({
       value: parsed
     }, () => {
@@ -29,29 +58,47 @@ export default class GlobalFormFieldKeyPublic extends Component<Props> {
     const {
       autoFocus,
       disabled,
+      generate,
       icon,
       label,
       loading,
-      name
+      name,
+      t
     } = this.props;
 
     const {
+      generated,
       value
     } = this.state;
 
+    let action;
+    if (generate) {
+      action = {
+        color: 'grey',
+        icon: 'redo alternate',
+        onClick: this.generate,
+        size: 'tiny'
+      };
+    }
+
     return (
       <Form.Field
+        action={action}
         autoFocus={autoFocus}
         control={Input}
         disabled={disabled}
         fluid
+        key={generated}
         icon={icon}
         label={label}
         loading={loading}
         name={name}
         onChange={this.onChange}
-        defaultValue={value}
+        defaultValue={generated || value}
       />
     );
   }
 }
+
+
+export default translate('tools')(GlobalFormFieldKeyPublic);
