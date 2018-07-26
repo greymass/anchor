bad80b9080cc1af9061ebaa12899119d14ca80ae
Aaron Cox <aaron@greymass.com>
2018-07-26 00:25:24 -0400

    Fix for ExplorerLink and no default

--- app/shared/components/Global/Modal/ExplorerLink.js
+++ app/shared/components/Global/Modal/ExplorerLink.js
@@ -2,14 +2,20 @@
 import React, { Component } from 'react';
 import GlobalModalDangerLink from './DangerLink';
 
-export default class ModalExplorerLink extends Component<Props> {
+export default class GlobalModalExplorerLink extends Component<Props> {
   render() {
     const {
-      blockExplorer,
+      blockExplorers,
       content,
       linkData,
-      linkType
+      linkType,
+      settings
     } = this.props;
+
+    const { blockExplorer: selected } = settings;
+    const blockExplorer = (selected in blockExplorers)
+      ? blockExplorers[selected]
+      : blockExplorers[Object.keys(blockExplorers)[0]];
 
     const urlPartsWithoutVariable = blockExplorer[linkType].split(`{${linkType}}`);
 
