1b259b74b0722b147b0472d9086b5d8cb7b2bb00
dafuga <daniel.fugere@ballistiq.com>
2018-07-27 16:08:17 -0700

    moved idleHandler to its own component

--- app/shared/containers/Root.js
+++ app/shared/containers/Root.js
@@ -2,18 +2,16 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
-import { ConnectedRouter } from 'react-router-redux';
-import IdleTimer from 'react-idle-timer';
 
-import { configureStore, history } from '../store/renderer/configureStore';
+import IdleHandler from './IdleHandler';
+import { configureStore } from '../store/renderer/configureStore';
 import i18n from '../i18n';
-import UpdaterContainer from './Updater';
 import { downloadProgress } from '../actions/app';
+
 import {
   cancelTransaction,
   setTransaction
 } from '../actions/transaction';
-import { lockWallet } from '../actions/wallet';
 
 import '../app.global.css';
 
@@ -22,39 +20,14 @@ const { ipcRenderer } = require('electron');
 const { store } = configureStore();
 
 export default class Root extends Component<Props> {
-  constructor(props) {
-    super(props);
-
-    this.idleTimer = null;
-  }
-
   render() {
-    const Routes = this.props.routes;
-
     return (
       <I18nextProvider i18n={i18n}>
         <Provider store={store}>
-          <IdleTimer
-            ref={ref => { this.idleTimer = ref; }}
-            element={document}
-            onIdle={onIdle}
-            timeout={store.getState().settings.idleTimeout || 999999999}
-          >
-            <UpdaterContainer>
-              <ConnectedRouter history={history}>
-                <Routes />
-              </ConnectedRouter>
-            </UpdaterContainer>
-          </IdleTimer>
+          <IdleHandler routes={this.props.routes} />
         </Provider>
       </I18nextProvider>
     );
-  }
-}
-
-function onIdle() {
-  if (store.getState().keys.key.length > 0) {
-    store.dispatch(lockWallet());
   }
 }
 
