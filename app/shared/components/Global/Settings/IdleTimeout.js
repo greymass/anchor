91fd2f7728a3b2491589b729e64b9e4693f54d7c
Aaron Cox <aaron@greymass.com>
2018-07-31 14:36:33 -0400

    Ordered so default is first.

--- app/shared/components/Global/Settings/IdleTimeout.js
+++ app/shared/components/Global/Settings/IdleTimeout.js
@@ -19,11 +19,11 @@ class GlobalSettingsIdleTimeout extends Component<Props> {
     } = this.props;
 
     const timeoutOptions = [
+      { key: 'no_timeout', value: 999999999, text: t('global_settings_idle_timeout_no_timeout') },
       { key: 'five_minutes', value: 5 * 60 * 1000, text: t('global_settings_idle_timeout_five_minutes') },
       { key: 'fifteen_minutes', value: 15 * 60 * 1000, text: t('global_settings_idle_timeout_fifteen_minutes') },
       { key: 'thirty_minutes', value: 30 * 60 * 1000, text: t('global_settings_idle_timeout_thirty_minutes') },
-      { key: 'sixty_minutes', value: 60 * 60 * 1000, text: t('global_settings_idle_timeout_sixty_minutes') },
-      { key: 'no_timeout', value: 999999999, text: t('global_settings_idle_timeout_no_timeout') }
+      { key: 'sixty_minutes', value: 60 * 60 * 1000, text: t('global_settings_idle_timeout_sixty_minutes') }
     ];
 
     return (
