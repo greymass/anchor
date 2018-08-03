f720460305ab6fa06b7e239b8edc3bff884951c2
dafuga <daniel.fugere@ballistiq.com>
2018-08-03 14:37:42 -0700

    fixed the width of account field

--- app/shared/components/Global/Form/Field/Account.js
+++ app/shared/components/Global/Form/Field/Account.js
@@ -65,57 +65,76 @@ class GlobalFormFieldAccount extends Component<Props> {
       });
     }
 
-    return (
-      <div>
-        <label htmlFor={name}>
-          <strong>{label}</strong>
-          {(fieldOption === 'manual')
-          ? (
-            <Form.Field
-              autoFocus={autoFocus}
-              control={Input}
-              disabled={disabled}
-              fluid={fluid}
-              icon={icon}
-              loading={loading}
-              name={name}
-              onChange={this.onChange}
-              ref={ref => { this.input = ref; }}
-              value={value}
-              width={width}
-            />
-          ) : ''}
+    return (hideOptions)
+      ? (
+        <Form.Field
+          autoFocus={autoFocus}
+          control={Input}
+          disabled={disabled}
+          fluid={fluid}
+          icon={icon}
+          loading={loading}
+          name={name}
+          onChange={this.onChange}
+          ref={ref => {
+            this.input = ref;
+          }}
+          value={value}
+          width={width}
+        />
+      ) : (
+        <div>
+          <label htmlFor={name}>
+            <strong>{label}</strong>
+            {(fieldOption === 'manual')
+            ? (
+              <Form.Field
+                autoFocus={autoFocus}
+                control={Input}
+                disabled={disabled}
+                fluid={fluid}
+                icon={icon}
+                loading={loading}
+                name={name}
+                onChange={this.onChange}
+                ref={ref => {
+                  this.input = ref;
+                }}
+                value={value}
+                width={width}
+              />
+            ) : ''}
 
-          {(fieldOption !== 'manual')
-          ? (
-            <Dropdown
-              defaultValue={value}
-              fluid
-              name={name}
-              onChange={this.onChange}
-              options={dropdownOptions}
-              selection
-            />
-          ) : ''}
-        </label>
-        {(!hideOptions)
-          ? (
-            <Form.Field style={{ margin: '10px' }}>
-              {['manual', 'exchanges', 'contacts'].map((option) => (
-                <Radio
-                  label={t(`global_form_field_${option}`)}
-                  key={option}
-                  name="inputRadioOptions"
-                  style={{ marginLeft: '10px' }}
-                  value={option}
-                  checked={this.state.fieldOption === option}
-                  onChange={this.handleRadioChange}
-                />
-              ))}
-            </Form.Field>
-          ) : <br />}
-      </div>
-    );
+            {(fieldOption !== 'manual')
+            ? (
+              <Dropdown
+                defaultValue={value}
+                fluid
+                name={name}
+                onChange={this.onChange}
+                options={dropdownOptions}
+                selection
+              />
+            ) : ''}
+          </label>
+          {(!hideOptions)
+            ? (
+              <Form.Field style={{ margin: '10px' }}>
+                {['manual', 'exchanges', 'contacts'].map((option) => (
+                  <Radio
+                    label={t(`global_form_field_${option}`)}
+                    key={option}
+                    name="inputRadioOptions"
+                    style={{ marginLeft: '10px' }}
+                    value={option}
+                    checked={this.state.fieldOption === option}
+                    onChange={this.handleRadioChange}
+                  />
+                ))}
+              </Form.Field>
+            ) : <br />}
+        </div>
+      );
   }
 }
 
