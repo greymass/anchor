28eaa04a73845675bd04571e0a609ebab39cc12b
dafuga <daniel.fugere@ballistiq.com>
2018-08-06 19:02:24 -0800

    using ResourcePercentage global component to display resources used or available

---
+++ app/shared/components/Global/Data/Resource/Percentage.js
@@ -0,0 +1,34 @@
+// @flow
+import React, { Component } from 'react';
+import { Progress, Responsive } from 'semantic-ui-react';
+
+export default class GlobalDataResourcePercentage extends Component<Props> {
+  render() {
+    const {
+      color,
+      displayAvailableSetting,
+      percentageUsed,
+      size,
+      style
+    } = this.props;
+
+    const percentageToDisplay =
+      (displayAvailableSetting ? (100 - percentageUsed) : percentageUsed);
+
+    return (
+      <Progress
+        color={color}
+        label={(
+          <div className="label">
+            {percentageToDisplay}%
+            {' '}
+            <Responsive as="span" minWidth={800} />
+          </div>
+        )}
+        percent={percentageToDisplay}
+        size={size}
+        style={style}
+      />
+    );
+  }
+}
