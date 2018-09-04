2d85da7cafdd8f3e3976440c35c60f715f9136c0
dafuga <daniel.fugere@ballistiq.com>
2018-09-04 18:06:24 -0400

    added a bidname confirm component

---
+++ app/shared/components/Tools/Form/BidName/Confirming.js
@@ -0,0 +1,61 @@
+// @flow
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+import { Button, Divider, Icon, Segment, Table, Header } from 'semantic-ui-react';
+
+class ToolsFormBidNameConfirming extends Component<Props> {
+  onConfirm = () => {
+    const {
+      onConfirm
+    } = this.props;
+
+    onConfirm();
+  }
+
+  render() {
+    const {
+      formAttributes,
+      onBack,
+      t,
+    } = this.props;
+
+    return (
+      <Segment padding="true" basic>
+        <Header textAlign="center">
+          {t('tools_form_bid_name_confirming_header')}
+        </Header>
+        <Table size="small" celled>
+          <Table.Body>
+            {formAttributes.map((formAttribute) => {
+              return (
+                <Table.Row>
+                  <Table.Cell width={8}>
+                    {t(`tools_form_bid_name_${formAttribute}`)}
+                  </Table.Cell>
+                  <Table.Cell width={8}>
+                    {this.props.formValues[formAttribute]}
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table>
+        <Divider />
+        <Button
+          onClick={onBack}
+        >
+          <Icon name="arrow left" /> {t('tools_form_bid_name_confirming_back')}
+        </Button>
+        <Button
+          color="blue"
+          floated="right"
+          onClick={this.onConfirm}
+        >
+          <Icon name="check" /> {t('tools_form_bid_name_confirming_button')}
+        </Button>
+      </Segment>
+    );
+  }
+}
+
+export default translate('tools')(ToolsFormBidNameConfirming);
