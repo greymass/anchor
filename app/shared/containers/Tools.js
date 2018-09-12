9084bcd8de173851ffefb77793f5f008f42bad66
dafuga <daniel.fugere@ballistiq.com>
2018-09-11 21:24:39 -0700

    added all localization keys to bidname section

--- app/shared/containers/Tools.js
+++ app/shared/containers/Tools.js
@@ -29,6 +29,7 @@ import ToolsStateWallet from '../components/Tools/State/Wallet';
 import ToolsWallets from '../components/Tools/Wallets';
 
 import * as AccountsActions from '../actions/accounts';
+import * as BidNameActions from '../actions/system/bidname';
 import * as ContractsActions from '../actions/contracts';
 import * as CreateAccountActions from '../actions/createaccount';
 import * as CustomTokensActions from '../actions/customtokens';
@@ -255,6 +256,7 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators({
       ...AccountsActions,
+      ...BidNameActions,
       ...ContractsActions,
       ...CreateAccountActions,
       ...CustomTokensActions,
