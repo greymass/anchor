aab4ca5eb7ae78a5702d74347d7a6b78bf9195d5
dafuga <daniel.fugere@ballistiq.com>
2018-09-18 12:49:25 -0700

    Checking that account bids are the highest on BidName page load

--- app/shared/actions/namebids.js
+++ app/shared/actions/namebids.js
@@ -1,12 +1,14 @@
 import * as types from './types';
 import eos from './helpers/eos';
+import { setSetting } from './settings';
+import { findIndex } from 'lodash';
 
 export function getBidForName(name) {
   return (dispatch: () => void, getState) => {
     dispatch({
       type: types.SYSTEM_NAMEBID_PENDING
     });
-    const { connection } = getState();
+    const { connection, settings } = getState();
     // Don't retrieve if we're not on mainnet
     if (connection.chain !== 'eos-mainnet') {
       return dispatch({
@@ -26,6 +28,17 @@ export function getBidForName(name) {
     eos(connection).getTableRows(query).then((results) => {
       const { rows } = results;
       const namebid = rows[0];
+
+      const recentBids = (settings.recentBids && settings.recentBids) || {};
+
+      const bidIndex = findIndex(recentBids[settings.account] || [], { newname: namebid.newname });
+
+      if (bidIndex > -1) {
+        recentBids[settings.account][bidIndex] =
+          { newname: namebid.newname, bid: recentBids[settings.account][bidIndex].bid, highestBid: namebid.highest_bid };
+
+        dispatch(setSetting('recentBids', recentBids));
+      }
 
       return dispatch({
         type: types.SYSTEM_NAMEBID_SUCCESS,
