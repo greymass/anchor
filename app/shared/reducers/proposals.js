9e979ca7bea60eb165ec2ea98f1b8a8aa717301a
Aaron Cox <aaron@greymass.com>
2018-09-19 14:28:01 -0400

    Implement voting

--- app/shared/reducers/proposals.js
+++ app/shared/reducers/proposals.js
@@ -2,6 +2,7 @@ import * as types from '../actions/types';
 
 const initialState = {
   list: [],
+  votes: [],
   scope: ''
 };
 
@@ -9,6 +10,12 @@ export default function proposals(state = initialState, action) {
   switch (action.type) {
     case types.RESET_ALL_STATES: {
       return Object.assign({}, initialState);
+    }
+    case types.SYSTEM_GOVERNANCE_GET_PROPOSALVOTES_SUCCESS: {
+      return Object.assign({}, state, {
+        votes: action.payload.votes,
+        scope: action.payload.scope
+      });
     }
     case types.SYSTEM_GOVERNANCE_GET_PROPOSALS_SUCCESS: {
       return Object.assign({}, state, {
