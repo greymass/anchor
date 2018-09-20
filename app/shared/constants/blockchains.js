59b5531bf5c4ee1ddf3d901622fb5729ca268994
dafuga <daniel.fugere@ballistiq.com>
2018-09-20 11:51:08 -0700

    added a blockchains constant

---
+++ app/shared/constants/blockchains.js
@@ -0,0 +1,22 @@
+const blockchains = [
+  {
+    name: 'eos-mainnet',
+    prefix: 'EOS',
+    node: 'https://eos.greymass.com',
+    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+  },
+  {
+    name: 'jungle-testnet',
+    prefix: 'EOS',
+    node: 'http://jungle.cryptolions.io:18888',
+    chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca'
+  },
+  {
+    name: 'telos-testnet',
+    prefix: 'TLOS',
+    node: 'https://api.eos.miami:17441',
+    chainId: '6c8aacc339bf1567743eb9c8ab4d933173aa6dca4ae6b6180a849c422f5bb207'
+  }
+];
+
+export default blockchains;
