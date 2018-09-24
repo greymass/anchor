f037bd9505b271399607de7767870904bbf5f6e2
dafuga <daniel.fugere@ballistiq.com>
2018-09-24 09:49:10 -0700

    updated action row components to handle all eosio blockchains

--- app/shared/components/Wallet/Status/Actions/Table/Row/Delegatebw.js
+++ app/shared/components/Wallet/Status/Actions/Table/Row/Delegatebw.js
@@ -7,22 +7,23 @@ class WalletStatusActionsTableRowDelegatebw extends Component<Props> {
   render() {
     const {
       action,
+      connection,
       t
     } = this.props;
     const {
       data
     } = action.action_trace.act;
 
-    const stakeToCpuDescription = (data.stake_cpu_quantity !== '0.0000 EOS') ? (
+    const stakeToCpuDescription = (data.stake_cpu_quantity !== `0.0000 ${connection.chainSymbol}`) ? (
       `${data.stake_cpu_quantity} ${t('actions_table_row_delegatebw_text_two')}`
     ) : '';
 
-    const stakeToNetDescription = (data.stake_net_quantity !== '0.0000 EOS') ? (
+    const stakeToNetDescription = (data.stake_net_quantity !== `0.0000 ${connection.chainSymbol}`) ? (
       `${data.stake_net_quantity} ${t('actions_table_row_delegatebw_text_three')}`
     ) : '';
 
     const stakeConnector =
-      (data.stake_cpu_quantity !== '0.0000 EOS' && data.stake_net_quantity !== '0.0000 EOS') ? (
+      (data.stake_cpu_quantity !== `0.0000 ${connection.chainSymbol}` && data.stake_net_quantity !== `0.0000 ${connection.chainSymbol}`) ? (
         ` ${t('actions_table_row_text_and')} `
       ) : '';
 
