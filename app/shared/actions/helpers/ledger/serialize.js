419f87b5accea00105755ffc4d38ce671bf9cc01
Aaron Cox <aaron@greymass.com>
2018-10-16 02:36:27 -0400

    Helpers/Action flow for Ledger signing

--- app/shared/actions/helpers/ledger/serialize.js
+++ app/shared/actions/helpers/ledger/serialize.js
@@ -4,65 +4,65 @@ const assert = require('assert');
 const asn1 = require('asn1-ber');
 
 export default function serialize(chainId, transaction, types) {
-  const writter = new asn1.BerWriter();
+  const writer = new asn1.BerWriter();
 
-  encode(writter, fcbuffer.toBuffer(types.checksum256(), chainId));
-  encode(writter, fcbuffer.toBuffer(types.time(), transaction.expiration));
-  encode(writter, fcbuffer.toBuffer(types.uint16(), transaction.ref_block_num));
+  encode(writer, fcbuffer.toBuffer(types.checksum256(), chainId));
+  encode(writer, fcbuffer.toBuffer(types.time(), transaction.expiration));
+  encode(writer, fcbuffer.toBuffer(types.uint16(), transaction.ref_block_num));
   encode(
-    writter,
+    writer,
     fcbuffer.toBuffer(types.uint32(), transaction.ref_block_prefix)
   );
   encode(
-    writter,
-    fcbuffer.toBuffer(types.unsigned_int(), transaction.net_usage_words)
+    writer,
+    fcbuffer.toBuffer(types.unsigned_int(), 0) //transaction.net_usage_words
   );
   encode(
-    writter,
+    writer,
     fcbuffer.toBuffer(types.uint8(), transaction.max_cpu_usage_ms)
   );
   encode(
-    writter,
+    writer,
     fcbuffer.toBuffer(types.unsigned_int(), transaction.delay_sec)
   );
 
   assert(transaction.context_free_actions.length === 0);
-  encode(writter, fcbuffer.toBuffer(types.unsigned_int(), 0));
+  encode(writer, fcbuffer.toBuffer(types.unsigned_int(), 0));
 
   assert(transaction.actions.length === 1);
-  encode(writter, fcbuffer.toBuffer(types.unsigned_int(), 1));
+  encode(writer, fcbuffer.toBuffer(types.unsigned_int(), 1));
 
   const action = transaction.actions[0];
 
-  encode(writter, fcbuffer.toBuffer(types.account_name(), action.account));
-  encode(writter, fcbuffer.toBuffer(types.action_name(), action.name));
+  encode(writer, fcbuffer.toBuffer(types.account_name(), action.account));
+  encode(writer, fcbuffer.toBuffer(types.action_name(), action.name));
 
   encode(
-    writter,
+    writer,
     fcbuffer.toBuffer(types.unsigned_int(), action.authorization.length)
   );
   for (let i = 0; i < action.authorization.length; i += 1) {
     const authorization = action.authorization[i];
 
     encode(
-      writter,
+      writer,
       fcbuffer.toBuffer(types.account_name(), authorization.actor)
     );
     encode(
-      writter,
+      writer,
       fcbuffer.toBuffer(types.permission_name(), authorization.permission)
     );
   }
 
   const data = Buffer.from(action.data, 'hex');
-  encode(writter, fcbuffer.toBuffer(types.unsigned_int(), data.length));
-  encode(writter, data);
+  encode(writer, fcbuffer.toBuffer(types.unsigned_int(), data.length));
+  encode(writer, data);
 
-  assert(writter, transaction.transaction_extensions.length === 0);
-  encode(writter, fcbuffer.toBuffer(types.unsigned_int(), 0));
-  encode(writter, fcbuffer.toBuffer(types.checksum256(), Buffer.alloc(32, 0)));
+  assert(writer, transaction.transaction_extensions.length === 0);
+  encode(writer, fcbuffer.toBuffer(types.unsigned_int(), 0));
+  encode(writer, fcbuffer.toBuffer(types.checksum256(), Buffer.alloc(32, 0)));
 
-  return writter.buffer;
+  return writer.buffer;
 }
 
 function encode(writter, buffer) {
