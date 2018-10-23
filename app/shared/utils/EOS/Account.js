e0c4a25ff44f60c806a7031fcf8f7c36433333ac
Aaron Cox <aaron@greymass.com>
2018-10-22 22:32:46 -0400

    Updated EOSAccount model

--- app/shared/utils/EOS/Account.js
+++ app/shared/utils/EOS/Account.js
@@ -1,4 +1,4 @@
-import { find } from 'lodash';
+import { filter, find, flatten } from 'lodash';
 
 export default class EOSAccount {
   constructor(account = undefined) {
@@ -6,25 +6,82 @@ export default class EOSAccount {
     return this;
   }
 
-  getAuthorization(pubkey) {
+  getPermission(name) {
+    const { account } = this;
+    if (account) {
+      return find(account.permissions, (perm) => (perm.perm_name === name));
+    }
+    return {};
+  }
+
+  getAuthorization(pubkey, returnObject = false) {
     const { account } = this;
     if (account) {
       // Is this the owner key? If so, return authoritization
       const isOwner = find(account.permissions, (perm) =>
         find(perm.required_auth.keys, (key) =>
           (key.key === pubkey && perm.perm_name === 'owner')));
-      if (isOwner) return `${account.account_name}@owner`;
+      if (isOwner) {
+        if (returnObject) return isOwner;
+        return `${account.account_name}@owner`;
+      }
       // Is this the active key? If so, return authoritization
       const isActive = find(account.permissions, (perm) =>
         find(perm.required_auth.keys, (key) =>
           (key.key === pubkey && perm.perm_name === 'active')));
-      if (isActive) return `${account.account_name}@active`;
+      if (isActive) {
+        if (returnObject) return isActive;
+        return `${account.account_name}@active`;
+      }
       // otherwise find the first non active/owner match
       const permission = find(account.permissions, (perm) =>
         find(perm.required_auth.keys, (key) => key.key === pubkey));
-      if (permission) return `${account.account_name}@${permission.perm_name}`;
+      if (permission) {
+        if (returnObject) return permission;
+        return `${account.account_name}@${permission.perm_name}`;
+      }
       // if no matches, return undefined
     }
     return undefined;
+  }
+
+  getAuthorizations(pubkey) {
+    const { account } = this;
+    if (account) {
+      // Return all authorizations which this key matches
+      return filter(account.permissions, (perm) =>
+        !!filter(perm.required_auth.keys, { key: pubkey }).length);
+    }
+    return [];
+  }
+
+  getAuthorizationOptions() {
+    const { account } = this;
+    if (account) {
+      return flatten(account.permissions.map(perm => {
+        const { keys } = perm.required_auth;
+        return keys.map(key => ({
+          type: perm.perm_name,
+          pubkey: key.key
+        }));
+      }));
+    }
+    return [];
+  }
+
+  getKeysForAuthorization(authorization) {
+    const { account } = this;
+    if (account) {
+      const permissions = filter(account.permissions, {
+        perm_name: authorization
+      });
+      return flatten(permissions.map(perm => {
+        const { keys } = perm.required_auth;
+        return keys.map(key => ({
+          type: perm.perm_name,
+          pubkey: key.key
+        }));
+      }));
+    }
   }
 }
