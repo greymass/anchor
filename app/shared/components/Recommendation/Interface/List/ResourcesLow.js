650ef6eeed4b872e85ec4192792d6e2f2a81cd63
dafuga <daniel.fugere@ballistiq.com>
2018-10-22 17:44:54 -0700

    Added ActiveAndOwnerSame as well as ResourcesLow recommendation components.

---
+++ app/shared/components/Recommendation/Interface/List/ResourcesLow.js
@@ -0,0 +1,41 @@
+// @flow
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+
+import { Message } from 'semantic-ui-react';
+
+class RecommendationInterfaceListResourcesLow extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    const {
+      account,
+      t
+    } = this.props;
+
+    const netAmount = account.total_resources &&
+      account.total_resources.net_weight &&
+      Number(account.total_resources.net_weight.split(' ')[0]);
+
+    const cpuAmount = account.total_resources &&
+      account.total_resources.net_weight &&
+      Number(account.total_resources.cpu_weight.split(' ')[0]);
+
+    const shouldDisplay = netAmount < 1 || cpuAmount < 1;
+
+    return (shouldDisplay)
+      ? (
+        <Message
+          content={t('recommendations_warning_resources_low')}
+          icon="warning"
+          warning
+        />
+      ) : null;
+  }
+}
+
+export default translate('recommendations')(RecommendationInterfaceListResourcesLow);
