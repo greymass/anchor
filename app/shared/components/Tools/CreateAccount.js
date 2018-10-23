8f1e7dde6e585ec528199708943f4e184e997596
Aaron Cox <aaron@greymass.com>
2018-10-23 15:30:06 -0400

    Block account creation for Ledger (until functional)

--- app/shared/components/Tools/CreateAccount.js
+++ app/shared/components/Tools/CreateAccount.js
@@ -28,6 +28,18 @@ class ToolsCreateAccount extends Component<Props> {
       t
     } = this.props;
 
+    if (settings.walletMode === 'ledger') {
+      return (
+        <Segment>
+          <Header
+            content={t('tools_create_account_unavailable_header')}
+            icon="warning sign"
+            subheader={t('tools_create_account_unavailable_subheader')}
+          />
+        </Segment>
+      );
+    }
+
     const account = accounts[settings.account];
 
     const transaction = system && system.CREATEACCOUNT_LAST_TRANSACTION;
