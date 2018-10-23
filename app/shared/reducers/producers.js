a92c7fa8798d98689cc130e014cc5d435b6e8fc4
Aaron Cox <aaron@greymass.com>
2018-10-23 15:31:58 -0400

    Prevent updating if not specified

--- app/shared/reducers/producers.js
+++ app/shared/reducers/producers.js
@@ -68,6 +68,7 @@ export default function producers(state = initialState, action) {
       });
     }
     case types.SYSTEM_VOTEPRODUCER_SUCCESS: {
+      if (!action.payload.proxy && !action.payload.producers) return state;
       return Object.assign({}, state, {
         proxy: action.payload.proxy,
         selected: action.payload.producers
