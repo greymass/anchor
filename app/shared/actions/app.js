a8e70813b99e578f613d6082dccca4f7ce3480e6
Aaron Cox <aaron@greymass.com>
2018-10-22 23:37:31 -0400

    constants loading from contract

--- app/shared/actions/app.js
+++ app/shared/actions/app.js
@@ -1,4 +1,5 @@
 import * as types from './types';
+import eos from './helpers/eos';
 
 export function downloadProgress(progress) {
   return (dispatch: () => void) => {
@@ -6,6 +7,41 @@ export function downloadProgress(progress) {
       type: types.APP_UPDATE_DOWNLOAD_PROGRESS,
       payload: progress
     });
+  };
+}
+
+export function getConstants() {
+  return (dispatch: () => void, getState) => {
+    dispatch({
+      type: types.SYSTEM_GETCONSTANTS_PENDING
+    });
+    const { connection } = getState();
+    // Don't retrieve if we're not on mainnet
+    if (connection.chain !== 'eos-mainnet') {
+      return dispatch({
+        type: types.SYSTEM_GETCONSTANTS_FAILURE
+      });
+    }
+
+    const query = {
+      json: true,
+      code: 'anchorwallet',
+      scope: 'anchorwallet',
+      table: 'constants',
+      limit: 1000,
+    };
+    eos(connection).getTableRows(query).then((results) => {
+      const { rows } = results;
+      const data = rows.reduce((map, { key, value }) =>
+        ({ ...map, [key]: value }), {});
+      return dispatch({
+        type: types.SYSTEM_GETCONSTANTS_SUCCESS,
+        payload: { data }
+      });
+    }).catch((err) => dispatch({
+      type: types.SYSTEM_GETCONSTANTS_FAILURE,
+      payload: { err },
+    }));
   };
 }
 
