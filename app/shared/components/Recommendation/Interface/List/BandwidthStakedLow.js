7d7d34e21995b437bea78d385536fba1be531782
dafuga <daniel.fugere@ballistiq.com>
2018-10-25 21:12:42 -0700

    feature: added more recommendations

---
+++ app/shared/components/Recommendation/Interface/List/BandwidthStakedLow.js
@@ -0,0 +1,43 @@
+// @flow
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+
+import { Message } from 'semantic-ui-react';
+
+class RecommendationInterfaceListResourcesLow extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    const {
+      account,
+      t
+    } = this.props;
+
+    const netStaked = account.total_resources &&
+      account.total_resources.net_weight &&
+      Number(account.total_resources.net_weight.split(' ')[0]);
+
+    const shouldDisplayWarning = netStaked < 1;
+
+    return (shouldDisplayWarning)
+      ? (
+        <Message
+          content={t('recommendations_warning_net_staked_low')}
+          icon="warning"
+          warning
+        />
+      ) : (
+        <Message
+          content={t('recommendations_success_net_staked_sufficient')}
+          icon="thumbs up"
+          success
+        />
+      );
+  }
+}
+
+export default translate('recommendations')(RecommendationInterfaceListResourcesLow);
