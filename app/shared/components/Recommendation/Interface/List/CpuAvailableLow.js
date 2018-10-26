54f57a55b16b51f31e2cb73eb5cd433fe6ddd924
dafuga <daniel.fugere@ballistiq.com>
2018-10-25 21:12:42 -0700

    feature: added more recommendations

---
+++ app/shared/components/Recommendation/Interface/List/CpuAvailableLow.js
@@ -0,0 +1,34 @@
+// @flow
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+
+import { Message } from 'semantic-ui-react';
+
+class RecommendationInterfaceListResourcesLow extends Component<Props> {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    const {
+      account,
+      t
+    } = this.props;
+
+    const shouldDisplayWarning = account.cpu_limit &&
+      Number(account.cpu_limit.available) < 10000;
+
+    return (shouldDisplayWarning)
+      ? (
+        <Message
+          content={t('recommendations_warning_cpu_available_low')}
+          icon="warning"
+          warning
+        />
+      ) : '';
+  }
+}
+
+export default translate('recommendations')(RecommendationInterfaceListResourcesLow);
