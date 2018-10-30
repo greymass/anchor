28d7e7b0e4c6e2d82a88483a4cd70c38c06475f3
Aaron Cox <aaron@greymass.com>
2018-10-30 14:21:29 -0400

    Split upgrade message into two components

--- app/shared/components/Global/Message/App/Upgrade.js
+++ app/shared/components/Global/Message/App/Upgrade.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import { translate } from 'react-i18next';
 
-import { Button, Form, Header, Icon, Modal, Segment } from 'semantic-ui-react';
+import { Button, Form, Header, Icon, Segment } from 'semantic-ui-react';
 
 const { ipcRenderer } = require('electron');
 
@@ -23,48 +23,44 @@ class GlobalMessageAppUpgrade extends Component<Props> {
       t,
     } = this.props;
     return (
-      <Modal
-        open
-        size="small"
-      >
-        <Modal.Content>
-          <Segment basic size="large" textAlign="center">
-            <Header color="blue" icon size="large">
-              <Icon name="info circle" />
-              <Header.Content>
-                {t('global_app_requires_upgrade_header', { new: constants.version })}
-                <Header.Subheader>
-                  {t('global_app_requires_upgrade_subheader')}
-                </Header.Subheader>
-              </Header.Content>
-            </Header>
-          </Segment>
+      <React.Fragment>
+        <Segment basic size="large" textAlign="center">
+          <Header color="blue" icon size="large">
+            <Icon name="info circle" />
+            <Header.Content>
+              {t('global_app_requires_upgrade_header', { new: constants.version })}
+              <Header.Subheader>
+                {t('global_app_requires_upgrade_subheader')}
+              </Header.Subheader>
+            </Header.Content>
+          </Header>
           <Form>
             <Form.TextArea
+              label={t('global_app_requires_upgrade_details')}
               rows={6}
               value={constants.versioninfo[constants.version]}
             />
           </Form>
-          <Segment basic size="large" textAlign="center">
-            <p>
-              <Button
-                content="Upgrade"
-                color="blue"
-                icon="up arrow"
-                onClick={this.checkForUpdates}
-                size="large"
-              />
-            </p>
-            <p>
-              <Button
-                content="Skip this Version"
-                color="grey"
-                onClick={this.skipUpdate}
-              />
-            </p>
-          </Segment>
-        </Modal.Content>
-      </Modal>
+        </Segment>
+        <Segment basic size="large" textAlign="center">
+          <p>
+            <Button
+              content="Upgrade"
+              color="blue"
+              icon="up arrow"
+              onClick={this.checkForUpdates}
+              size="large"
+            />
+          </p>
+          <p>
+            <Button
+              content="Skip this Version"
+              color="grey"
+              onClick={this.skipUpdate}
+            />
+          </p>
+        </Segment>
+      </React.Fragment>
     );
   }
 }
