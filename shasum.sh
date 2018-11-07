e607e72fd8b52eff46945c32c009d1570c89e5e1
Aaron Cox <aaron@greymass.com>
2018-11-07 16:50:26 -0500

    Temp removed shasum checks for disabled files

--- shasum.sh
+++ shasum.sh
@@ -4,14 +4,14 @@ cd release
 
 echo "shasum -b -a 512 linux-eos-voter-0.6.0-amd64.deb"
 shasum -b -a 512 linux-eos-voter-0.6.0-amd64.deb
-echo "shasum -b -a 512 linux-eos-voter-0.6.0-amd64.snap"
-shasum -b -a 512 linux-eos-voter-0.6.0-amd64.snap
+# echo "shasum -b -a 512 linux-eos-voter-0.6.0-amd64.snap"
+# shasum -b -a 512 linux-eos-voter-0.6.0-amd64.snap
 echo "shasum -b -a 512 linux-eos-voter-0.6.0-arm64.deb"
 shasum -b -a 512 linux-eos-voter-0.6.0-arm64.deb
 echo "shasum -b -a 512 linux-eos-voter-0.6.0-armv7l.deb"
 shasum -b -a 512 linux-eos-voter-0.6.0-armv7l.deb
-echo "shasum -b -a 512 linux-eos-voter-0.6.0-i386.deb"
-shasum -b -a 512 linux-eos-voter-0.6.0-i386.deb
+# echo "shasum -b -a 512 linux-eos-voter-0.6.0-i386.deb"
+# shasum -b -a 512 linux-eos-voter-0.6.0-i386.deb
 echo "shasum -b -a 512 linux-eos-voter-0.6.0-x86_64.AppImage"
 shasum -b -a 512 linux-eos-voter-0.6.0-x86_64.AppImage
 echo "shasum -b -a 512 mac-eos-voter-0.6.0.dmg"
