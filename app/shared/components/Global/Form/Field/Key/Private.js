71fd3842c6a4ee300429492e9bbea02b4f1312e5
Aaron Cox <aaron@greymass.com>
2018-11-08 13:07:45 -0500

    Default to EOS prefix

--- app/shared/components/Global/Form/Field/Key/Private.js
+++ app/shared/components/Global/Form/Field/Key/Private.js
@@ -19,7 +19,8 @@ class GlobalFormFieldKeyPrivate extends Component<Props> {
     }, () => {
       try {
         const { connection } = this.props;
-        const publicKey = ecc.privateToPublic(parsed, connection.keyPrefix);
+        const prefix = (connection && connection.keyPrefix) ? connection.keyPrefix : 'EOS';
+        const publicKey = ecc.privateToPublic(parsed, prefix);
         const valid = ecc.isValidPrivate(parsed);
         this.props.onChange(e, {
           name,
