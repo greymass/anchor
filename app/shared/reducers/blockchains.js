1dfbf42341c7fc634e95fd41e3cfa48324c5b005
Aaron Cox <aaron@greymass.com>
2018-12-13 21:28:56 -0500

    Updated a bunch of default state for existing chains

--- app/shared/reducers/blockchains.js
+++ app/shared/reducers/blockchains.js
@@ -20,7 +20,7 @@ const initialState = [
     "_id": "eos-testnet-jungle",
     "chainId": "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",
     "keyPrefix": "EOS",
-    "name": "Jungle Testnet (EOS)",
+    "name": "Jungle (EOS)",
     "node": "http://jungle.cryptolions.io:18888",
     "supportedContracts": [
     ],
@@ -31,7 +31,7 @@ const initialState = [
     "_id": "eos-testnet-cryptokylin",
     "chainId": "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191",
     "keyPrefix": "EOS",
-    "name": "CryptoKylin Testnet (EOS)",
+    "name": "CryptoKylin (EOS)",
     "node": "https://kylin.eoscanada.com:443",
     "supportedContracts": [
     ],
@@ -42,7 +42,7 @@ const initialState = [
     "_id": "insights-mainnet",
     "chainId": "b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664",
     "keyPrefix": "EOS",
-    "name": "Insights Mainnet",
+    "name": "Insights",
     "node": "https://ireland-history.insights.network",
     "supportedContracts": [
     ],
@@ -51,20 +51,20 @@ const initialState = [
   },
   {
     "_id": "telos-mainnet",
-    "chainId": "6c8aacc339bf1567743eb9c8ab4d933173aa6dca4ae6b6180a849c422f5bb207",
+    "chainId": "4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11",
     "keyPrefix": "EOS",
     "name": "Telos",
     "node": "https://telos.greymass.com",
     "supportedContracts": [
     ],
     "symbol": "TLOS",
-    "testnet": true
+    "testnet": false
   },
   {
     "_id": "telos-testnet",
     "chainId": "e17615decaecd202a365f4c029f206eee98511979de8a5756317e2469f2289e3",
     "keyPrefix": "EOS",
-    "name": "Telos Testnet",
+    "name": "Telos",
     "node": "https://api.eos.miami:17441",
     "supportedContracts": [
     ],
