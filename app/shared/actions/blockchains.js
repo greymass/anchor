d7981c741a374a389d9745bd171a1f629e7c11ed
Aaron Cox <aaron@greymass.com>
2018-12-13 21:19:58 -0500

    Swap blockchain method that resets current wallet and switches node

--- app/shared/actions/blockchains.js
+++ app/shared/actions/blockchains.js
@@ -1,9 +1,27 @@
 import { find, forEach, partition } from 'lodash';
 
 import * as types from './types';
-import { setSettings } from './settings';
+import { clearWallet } from './wallet';
+import { setSettings, setSettingWithValidation } from './settings';
+
+function swapBlockchain(chainId) {
+  return (dispatch: () => void, getState) => {
+    const { blockchains } = getState();
+    const blockchain = find(blockchains, { chainId });
+    dispatch(clearWallet());
+    dispatch(setSettingWithValidation('node', blockchain.node));
+    return dispatch(setSettings({
+      account: undefined,
+      authorization: undefined,
+      chainId,
+      walletHash: undefined,
+      walletMode: undefined,
+    }));
+  }
+}
 
 export default {
-  addBlockchain,
-  setNode,
+  // addBlockchain,
+  // setNode,
+  swapBlockchain,
 };
