86df9689bbc7eb46d32d118d950456c84db2a451
Aaron Cox <aaron@greymass.com>
2018-12-13 21:28:30 -0500

    Start of new blockchain management interface

---
+++ app/shared/components/Global/Button/Blockchain/Import.js
@@ -0,0 +1,45 @@
+// @flow
+import React, { Component } from 'react';
+import { translate } from 'react-i18next';
+import { Button } from 'semantic-ui-react';
+
+import GlobalModalBlockchainImport from '../../Modal/Blockchain/Import';
+
+class GlobalButtonBlockchainImport extends Component<Props> {
+  state = {
+    open: false
+  }
+
+  onOpen = () => this.setState({ open: true });
+  onClose = () => this.setState({ open: false });
+
+  render() {
+    const {
+      connection,
+      settings,
+      t
+    } = this.props;
+    const {
+      open
+    } = this.state;
+    return false;
+    return (
+      <GlobalModalBlockchainImport
+        connection={connection}
+        onClose={this.onClose}
+        open={open}
+        settings={settings}
+        trigger={(
+          <Button
+            color="blue"
+            content={t('global_button_blockchain_import_action')}
+            icon="circle plus"
+            onClick={this.onOpen}
+          />
+        )}
+      />
+    );
+  }
+}
+
+export default translate('global')(GlobalButtonBlockchainImport);
---
+++ app/shared/components/Global/Form/Field/Server.js
@@ -0,0 +1,43 @@
+// @flow
+import React, { Component } from 'react';
+import { I18n } from 'react-i18next';
+import { Form, Input } from 'semantic-ui-react';
+
+import debounce from 'lodash/debounce';
+
+export default class GlobalFormFieldServer extends Component<Props> {
+  state = { value: '' };
+  onChange = debounce((e, { name, value }) => {
+    const parsed = value;
+    this.setState({
+      value: parsed
+    }, () => {
+      this.props.onChange(e, { name, value: parsed });
+    });
+  }, 300)
+  render() {
+    const {
+      autoFocus,
+      disabled,
+      icon,
+      label,
+      loading,
+      name,
+      value
+    } = this.props;
+    return (
+      <Form.Field
+        autoFocus={autoFocus}
+        control={Input}
+        disabled={disabled}
+        fluid
+        icon={icon}
+        label={label}
+        loading={loading}
+        name={name}
+        onChange={this.onChange}
+        defaultValue={value}
+      />
+    );
+  }
+}
