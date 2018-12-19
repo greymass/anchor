1c264c9c37f871b1f0fee20b320a0bb36a3b1fe3
dafuga <danielfugere28@gmail.com>
2018-12-18 22:37:40 -0800

    chore: passing the account info to the wallet selector

--- app/shared/actions/blockchains.js
+++ app/shared/actions/blockchains.js
@@ -5,7 +5,7 @@ import { setSettings, setSettingWithValidation } from './settings';
 import { clearActionsCache } from './accounts';
 import { clearProducerInfo } from './producers';
 
-function swapBlockchain(chainId) {
+function swapBlockchain(chainId, account, authorization) {
   return (dispatch: () => void, getState) => {
     const { blockchains, settings } = getState();
     const blockchain = find(blockchains, { chainId });
