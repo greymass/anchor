985ce587dab2e5e0f7fd7a45a382f4d3342e40d4
dafuga <danielfugere28@gmail.com>
2018-12-19 19:28:10 -0800

    enhancement: more polishing of the duplication modal

--- app/shared/actions/wallets.js
+++ app/shared/actions/wallets.js
@@ -48,14 +48,12 @@ export function completeConvertToLedger(
   };
 }
 
-export function duplicateWallet(account, authorization, chainDuplicatingTo, chainDuplicatingFrom, successCallback) {
+export function duplicateWallet(account, authorization, chainDuplicatingTo, chainDuplicatingFrom) {
   return (dispatch: () => void, getState) => {
     const { wallets } = getState();
     const currentWallet = find(wallets, { chainId: chainDuplicatingFrom, authorization, account });
     const newWallet = Object.assign({}, currentWallet, { chainId: chainDuplicatingTo });
 
-
-    successCallback();
     return dispatch({
       type: types.ADD_WALLET,
       payload: newWallet
