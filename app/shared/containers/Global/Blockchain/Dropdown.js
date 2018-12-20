c9b84891247aa27ee17111aab1d9f5d48f9fc1f0
dafuga <danielfugere28@gmail.com>
2018-12-19 19:30:40 -0800

    revert: reverted changes to the global blockchain  dropdown

--- app/shared/containers/Global/Blockchain/Dropdown.js
+++ app/shared/containers/Global/Blockchain/Dropdown.js
@@ -65,6 +65,7 @@ class GlobalBlockchainDropdown extends Component<Props> {
             text: `${b.name} ${(b.testnet ? '(TESTNET)' : '')}`,
             value: b.chainId,
           },
+          b
         };
       });
     let icon = {
