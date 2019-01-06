9015d08bcfdf01812879368b2ab00586f51fff64
Aaron Cox <aaron@greymass.com>
2019-01-06 18:32:14 -0500

    Updated server field

--- app/shared/components/Global/Form/Field/Server.js
+++ app/shared/components/Global/Form/Field/Server.js
@@ -1,42 +1,46 @@
 // @flow
 import React, { Component } from 'react';
-import { I18n } from 'react-i18next';
 import { Form, Input } from 'semantic-ui-react';
-
 import debounce from 'lodash/debounce';
 
 export default class GlobalFormFieldServer extends Component<Props> {
-  state = { value: '' };
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.defaultValue
+    };
+  }
   onChange = debounce((e, { name, value }) => {
-    const parsed = value;
+    const valid = !!value.match(/^(http|https):\/\//);
     this.setState({
-      value: parsed
+      value
     }, () => {
-      this.props.onChange(e, { name, value: parsed });
+      this.props.onChange(e, { name, value, valid });
     });
   }, 300)
+
   render() {
     const {
       autoFocus,
-      disabled,
       icon,
       label,
       loading,
-      name,
-      value
+      name
     } = this.props;
+    const {
+      value
+    } = this.state;
+
     return (
       <Form.Field
         autoFocus={autoFocus}
         control={Input}
-        disabled={disabled}
-        fluid
+        defaultValue={value}
         icon={icon}
         label={label}
         loading={loading}
         name={name}
         onChange={this.onChange}
-        defaultValue={value}
       />
     );
   }
