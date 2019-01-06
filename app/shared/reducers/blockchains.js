a3fe939f8585ce95cd18271e5d4a7f8fbf161cf5
Aaron Cox <aaron@greymass.com>
2019-01-06 15:44:26 -0500

    linting

--- app/shared/reducers/blockchains.js
+++ app/shared/reducers/blockchains.js
@@ -2,100 +2,111 @@ import { partition, unionBy } from 'lodash';
 
 import * as types from '../actions/types';
 
-const initialState = [
+const knownChains = [
   {
-    "_id": "eos-mainnet",
-    "chainId": "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
-    "keyPrefix": "EOS",
-    "name": "EOS",
-    "node": "https://eos.greymass.com",
-    "supportedContracts": [
-      "customtokens",
-      "producerinfo",
-      "proposals",
-      "regproxyinfo"
+    _id: 'test',
+    chainId: 'ada376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+    keyPrefix: 'EOS',
+    name: 'EOS',
+    node: 'https://eos.greymass.com',
+    supportedContracts: [
+      'customtokens',
+      'producerinfo',
+      'proposals',
+      'regproxyinfo'
     ],
-    "symbol": "EOS",
-    "testnet": false
+    symbol: 'EOS',
+    testnet: false
   },
   {
-    "_id": "eos-testnet-jungle",
-    "chainId": "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",
-    "keyPrefix": "EOS",
-    "name": "Jungle (EOS)",
-    "node": "http://jungle.cryptolions.io:18888",
-    "supportedContracts": [
+    _id: 'eos-mainnet',
+    chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+    keyPrefix: 'EOS',
+    name: 'EOS',
+    node: 'https://eos.greymass.com',
+    supportedContracts: [
+      'customtokens',
+      'producerinfo',
+      'proposals',
+      'regproxyinfo'
     ],
-    "symbol": "EOS",
-    "testnet": true
+    symbol: 'EOS',
+    testnet: false
   },
   {
-    "_id": "eos-testnet-cryptokylin",
-    "chainId": "5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191",
-    "keyPrefix": "EOS",
-    "name": "CryptoKylin (EOS)",
-    "node": "https://kylin.eoscanada.com:443",
-    "supportedContracts": [
-    ],
-    "symbol": "EOS",
-    "testnet": true
+    _id: 'eos-testnet-jungle',
+    chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',
+    keyPrefix: 'EOS',
+    name: 'Jungle (EOS)',
+    node: 'http://jungle.cryptolions.io:18888',
+    supportedContracts: [],
+    symbol: 'EOS',
+    testnet: true
+  },
+  {
+    _id: 'eos-testnet-cryptokylin',
+    chainId: '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191',
+    keyPrefix: 'EOS',
+    name: 'CryptoKylin (EOS)',
+    node: 'https://kylin.eoscanada.com:443',
+    supportedContracts: [],
+    symbol: 'EOS',
+    testnet: true
   },
   // {
-  //   "_id": "eosforce-mainnet",
-  //   "chainId": "bd61ae3a031e8ef2f97ee3b0e62776d6d30d4833c8f7c1645c657b149151004b",
-  //   "keyPrefix": "EOS",
-  //   "name": "EOSForce",
-  //   "node": "https://w1.eosforce.cn",
-  //   "supportedContracts": [
+  //   _id: 'eosforce-mainnet',
+  //   chainId: 'bd61ae3a031e8ef2f97ee3b0e62776d6d30d4833c8f7c1645c657b149151004b',
+  //   keyPrefix: 'EOS',
+  //   name: 'EOSForce',
+  //   node: 'https://w1.eosforce.cn',
+  //   supportedContracts: [
   //   ],
-  //   "symbol": "EOS",
-  //   "testnet": false
+  //   symbol: 'EOS',
+  //   testnet: false
   // },
   {
-    "_id": "insights-mainnet",
-    "chainId": "b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664",
-    "keyPrefix": "EOS",
-    "name": "Insights",
-    "node": "https://ireland-history.insights.network",
-    "supportedContracts": [
-    ],
-    "symbol": "INSTAR",
-    "testnet": false
+    _id: 'insights-mainnet',
+    chainId: 'b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664',
+    keyPrefix: 'EOS',
+    name: 'Insights',
+    node: 'https://ireland-history.insights.network',
+    supportedContracts: [],
+    symbol: 'INSTAR',
+    testnet: false
   },
   {
-    "_id": "telos-mainnet",
-    "chainId": "4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11",
-    "keyPrefix": "EOS",
-    "name": "Telos",
-    "node": "https://telos.greymass.com",
-    "supportedContracts": [
-    ],
-    "symbol": "TLOS",
-    "testnet": false
+    _id: 'telos-mainnet',
+    chainId: '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11',
+    keyPrefix: 'EOS',
+    name: 'Telos',
+    node: 'https://telos.greymass.com',
+    supportedContracts: [],
+    symbol: 'TLOS',
+    testnet: false
   },
   {
-    "_id": "telos-testnet",
-    "chainId": "e17615decaecd202a365f4c029f206eee98511979de8a5756317e2469f2289e3",
-    "keyPrefix": "EOS",
-    "name": "Telos",
-    "node": "https://api.eos.miami:17441",
-    "supportedContracts": [
-    ],
-    "symbol": "TLOS",
-    "testnet": true
+    _id: 'telos-testnet',
+    chainId: 'e17615decaecd202a365f4c029f206eee98511979de8a5756317e2469f2289e3',
+    keyPrefix: 'EOS',
+    name: 'Telos',
+    node: 'https://api.eos.miami:17441',
+    supportedContracts: [],
+    symbol: 'TLOS',
+    testnet: true
   },
   {
-    "_id": "worbli-mainnet",
-    "chainId": "73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f",
-    "keyPrefix": "EOS",
-    "name": "Worbli",
-    "node": "https://api.worbli-mainnet.eoscalgary.io",
-    "supportedContracts": [
-    ],
-    "symbol": "WBI",
-    "testnet": false
+    _id: 'worbli-mainnet',
+    chainId: '73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f',
+    keyPrefix: 'EOS',
+    name: 'Worbli',
+    node: 'https://api.worbli-mainnet.eoscalgary.io',
+    supportedContracts: [],
+    symbol: 'WBI',
+    testnet: false
   }
 ];
+
+const initialState = [...knownChains];
 
 export default function blockchains(state = initialState, action) {
   switch (action.type) {
