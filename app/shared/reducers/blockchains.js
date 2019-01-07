156edf3332d838496529699eb7f6e2e03323ef47
Aaron Cox <aaron@greymass.com>
2019-01-07 14:02:55 -0500

    Conditional logic to save as default when validating

--- app/shared/reducers/blockchains.js
+++ app/shared/reducers/blockchains.js
@@ -207,6 +207,36 @@ export default function blockchains(state = initialState, action) {
       }
       return [modified, ...others];
     }
+    // Upon node validation for a given chain, check if it should be set as the default
+    case types.VALIDATE_NODE_SUCCESS: {
+      const { info, node, saveAsDefault } = action.payload;
+      // If saveAsDefault is false, ignore update
+      if (!saveAsDefault) {
+        return state;
+      }
+      const [existing, others] = partition(state, {
+        chainId: info.chain_id,
+      });
+      let modified;
+      if (existing.length) {
+        // If an existing entry for this chain exists, edit it
+        modified = Object.assign({}, existing[0]);
+        modified.node = node;
+      } else {
+        // Otherwise create a new entry for it
+        modified = {
+          _id: `unknown-${info.chain_id}`,
+          chainId: info.chain_id,
+          keyPrefix: 'EOS',
+          name: `Unknown (${info.chain_id.substr(0, 5)})`,
+          node,
+          supportedContracts: [],
+          symbol: 'EOS',
+          testnet: false
+        };
+      }
+      return [modified, ...others];
+    }
     default: {
       return state;
     }
