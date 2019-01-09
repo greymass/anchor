985e63d94cc1258643d4091163546c3f6ea6ed90
Hayrullah Behar <>
2019-01-09 09:09:55 +0100

    set/get exchange accounts by chainId

--- app/shared/constants/exchangeAccounts.js
+++ app/shared/constants/exchangeAccounts.js
@@ -1,7 +1,14 @@
-const exchangeAccounts = [
+const defaultExchangeAccounts = [
   'binancecleos',
   'bitfinexdep1',
   'krakenkraken'
 ];
 
-export default exchangeAccounts;
+// exchange accounts by chain id
+// if chain id not specified, default will be used 
+const exchangeAccounts = {
+  default: defaultExchangeAccounts,
+  '50f1cee2e3750f473e673049c1b828ec10e10eb96c7211a91cc2bd29ae94c6dd': []
+}
+
+export default (chainId = null) => chainId && exchangeAccounts.hasOwnProperty(chainId) ? exchangeAccounts[chainId] : exchangeAccounts.default;
