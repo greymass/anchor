e6d759ffb5d4b23bf4bfd7370fda8a2ac5f680de
Aaron Cox <aaron@greymass.com>
2019-01-10 02:01:19 -0500

    create prompt window and application

---
+++ app/modules/handler/containers/Prompt.js
@@ -0,0 +1,32 @@
+// @flow
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+class PromptContainer extends Component<Props> {
+  render() {
+    const {
+      settings
+    } = this.props;
+    return (
+      <span>Hello {settings.account}!</span>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    settings: state.settings
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+
+    }, dispatch)
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PromptContainer));
--- webpack.config.renderer.dev.js
+++ webpack.config.renderer.dev.js
@@ -46,6 +46,12 @@ export default merge.smart(baseConfig, {
       'webpack/hot/only-dev-server',
       path.join(__dirname, 'app/renderer/basic/index'),
     ],
+    handler: [
+      'react-hot-loader/patch',
+      `webpack-dev-server/client?http://localhost:${port}/`,
+      'webpack/hot/only-dev-server',
+      path.join(__dirname, 'app/renderer/handler/index'),
+    ],
   },
 
   output: {
