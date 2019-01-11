f8f4330f0f36ad236259dcd11313e9361d9cc4be
dafuga <danielfugere28@gmail.com>
2019-01-11 12:50:21 -0800

    enhancement: polished the  proposals table

--- app/shared/components/Tools/Governance/Table.js
+++ app/shared/components/Tools/Governance/Table.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { translate } from 'react-i18next';
-import { Table } from 'semantic-ui-react';
-import ToolsGovernanceProposalsProposal from './Proposals';
+import { Button, Table } from 'semantic-ui-react';
+import ToolsGovernanceProposalsProposal from './Proposals/Proposal';
 
 class ProposalsTable extends Component<Props> {
   state= { selectedProposal: null };
@@ -20,64 +20,68 @@ class ProposalsTable extends Component<Props> {
     const {
       selectedProposal
     } = this.state;
-    debugger
     return (
-      <Table>
+      <Table style={{ marginTop: 20 }}>
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>
-              {t('governance_proposals_title')}
+              {t('tools_proposals_title')}
             </Table.HeaderCell>
             <Table.HeaderCell>
-              {t('governance_proposals_name')}
+              {t('tools_proposals_expires')}
             </Table.HeaderCell>
             <Table.HeaderCell />
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {([].concat(list)
-            .map((proposal) => (
-              <React.Fragment>
-                <Table.Row>
-                  <Table.Cell>
-                    {proposal.title}
-                  </Table.Cell>
-                  <Table.Cell>
-                    {proposal.proposal_name}
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Button
-                      onClick={() => {
-                        this.setState({ selectedProposal: proposal.proposal_name })
-                      }}
-                      content={t('proposals_select_button')}
-                    />
-                  </Table.Cell>
-                </Table.Row>
-                {selectedProposal === proposal.proposal_name &&
-                (
+            .map((proposal) => {
+              const selected = selectedProposal === proposal.proposal_name;
+              return (
+                <React.Fragment>
                   <Table.Row>
-                    <Table.Cell colSpan='3'>
-                      <ToolsGovernanceProposalsProposal
-                        actions={actions}
-                        blockExplorers={blockExplorers}
-                        isLocked={isLocked}
-                        key={proposal.proposal_name}
-                        proposal={proposal}
-                        scope={scope}
-                        settings={settings}
-                        system={system}
-                        votes={votes}
+                    <Table.Cell>
+                      {proposal.title}
+                    </Table.Cell>
+                    <Table.Cell>
+                      {(new Date(proposal.expires_at)).toLocaleDateString('en-US')}
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Button
+                        onClick={() => {
+                          this.setState({
+                            selectedProposal: selected ? null : proposal.proposal_name
+                          });
+                        }}
+                        content={selected ? t('tools_proposals_hide_button') : t('tools_proposals_select_button')}
                       />
                     </Table.Cell>
                   </Table.Row>
-                )}
-              </React.Fragment>
-            )))}
+                  {selected &&
+                    (
+                      <Table.Row>
+                        <Table.Cell colSpan="3">
+                          <ToolsGovernanceProposalsProposal
+                            actions={actions}
+                            blockExplorers={blockExplorers}
+                            isLocked={isLocked}
+                            key={proposal.proposal_name}
+                            proposal={proposal}
+                            scope={scope}
+                            settings={settings}
+                            system={system}
+                            votes={votes}
+                          />
+                        </Table.Cell>
+                      </Table.Row>
+                    )}
+                </React.Fragment>
+              );
+            }))}
         </Table.Body>
       </Table>
     );
   }
 }
 
-export default translate(ProposalsTable);
+export default translate('tools')(ProposalsTable);
