3113ee9fe3f22b2a301951f02e87b4dca4a272ef
Aaron Cox <aaron@greymass.com>
2019-01-24 15:51:36 -0500

    Reworked new main module + navigation

---
+++ app/modules/main/containers/Sections/Overview.js
@@ -0,0 +1,54 @@
+// @flow
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { Placeholder, Segment } from 'semantic-ui-react';
+import { times } from 'lodash';
+
+class OverviewContainer extends Component<Props> {
+  render() {
+    const {
+      app
+    } = this.props;
+    console.log(app)
+    return (
+      <Segment>
+        overview
+        <Segment vertical>
+          {times(10, i => (
+            <Placeholder fluid key={i}>
+              <Placeholder.Header image>
+                <Placeholder.Line />
+                <Placeholder.Line />
+              </Placeholder.Header>
+              <Placeholder.Paragraph>
+                <Placeholder.Line />
+                <Placeholder.Line />
+                <Placeholder.Line />
+                <Placeholder.Line />
+              </Placeholder.Paragraph>
+            </Placeholder>
+          ))}
+        </Segment>
+      </Segment>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    app: state.app,
+    navigation: state.navigation
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+
+    }, dispatch)
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(OverviewContainer));
