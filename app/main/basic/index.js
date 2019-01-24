a738dd7767fa94f0f76a55775c3092be8ff2af87
Aaron Cox <aaron@greymass.com>
2019-01-24 15:48:18 -0500

    fixes to allow the old basic UI to still function

--- app/main/basic/index.js
+++ app/main/basic/index.js
@@ -1,9 +1,9 @@
 import { BrowserWindow } from 'electron';
-import MenuBuilder from '../menu';
+import MenuBuilder from '../../shared/electron/menu';
 import packageJson from '../../package.json';
 
-import { configureIPC } from '../shared/ipc';
-import { windowStateKeeper } from '../shared/windowStateKeeper';
+import { configureIPC } from '../../shared/electron/ipc';
+import { windowStateKeeper } from '../../shared/electron/windowStateKeeper';
 
 const log = require('electron-log');
 const path = require('path');
