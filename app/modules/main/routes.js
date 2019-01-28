c7d0b6b1ad8e8f86d4ccc136d5de303580662d75
Aaron Cox <aaron@greymass.com>
2019-01-28 11:17:45 -0500

    cleanup

--- app/modules/main/routes.js
+++ app/modules/main/routes.js
@@ -1,7 +1,6 @@
 /* eslint flowtype-errors/show-errors: 0 */
 import React from 'react';
 
-// import ColdWallet from '../../shared/containers/ColdWallet';
 import Wallet from '../../modules/main/containers/Wallet';
 
 export default () => <Wallet />;
