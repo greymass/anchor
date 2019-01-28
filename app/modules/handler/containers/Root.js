1e9e22527b17caf2db3acda200c717411fe1a33c
Aaron Cox <aaron@greymass.com>
2019-01-28 11:18:47 -0500

    more structural changes to the handler

--- app/modules/handler/containers/Root.js
+++ app/modules/handler/containers/Root.js
@@ -2,34 +2,26 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
-import { ConnectedRouter } from 'react-router-redux';
 
-import { configureStore, history } from '../../../shared/store/renderer/configureStore';
+import { configureStore } from '../../../shared/store/renderer/configureStore';
 import i18n from '../../../shared/i18n';
+import { setURI } from '../actions/uri';
+import Prompt from './Prompt';
 
 import '../../../shared/app.global.css';
 
 const { ipcRenderer } = require('electron');
-import { setURI } from '../actions/uri';
 
 const { store } = configureStore();
 
 export default class Root extends Component<Props> {
-  render() {
-    const Routes = this.props.routes;
-    return (
-      <I18nextProvider i18n={i18n}>
-        <Provider store={store}>
-          <ConnectedRouter history={history}>
-            <Routes />
-          </ConnectedRouter>
-        </Provider>
-      </I18nextProvider>
-    );
-  }
+  render = () => (
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        <Prompt />
+      </Provider>
+    </I18nextProvider>
+  );
 }
 
-ipcRenderer.on('openUri', (event, data) => {
-  console.log(event, data)
-  store.dispatch(setURI(data));
-});
+ipcRenderer.on('openUri', (event, data) => store.dispatch(setURI(data)));
