4126ec0148d6f258aec45e4b299a7d574fb3074f
Aaron Cox <aaron@greymass.com>
2019-01-28 11:21:08 -0500

    wallet fragment design

---
+++ app/shared/components/Global/Fragment/Wallet.js
@@ -0,0 +1,37 @@
+// @flow
+import React, { PureComponent } from 'react';
+import { Header } from 'semantic-ui-react';
+import { translate } from 'react-i18next';
+import Blockies from 'react-blockies';
+
+class GlobalFragmentWallet extends PureComponent<Props> {
+  render() {
+    const {
+      account,
+      authorization,
+      mode,
+      pubkey,
+    } = this.props;
+    return (
+      <Header
+        size="tiny"
+        style={{
+          margin: 0
+        }}
+      >
+        <Blockies
+          className="ui image"
+          seed={pubkey}
+        />
+        <Header.Content>
+          {account}
+          <Header.Subheader>
+            {authorization} / {mode}
+          </Header.Subheader>
+        </Header.Content>
+      </Header>
+    );
+  }
+}
+
+export default translate('global')(GlobalFragmentWallet);
