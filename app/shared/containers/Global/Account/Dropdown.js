5c59eaf00b34f73a6f49e6e829db26a6f44192b5
Aaron Cox <aaron@greymass.com>
2019-01-28 11:20:01 -0500

    Reworked account dropdown

--- app/shared/containers/Global/Account/Dropdown.js
+++ app/shared/containers/Global/Account/Dropdown.js
@@ -7,6 +7,7 @@ import compose from 'lodash/fp/compose';
 import { Button, Dropdown, Header, Icon, Input, Segment, Tab } from 'semantic-ui-react';
 
 import GlobalButtonElevate from '../Button/Elevate';
+import GlobalFragmentWallet from '../../../components/Global/Fragment/Wallet';
 import * as WalletActions from '../../../actions/wallet';
 import * as WalletsActions from '../../../actions/wallets';
 
@@ -31,6 +32,7 @@ class GlobalAccountDropdown extends Component<Props> {
   render() {
     const {
       settings,
+      style,
       t,
       validate,
       wallet,
@@ -48,133 +50,55 @@ class GlobalAccountDropdown extends Component<Props> {
         )
       ))
       .sort((a, b) => a.account > b.account)
-      .map((w) => {
-        let icon = {
-          color: 'green',
-          name: 'id card'
-        };
-        switch (w.mode) {
-          case 'cold': {
-            icon = {
-              color: 'blue',
-              name: 'snowflake'
-            };
-            break;
-          }
-          case 'ledger': {
-            icon = {
-              color: 'purple',
-              name: 'usb'
-            };
-            break;
-          }
-          case 'watch': {
-            icon = {
-              color: 'grey',
-              name: 'eye'
-            };
-            break;
-          }
-          case 'wait': {
-            icon = {
-              color: 'grey',
-              name: 'sync'
-            };
-            break;
-          }
-          default: {
-            // no default
-          }
-        }
-        return {
-          props: {
-            key: (w.authorization) ? `${w.account}@${w.authorization}` : w.account,
-            icon,
-            onClick: () => {
-              return (w.mode === 'watch' || w.mode === 'ledger') ? this.swapAccount(w.account, w.authorization) : false;
-            },
-            text: (w.authorization) ? `${w.account}@${w.authorization}` : `${w.account} (${t('global_accounts_dropdown_upgrade_required')})`,
-            value: `${w.account}@${w.authorization}`,
-          },
-          w
-        };
-      });
-    let currentName = (wallet.authorization)
-      ? `${wallet.account}@${wallet.authorization}`
-      : `${wallet.account}`;
-    let icon = {
-      color: 'green',
-      name: 'id card'
-    };
-    if (!wallet.mode) {
-      currentName = "Select an Account...";
-      icon = {
-        color: 'red',
-        name: 'x'
-      }
-    }
-    switch (wallet.mode) {
-      case 'cold': {
-        icon = {
-          color: 'blue',
-          name: 'snowflake'
-        };
-        break;
-      }
-      case 'ledger': {
-        icon = {
-          color: 'purple',
-          name: 'usb'
-        };
-        break;
-      }
-      case 'wait': {
-        icon = {
-          color: 'grey',
-          name: 'sync'
-        };
-        break;
-      }
-      case 'watch': {
-        icon = {
-          color: 'grey',
-          name: 'eye'
-        };
-        break;
-      }
-      default: {
-        // no default
-      }
-    }
     return (
       <Dropdown
         item
         labeled
+        style={style || {}}
         trigger={(
           <span>
-            <Icon color={icon.color} name={icon.name} />
-            {' '}
-            {currentName}
+            <GlobalFragmentWallet
+              account={wallet.account}
+              authorization={wallet.authorization}
+              mode={wallet.mode}
+              pubkey={wallet.pubkey}
+            />
           </span>
         )}
       >
         <Dropdown.Menu key="parent">
           <Dropdown.Menu key="menu" scrolling>
             {(options.length > 0)
-              ? options.map(option => {
-                const {
-                  props,
-                  w
-                } = option;
+              ? options.map(w => {
                 if (w.mode === 'watch' || w.mode === 'ledger') {
-                  return <Dropdown.Item {...props} />;
+                  return (
+                    <Dropdown.Item
+                      onClick={() => this.swapAccount(w.account, w.authorization)}
+                      key={`${w.account}@${w.authorization}`}
+                    >
+                      <GlobalFragmentWallet
+                        account={w.account}
+                        authorization={w.authorization}
+                        mode={w.mode}
+                        pubkey={w.pubkey}
+                      />
+                    </Dropdown.Item>
+                  );
                 }
                 return (
                   <GlobalButtonElevate
-                    key={props.key}
                     onSuccess={(password) => this.swapAccount(w.account, w.authorization, password)}
                     settings={settings}
-                    trigger={<Dropdown.Item {...props} />}
+                    trigger={(
+                      <Dropdown.Item>
+                        <GlobalFragmentWallet
+                          account={w.account}
+                          authorization={w.authorization}
+                          mode={w.mode}
+                          pubkey={w.pubkey}
+                        />
+                      </Dropdown.Item>
+                    )}
                     validate={validate}
                     wallet={w}
                   />
