90073dada4cb47759b56107e4d629089da686544
dafuga <danielfugere28@gmail.com>
2019-02-02 15:38:23 -0800

    chore: added an empty claimAirgrab action for now

---
+++ app/shared/actions/system/airgrab.js
@@ -0,0 +1,14 @@
+import { get, set } from 'dot-prop-immutable';
+
+import * as types from '../types';
+import eos from '../helpers/eos';
+
+export function claimAirgrab(data) {
+  return (dispatch: () => void, getState) => {
+    //   whatever code is needed..
+  };
+}
+
+export default {
+  claimAirgrab
+};
