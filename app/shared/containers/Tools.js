e1d2ad7e16da6b1ff99bd3cf10c93fd504f4e90d
dafuga <danielfugere28@gmail.com>
2019-02-09 13:58:35 -0800

    enhancement: design fixes of airgrab section

--- app/shared/containers/Tools.js
+++ app/shared/containers/Tools.js
@@ -56,6 +56,7 @@ import * as UnregProxyActions from '../actions/system/unregproxy';
 import * as UpdateAuthActions from '../actions/system/updateauth';
 import * as WalletActions from '../actions/wallet';
 import * as WalletsActions from '../actions/wallets';
+import * as AppActions from '../actions/app';
 
 const paneMapping = [
   {
@@ -373,6 +374,7 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators({
       ...AccountsActions,
+      ...AppActions,
       ...BidNameActions,
       ...ClaimAirgrabActions,
       ...ContractsActions,
