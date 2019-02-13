085de774a08f41d178b36b879cb62ad410acb251
Aaron Cox <aaron@greymass.com>
2019-02-12 19:21:22 -0500

    Added handler for GET_ACCOUNT_BALANCES_SUCCESS

--- app/shared/reducers/balances.js
+++ app/shared/reducers/balances.js
@@ -1,4 +1,6 @@
 import * as types from '../actions/types';
+import { get, set } from 'dot-prop-immutable';
+import { map, reduce } from 'lodash';
 
 const initialState = {
   __contracts: {
@@ -12,6 +14,27 @@ export default function balances(state = initialState, action) {
     case types.CLEAR_BALANCE_CACHE:
     case types.RESET_ALL_STATES: {
       return initialState;
+    }
+    case types.GET_ACCOUNT_BALANCES_SUCCESS: {
+      let modified;
+      const newBalances = Object.assign({}, state[action.payload.account], action.payload.tokens);
+      const newPrecisions = Object.assign({}, state.__contracts, reduce(
+        action.payload.results,
+        (res, { code, symbol }) => ({
+          ...res,
+          ...{
+            [symbol.toUpperCase()]: {
+              contract: code,
+              precision: {
+                [symbol]: get(action, `payload.precisions.${symbol}`, 4)
+              }
+            }
+          }
+        }), {}
+      ));
+      modified = set(state, action.payload.account, newBalances);
+      modified = set(modified, '__contracts', newPrecisions);
+      return modified;
     }
     case types.GET_ACCOUNT_BALANCE_SUCCESS: {
       const {
