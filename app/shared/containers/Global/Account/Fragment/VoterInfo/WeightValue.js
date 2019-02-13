82c0a74dd8784aa92caf6823ddd5857dd798279d
Aaron Cox <aaron@greymass.com>
2019-02-12 19:23:59 -0500

    Added overview section

---
+++ app/shared/containers/Global/Account/Fragment/VoterInfo/WeightValue.js
@@ -0,0 +1,46 @@
+// @flow
+import { get } from 'dot-prop-immutable';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { translate } from 'react-i18next';
+import compose from 'lodash/fp/compose';
+
+class GlobalAccountFragmentVoterInfoWeightValue extends PureComponent<Props> {
+  render() {
+    const {
+      weight,
+    } = this.props;
+    if (!weight) {
+      return (
+        <React.Fragment>
+          ~
+        </React.Fragment>
+      );
+    }
+    const epoch = 946684800;
+    const current = Math.round((new Date()).getTime() / 1000);
+    const doubleWeight = parseInt((current - epoch) / (24 * 60 * 60 * 7), 10) * parseFloat(1 / 52);
+    const value = (weight) / (2 ** doubleWeight);
+    return (
+      <React.Fragment>
+        {(value / 10000).toFixed(4)}
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  let voter = get(state, `accounts.${ownProps.account}.voter_info`, {});
+  // The get call above will return null as a retrieved value, and if so, set to {}
+  if (voter === null) {
+    voter = {};
+  }
+  return {
+    weight: voter.last_vote_weight
+  };
+};
+
+export default compose(
+  translate('global'),
+  connect(mapStateToProps)
+)(GlobalAccountFragmentVoterInfoWeightValue);
