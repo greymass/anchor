97b408d91ae7b463263c6b7c73d9a16aff92b669
Aaron Cox <aaron@greymass.com>
2019-02-12 19:26:08 -0500

    better monitoring of connection state

--- app/modules/main/containers/Wallet.js
+++ app/modules/main/containers/Wallet.js
@@ -3,35 +3,59 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Container, Image, Segment } from 'semantic-ui-react';
+import { Container, Dimmer, Header, Image, Loader, Segment } from 'semantic-ui-react';
 
 import ContentContainer from './Content';
 import MenuContainer from './Menu';
 import SidebarContainer from './Sidebar';
 import Notifications from '../../../shared/components/Notifications';
 import WelcomeContainer from '../../../shared/containers/Welcome';
+import * as ValidateActions from '../../../shared/actions/validate';
 
 import background from '../../../renderer/assets/images/geometric-background.svg';
 
 class WalletContainer extends Component<Props> {
+  constructor(props) {
+    super(props);
+    const { actions, connectionStatus, settings } = props;
+    if (connectionStatus !== 'SUCCESS' && settings.node) {
+      const { validateNode } = actions;
+      validateNode(settings.node, settings.chainId, false, true);
+      this.state = { initialized: false };
+    }
+  }
+  state = { initialized: true };
+  componentDidUpdate(prevProps) {
+    if (
+      !this.state.initialized
+      && this.props.connectionStatus !== prevProps.connectionStatus
+      && this.props.connectionStatus === 'SUCCESS'
+    ) {
+      this.setState({ initialized: true });
+    }
+  }
   render() {
     const {
-      actions,
-      app,
       settings,
-      validate,
-      wallet,
     } = this.props;
-    if (validate.NODE !== 'SUCCESS') {
+    const {
+      initialized
+    } = this.state;
+    if (!settings.walletInit) {
       return (
         <WelcomeContainer />
+      )
+    }
+    if (!initialized) {
+      return (
+        <span>Loading...</span>
       )
     }
     return (
       <React.Fragment>
         <div
           style={{
-            zIndex: 999,
+            zIndex: 1002,
             position: 'fixed',
             top: 0,
             left: 0,
@@ -41,6 +65,7 @@ class WalletContainer extends Component<Props> {
           <SidebarContainer />
         </div>
         <div
+          id="test-test"
           style={{
             flex: '1 1 auto',
             WebkitBoxFlex: 1,
@@ -73,13 +98,8 @@ class WalletContainer extends Component<Props> {
 
 function mapStateToProps(state) {
   return {
-    app: state.app,
-    actions: state.actions,
-    locked: state.locked,
-    prompt: state.prompt,
+    connectionStatus: state.validate.NODE,
     settings: state.settings,
-    system: state.system,
-    validate: state.validate,
     wallet: state.wallet,
   };
 }
@@ -87,7 +107,7 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators({
-
+      ...ValidateActions,
     }, dispatch)
   };
 }
