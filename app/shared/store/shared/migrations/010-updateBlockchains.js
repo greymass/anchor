4c49a2bbdb995fea1e6c2afffa256599b627daf9
Aaron Cox <aaron@greymass.com>
2019-02-20 13:49:37 -0500

    Fix for older schema change

--- app/shared/store/shared/migrations/010-updateBlockchains.js
+++ app/shared/store/shared/migrations/010-updateBlockchains.js
@@ -1,6 +1,9 @@
 import { uniq } from 'lodash';
 
 const update = (blockchains) => {
+  // If prior state had no array, return one
+  if (!blockchains) return [];
+  // Otherwise update it
   const newBlockchains = [];
   blockchains.forEach((blockchain) => {
     const chain = Object.assign({}, blockchain);
