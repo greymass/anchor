25286619e009784aefd397a4435ff1980de64138
Aaron Cox <aaron@greymass.com>
2019-03-03 13:39:14 -0500

    0.7.3 -> 0.7.4

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-eos-voter-0.7.3-amd64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.3-amd64.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.3-amd64.snap"
-# shasum -b -a 512 linux-eos-voter-0.7.3-amd64.snap
-echo "shasum -b -a 512 linux-eos-voter-0.7.3-arm64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.3-arm64.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.3-armv7l.deb"
-shasum -b -a 512 linux-eos-voter-0.7.3-armv7l.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.3-i386.deb"
-# shasum -b -a 512 linux-eos-voter-0.7.3-i386.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.3-x86_64.AppImage"
-shasum -b -a 512 linux-eos-voter-0.7.3-x86_64.AppImage
-echo "shasum -b -a 512 mac-eos-voter-0.7.3.dmg"
-shasum -b -a 512 mac-eos-voter-0.7.3.dmg
-echo "shasum -b -a 512 mac-eos-voter-0.7.3.zip"
-shasum -b -a 512 mac-eos-voter-0.7.3.zip
-echo "shasum -b -a 512 win-eos-voter-0.7.3.exe"
-shasum -b -a 512 win-eos-voter-0.7.3.exe
+echo "shasum -b -a 512 linux-eos-voter-0.7.4-amd64.deb"
+shasum -b -a 512 linux-eos-voter-0.7.4-amd64.deb
+# echo "shasum -b -a 512 linux-eos-voter-0.7.4-amd64.snap"
+# shasum -b -a 512 linux-eos-voter-0.7.4-amd64.snap
+echo "shasum -b -a 512 linux-eos-voter-0.7.4-arm64.deb"
+shasum -b -a 512 linux-eos-voter-0.7.4-arm64.deb
+echo "shasum -b -a 512 linux-eos-voter-0.7.4-armv7l.deb"
+shasum -b -a 512 linux-eos-voter-0.7.4-armv7l.deb
+# echo "shasum -b -a 512 linux-eos-voter-0.7.4-i386.deb"
+# shasum -b -a 512 linux-eos-voter-0.7.4-i386.deb
+echo "shasum -b -a 512 linux-eos-voter-0.7.4-x86_64.AppImage"
+shasum -b -a 512 linux-eos-voter-0.7.4-x86_64.AppImage
+echo "shasum -b -a 512 mac-eos-voter-0.7.4.dmg"
+shasum -b -a 512 mac-eos-voter-0.7.4.dmg
+echo "shasum -b -a 512 mac-eos-voter-0.7.4.zip"
+shasum -b -a 512 mac-eos-voter-0.7.4.zip
+echo "shasum -b -a 512 win-eos-voter-0.7.4.exe"
+shasum -b -a 512 win-eos-voter-0.7.4.exe
 
 cd ..
