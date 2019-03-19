2a4d6838e55fbb5c19cae8921c0bba739eecfaa5
motrebski <mateuszotrebski1z@gmail.com>
2019-03-19 09:58:25 +0100

    BEOS changes for mainnet release

--- app/shared/actions/chain.js
+++ app/shared/actions/chain.js
@@ -11,7 +11,8 @@ export function getInfo() {
 
     eos(connection).getInfo(true).then((chain) => {
       /* Get distribution period info if we are in BEOS blockchain */
-      if (chain.chain_id === 'b912d19a6abd2b1b05611ae5be473355d64d95aeff0c09bedc8c166cd6468fe4') {
+      if ((chain.chain_id === 'b912d19a6abd2b1b05611ae5be473355d64d95aeff0c09bedc8c166cd6468fe4') ||
+          (chain.chain_id === 'cbef47b0b26d2b8407ec6a6f91284100ec32d288a39d4b4bbd49655f7c484112')) {
         dispatch(getDistributionInfo());
       } else if (previousChain && previousChain.hasOwnProperty('distributionPeriodInfo')) {
         dispatch({ type: types.REMOVE_DISTRIBUTION_PERIOD_INFO })
