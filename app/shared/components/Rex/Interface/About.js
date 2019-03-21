23765952b7fe564f0e3c9d4dd34ae13ad3a3dde4
dafuga <danielfugere28@gmail.com>
2019-03-20 20:39:58 -0700

    feature: added fund, manage and rent rex interfaces

---
+++ app/shared/components/Rex/Interface/About.js
@@ -0,0 +1,27 @@
+// @flow
+import React, { PureComponent } from 'react';
+import { translate } from 'react-i18next';
+import {
+  Message,
+  Segment,
+} from 'semantic-ui-react';
+
+class RexInterfaceAbout extends PureComponent<Props> {
+  render() {
+    const {
+      t
+    } = this.props;
+
+    return (
+      <Segment basic>
+        <Message
+          warning
+        >
+          {t('rex_interface_about_message')}
+        </Message
+      </Segment>
+    );
+  }
+}
+
+export default translate('rex')(RexInterfaceAbout);
