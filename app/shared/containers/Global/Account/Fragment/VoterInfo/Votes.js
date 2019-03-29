99259ea1ad419e15caad5261534df8408d06523d
Aaron Cox <aaron@greymass.com>
2019-03-29 15:40:31 -0400

    Add popup with all votes around vote count

--- app/shared/containers/Global/Account/Fragment/VoterInfo/Votes.js
+++ app/shared/containers/Global/Account/Fragment/VoterInfo/Votes.js
@@ -4,15 +4,28 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 import compose from 'lodash/fp/compose';
+import { Popup } from 'semantic-ui-react';
 
 class GlobalAccountFragmentVoterInfoVotes extends PureComponent<Props> {
   render() {
     const {
+      producers,
       votes,
     } = this.props;
+    const sorted = producers.sort();
+    if (votes === 0) return votes;
     return (
       <React.Fragment>
-        {votes}
+        <Popup
+          content={
+            <span>
+              {sorted.join(', ')}
+            </span>
+          }
+          hoverable
+          position="right center"
+          trigger={(<div>{votes}</div>)}
+        />
       </React.Fragment>
     );
   }
@@ -22,16 +35,20 @@ class GlobalAccountFragmentVoterInfoVotes extends PureComponent<Props> {
 const mapStateToProps = (state, ownProps) => {
   let proxy;
   let voter = get(state, `accounts.${ownProps.account}.voter_info`, {});
+  let { producers } = voter;
   // The get call above will return null as a retrieved value, and if so, set to {}
   if (voter === null) {
     voter = {};
   }
+  // Overwrite local account if proxy is set
   if (voter.proxy) {
     ({ proxy } = voter);
     voter = get(state, `accounts.${voter.proxy}.voter_info`, {});
+    ({ producers } = voter);
   }
   const votes = get(voter, 'producers', []).length;
   return ({
+    producers,
     proxy,
     votes,
   });
