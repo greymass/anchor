92885189acf1683d8272ef21f8540d1f38158112
Aaron Cox <aaron@greymass.com>
2019-04-02 00:51:39 -0400

    version bump

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-eos-voter-0.7.4-amd64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.4-amd64.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.4-amd64.snap"
-# shasum -b -a 512 linux-eos-voter-0.7.4-amd64.snap
-echo "shasum -b -a 512 linux-eos-voter-0.7.4-arm64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.4-arm64.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.4-armv7l.deb"
-shasum -b -a 512 linux-eos-voter-0.7.4-armv7l.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.4-i386.deb"
-# shasum -b -a 512 linux-eos-voter-0.7.4-i386.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.4-x86_64.AppImage"
-shasum -b -a 512 linux-eos-voter-0.7.4-x86_64.AppImage
-echo "shasum -b -a 512 mac-eos-voter-0.7.4.dmg"
-shasum -b -a 512 mac-eos-voter-0.7.4.dmg
-echo "shasum -b -a 512 mac-eos-voter-0.7.4.zip"
-shasum -b -a 512 mac-eos-voter-0.7.4.zip
-echo "shasum -b -a 512 win-eos-voter-0.7.4.exe"
-shasum -b -a 512 win-eos-voter-0.7.4.exe
+echo "shasum -b -a 512 linux-eos-voter-0.7.5-amd64.deb"
+shasum -b -a 512 linux-eos-voter-0.7.5-amd64.deb
+# echo "shasum -b -a 512 linux-eos-voter-0.7.5-amd64.snap"
+# shasum -b -a 512 linux-eos-voter-0.7.5-amd64.snap
+echo "shasum -b -a 512 linux-eos-voter-0.7.5-arm64.deb"
+shasum -b -a 512 linux-eos-voter-0.7.5-arm64.deb
+echo "shasum -b -a 512 linux-eos-voter-0.7.5-armv7l.deb"
+shasum -b -a 512 linux-eos-voter-0.7.5-armv7l.deb
+# echo "shasum -b -a 512 linux-eos-voter-0.7.5-i386.deb"
+# shasum -b -a 512 linux-eos-voter-0.7.5-i386.deb
+echo "shasum -b -a 512 linux-eos-voter-0.7.5-x86_64.AppImage"
+shasum -b -a 512 linux-eos-voter-0.7.5-x86_64.AppImage
+echo "shasum -b -a 512 mac-eos-voter-0.7.5.dmg"
+shasum -b -a 512 mac-eos-voter-0.7.5.dmg
+echo "shasum -b -a 512 mac-eos-voter-0.7.5.zip"
+shasum -b -a 512 mac-eos-voter-0.7.5.zip
+echo "shasum -b -a 512 win-eos-voter-0.7.5.exe"
+shasum -b -a 512 win-eos-voter-0.7.5.exe
 
 cd ..
