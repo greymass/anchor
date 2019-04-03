f0ac25884eb85ec366d654417a8a8951c73ed271
dafuga <danielfugere28@gmail.com>
2019-04-03 19:52:50 +0800

    refactor: moved the permissions section into it's own container

---
+++ app/modules/main/containers/Sections/Tools/Permissions.js
@@ -0,0 +1,40 @@
+// @flow
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import ToolsPermissionsComponent from '../../../../../shared/components/Tools/Permissions';
+
+import * as AuthsActions from '../../../../../shared/actions/system/updateauth';
+
+class ToolsPermissions extends Component<Props> {
+  render = () => (
+    <ToolsPermissionsComponent
+      {...this.props}
+    />
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    accounts: state.accounts,
+    blockExplorers: state.blockExplorers,
+    connection: state.connection,
+    keys: state.keys,
+    settings: state.settings,
+    system: state.system,
+    validate: state.validate,
+    wallet: state.wallet
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      ...AuthsActions
+    }, dispatch)
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ToolsPermissions));
