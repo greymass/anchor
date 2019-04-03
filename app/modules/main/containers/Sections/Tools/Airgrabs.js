3183a13f0731e950550a1361ffd1dc46f3c28ad3
dafuga <danielfugere28@gmail.com>
2019-04-03 17:37:54 +0800

    refactor: moved the airgrabs tool to a container

---
+++ app/modules/main/containers/Sections/Tools/Airgrabs.js
@@ -0,0 +1,43 @@
+// @flow
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import ToolsAirgrabsComponent from '../../../../../shared/components/Tools/Airgrabs';
+
+import * as AirgrabActions from '../../../../../shared/actions/system/claimairgrab';
+import * as SystemStateActions from '../../../../../shared/actions/system/systemstate';
+
+class ToolsAirgrabs extends Component<Props> {
+  render = () => (
+    <ToolsAirgrabsComponent
+      {...this.props}
+    />
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    app: state.app,
+    accounts: state.accounts,
+    blockExplorers: state.blockexplorers,
+    keys: state.keys,
+    settings: state.settings,
+    system: state.system,
+    tables: state.tables,
+    validate: state.validate,
+    wallet: state.wallet
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      ...AirgrabActions,
+      ...SystemStateActions
+    }, dispatch)
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ToolsAirgrabs));
