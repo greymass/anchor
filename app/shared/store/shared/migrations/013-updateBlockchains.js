7fc67eaa4c278c1d9ce4101eb96d0c871aefd963
Aaron Cox <aaron@greymass.com>
2019-04-03 10:43:23 -0400

    Force skip beos-mainnet (to recreate) and relabel interal IDs of testnets

--- app/shared/store/shared/migrations/013-updateBlockchains.js
+++ app/shared/store/shared/migrations/013-updateBlockchains.js
@@ -3,13 +3,19 @@ const update = (blockchains) => {
 
   blockchains.forEach((blockchain) => {
     const chain = Object.assign({}, blockchain);
-
     if (chain.chainId === 'b912d19a6abd2b1b05611ae5be473355d64d95aeff0c09bedc8c166cd6468fe4') {
+      chain._id = 'beos-testnet-2';
       chain.testnet = true;
       chain.node = 'https://api.testnet.beos.world';
     }
-
-    newBlockchains.push(chain);
+    if (chain.chainId === '50f1cee2e3750f473e673049c1b828ec10e10eb96c7211a91cc2bd29ae94c6dd') {
+      chain.testnet = true;
+      chain._id = 'beos-testnet-1';
+    }
+    // Exclude any old beos-mainnet entry, to allow for recreation
+    if (chain._id !== 'beos-mainnet') {
+      newBlockchains.push(chain);
+    }
   });
 
   return newBlockchains;
