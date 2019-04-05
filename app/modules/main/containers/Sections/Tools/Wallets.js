c03e00c5370faf47cacb272bae5fdb24a13f3398
dafuga <danielfugere28@gmail.com>
2019-04-05 16:13:45 +0800

    refactor: moved wallets section into it's own container

---
+++ app/modules/main/containers/Sections/Tools/Wallets.js
@@ -0,0 +1,40 @@
+// @flow
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import ToolsWalletsComponent from '../../../../../shared/components/Tools/Wallets';
+
+import * as WalletsActions from '../../../../../shared/actions/wallets';
+
+class ToolsPermissions extends Component<Props> {
+  render = () => (
+    <ToolsWalletsComponent
+      {...this.props}
+    />
+  )
+}
+
+function mapStateToProps(state) {
+  return {
+    blockchains: state.blockchains,
+    connection: state.connection,
+    settings: state.settings,
+    status: state.status,
+    system: state.system,
+    validate: state.validate,
+    wallet: state.wallet,
+    wallets: state.wallets
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      ...WalletsActions
+    }, dispatch)
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ToolsPermissions));
