33a4c3098807821ff3546e95e8ba7e5b5b283fb0
dafuga <danielfugere28@gmail.com>
2019-04-06 14:29:15 +0800

    enhancement: added all tools to tools home page

--- app/modules/main/containers/Sections/Tools/SmartContracts.js
+++ app/modules/main/containers/Sections/Tools/SmartContracts.js
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { mapValues } from 'lodash';
 
 import ToolsSmartContractComponent from '../../../../../shared/components/Contract/Interface/Component';
 
 import * as ContractActions from '../../../../../shared/actions/contracts';
 import * as TableActions from '../../../../../shared/actions/table';
+
+import EOSContract from '../../../../../shared/utils/EOS/Contract';
 
 class ToolsPermissions extends Component<Props> {
   render = () => (
@@ -18,9 +21,12 @@ class ToolsPermissions extends Component<Props> {
 }
 
 function mapStateToProps(state) {
+  // Wrap all contracts in the associated helper
+  const contracts = mapValues(state.contracts, (contract) =>
+    new EOSContract(contract.abi, contract.account_name));
   return {
-    blockExplorers: state.blockExplorers,
-    contracts: state.contracts,
+    blockExplorers: state.blockexplorers[state.connection.chainKey],
+    contracts,
     settings: state.settings,
     system: state.system,
     tables: state.tables,
