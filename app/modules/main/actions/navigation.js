56e4bd5a1394423b842f28fe12531e627c4d380e
dafuga <danielfugere28@gmail.com>
2019-04-08 11:50:41 +0800

    enhancemnt: added loading screen on tools sections

--- app/modules/main/actions/navigation.js
+++ app/modules/main/actions/navigation.js
@@ -4,15 +4,24 @@ import * as types from '../../../shared/actions/types';
 
 export function changeModule(module) {
   return (dispatch: () => void) => {
-    console.log('test')
     dispatch(push(`/${module}`));
     return dispatch({
       type: types.NAVIGATION_CHANGE_MODULE,
-      payload: { module }
+      payload: { module, loading: true }
+    });
+  };
+}
+
+export function moduleLoaded() {
+  return (dispatch: () => void) => {
+    dispatch(push(`/${module}`));
+    return dispatch({
+      type: types.NAVIGATION_LOADING_ENDED,
     });
   };
 }
 
 export default {
-  changeModule
+  changeModule,
+  moduleLoaded
 };
--- app/modules/main/containers/Sections/Tools/Recommendations.js
+++ app/modules/main/containers/Sections/Tools/Recommendations.js
@@ -1,11 +1,20 @@
 // @flow
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 import RecommendationInterfaceList from '../../../../../shared/components/Recommendation/Interface/List';
 
+import * as NavigationActions from '../../../actions/navigation';
+
 class ToolsRecommendations extends Component<Props> {
+  componentDidMount() {
+    const { actions } = this.props;
+
+    actions.moduleLoaded();
+  }
+
   render = () => (
     <RecommendationInterfaceList
       {...this.props}
@@ -20,4 +29,12 @@ function mapStateToProps(state) {
   };
 }
 
-export default withRouter(connect(mapStateToProps)(ToolsRecommendations));
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      ...NavigationActions
+    }, dispatch)
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ToolsRecommendations));
