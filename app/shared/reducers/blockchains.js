f384e5a8e61fae7f902335d37fc384a93ad45ef5
Aaron Cox <aaron@greymass.com>
2019-04-11 14:43:09 -0400

    Fixed BOS keyPrefix (needs a migration built to fix existing users)

--- app/shared/reducers/blockchains.js
+++ app/shared/reducers/blockchains.js
@@ -36,7 +36,7 @@ const knownChains = [
   {
     _id: 'bos-mainnet',
     chainId: 'd5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86',
-    keyPrefix: 'BOS',
+    keyPrefix: 'EOS',
     name: 'BOS',
     node: 'https://hapi.bos.eosrio.io',
     supportedContracts: [
