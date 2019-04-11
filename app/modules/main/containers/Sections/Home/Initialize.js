e0b7bf7f27ceb6b8f6e612df8e97341b8b3923c9
Aaron Cox <aaron@greymass.com>
2019-04-11 14:41:53 -0400

    Moved from switching logic to routes

--- app/modules/main/containers/Sections/Home/Initialize.js
+++ app/modules/main/containers/Sections/Home/Initialize.js
@@ -4,27 +4,33 @@ import { bindActionCreators } from 'redux';
 import { translate } from 'react-i18next';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Button, Header, Image, Segment, Tab } from 'semantic-ui-react';
+import { Button, Image, Segment } from 'semantic-ui-react';
 import compose from 'lodash/fp/compose';
 
 import { setSetting } from '../../../../../shared/actions/settings';
 import { clearValidationState } from '../../../../../shared/actions/validate';
 import { setWalletMode } from '../../../../../shared/actions/wallet';
 
-import GlobalModalAccountImportCold from '../../../../../shared/containers/Global/Account/Import/Cold';
-import GlobalModalAccountImportHot from '../../../../../shared/containers/Global/Account/Import/Hot';
-import GlobalModalAccountImportLedger from '../../../../../shared/containers/Global/Account/Import/Ledger';
-import GlobalModalAccountImportWatch from '../../../../../shared/containers/Global/Account/Import/Watch';
 import WelcomeImportContainer from '../../../../../shared/containers/Welcome/Import';
 
 import anchorLogo from '../../../../../renderer/assets/images/anchor-logo.svg';
 import anchorText from '../../../../../renderer/assets/images/anchor-text.svg';
 
 class HomeInitializeContainer extends Component<Props> {
-  initialize = () => this.props.actions.setSetting('walletInit', true)
+  initialize = (e) => {
+    const {
+      actions,
+      history,
+    } = this.props;
+    actions.setSetting('walletInit', true);
+    e.preventDefault();
+    history.push('/home/blockchains');
+    return false;
+  }
   useColdWallet = (e) => {
     const {
       actions,
+      history,
     } = this.props;
     // Immediately set the wallet into cold storage mode
     actions.setWalletMode('cold');
@@ -33,12 +39,12 @@ class HomeInitializeContainer extends Component<Props> {
     // Initialize the wallet
     actions.setSetting('walletInit', true);
     e.preventDefault();
+    history.push('/home/blockchains');
     return false;
   }
 
   render() {
     const {
-      settings,
       t,
     } = this.props;
     return (
