d02092bc1abe2ae44df3b06d6e6c77327c4df06b
Aaron Cox <aaron@greymass.com>
2019-04-26 19:20:39 -0400

    Added a warning to low resources

--- app/shared/containers/Global/Account/Fragment/ResourcePercent.js
+++ app/shared/containers/Global/Account/Fragment/ResourcePercent.js
@@ -4,6 +4,7 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 import compose from 'lodash/fp/compose';
+import { Icon, Popup } from 'semantic-ui-react';
 
 class GlobalAccountFragmentResourcePercent extends PureComponent<Props> {
   render() {
@@ -11,9 +12,27 @@ class GlobalAccountFragmentResourcePercent extends PureComponent<Props> {
       resource,
     } = this.props;
     if (!resource) return false;
+    const percent = ((resource.available / resource.max) * 100).toFixed(2);
     return (
       <React.Fragment>
-        {((resource.available / resource.max) * 100).toFixed(1)}%
+        {(percent < 5)
+          ? (
+            <Popup
+              content="This specific resource is currently running low. Consider staking more in order to increase the account resources to avoid any potential interruptions in its actions."
+              trigger={(
+                <span>
+                  <Icon color="yellow" name="warning sign" />
+                  {percent}%
+                </span>
+              )}
+            />
+          )
+          : (
+            <span>
+              {percent}%
+            </span>
+          )
+        }
       </React.Fragment>
     );
   }
