db5baf72505d0a972372a07c187f92d086fcaa7a
Aaron Cox <aaron@greymass.com>
2019-04-26 19:25:48 -0400

    moved keystore into it's own property on the state

--- app/shared/reducers/auths.js
+++ app/shared/reducers/auths.js
@@ -1,28 +1,33 @@
+import { get, set } from 'dot-prop-immutable';
 import { partition } from 'lodash';
 import * as types from '../actions/types';
 
-const initialState = [];
+const initialState = {
+  keystore: [],
+};
 
 export default function auths(state = initialState, action) {
   switch (action.type) {
     case types.RESET_ALL_STATES:
     case types.WALLET_LOCK: {
-      return [...initialState];
+      return Object.assign({}, initialState);
     }
     case types.SET_AUTH:
     case types.SET_CURRENT_KEY: {
       const partitionQuery = {
         pubkey: action.payload.pubkey
       };
-      const [, other] = partition(state, partitionQuery);
-      return [
-        {
-          hash: action.payload.hash,
-          key: action.payload.key,
-          pubkey: action.payload.pubkey,
-        },
-        ...other
-      ];
+      const [, other] = partition(state.keystore, partitionQuery);
+      return Object.assign({}, state, {
+        keystore: [
+          {
+            hash: action.payload.hash,
+            key: action.payload.key,
+            pubkey: action.payload.pubkey,
+          },
+          ...other
+        ]
+      });
     }
     default: {
       return state;
