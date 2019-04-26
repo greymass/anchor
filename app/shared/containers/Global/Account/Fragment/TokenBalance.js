3bda65a43f9cc6d151a710cff9889fe9d855e1a1
Aaron Cox <aaron@greymass.com>
2019-04-26 19:20:59 -0400

    Added number formatters to balance components

--- app/shared/containers/Global/Account/Fragment/TokenBalance.js
+++ app/shared/containers/Global/Account/Fragment/TokenBalance.js
@@ -4,24 +4,28 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 import compose from 'lodash/fp/compose';
+import { Icon } from 'semantic-ui-react';
 
 class GlobalAccountFragmentTokenBalance extends PureComponent<Props> {
   render() {
     const {
       balance,
+      lng,
       precision,
     } = this.props;
+    if (balance === false) return <Icon name="clock" />;
+    const formatter = new Intl.NumberFormat(lng, { minimumFractionDigits: precision });
     return (
       <React.Fragment>
-        {balance && balance.toFixed(precision)}
+        {formatter.format(balance.toFixed(precision))}
       </React.Fragment>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  balance: get(state, `balances.${ownProps.account}.${ownProps.token}`, 0),
-  precision: get(state, `balances.__contracts.${ownProps.token}.precision.${ownProps.token}`, 4),
+  balance: get(state, `balances.${ownProps.account}.${ownProps.token}`, false),
+  precision: get(state, `balances.__contracts.${ownProps.token}.precision.${ownProps.token}`, false),
 });
 
 export default compose(
--- app/shared/containers/Global/Account/Fragment/TokenDelegated.js
+++ app/shared/containers/Global/Account/Fragment/TokenDelegated.js
@@ -4,22 +4,26 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 import compose from 'lodash/fp/compose';
+import { Icon } from 'semantic-ui-react';
 
 class GlobalAccountFragmentTokenDelegated extends PureComponent<Props> {
   render() {
     const {
-      balance
+      balance,
+      lng,
     } = this.props;
+    if (balance === false) return <Icon name="clock" />;
+    const formatter = new Intl.NumberFormat(lng, { minimumFractionDigits: 4 });
     return (
       <React.Fragment>
-        {balance.toFixed(4)}
+        {formatter.format(balance.toFixed(4))}
       </React.Fragment>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  balance: get(state, `accounts.${ownProps.account}.delegated.total`, 0)
+  balance: get(state, `accounts.${ownProps.account}.delegated.total`, false)
 });
 
 export default compose(
