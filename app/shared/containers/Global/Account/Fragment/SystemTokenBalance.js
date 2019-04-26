05281e657ee11a3716db198f3a5bd79d27a8d37b
Aaron Cox <aaron@greymass.com>
2019-04-26 19:20:59 -0400

    Added number formatters to balance components

--- app/shared/containers/Global/Account/Fragment/SystemTokenBalance.js
+++ app/shared/containers/Global/Account/Fragment/SystemTokenBalance.js
@@ -4,28 +4,36 @@ import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 import compose from 'lodash/fp/compose';
+import { Icon } from 'semantic-ui-react';
 
 class GlobalAccountFragmentSystemTokenBalance extends PureComponent<Props> {
   render() {
-    let {
-      balance
+    const {
+      lng
     } = this.props;
-    if (!balance) {
-      balance = 0;
-    }
+    let {
+      balance,
+    } = this.props;
+    if (balance === false) return <Icon name="clock" />;
+    const formatter = new Intl.NumberFormat(lng, { minimumFractionDigits: 4 });
     return (
       <React.Fragment>
-        {balance.toFixed(4)}
+        {formatter.format(balance.toFixed(4))}
       </React.Fragment>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const liquid = get(state, `balances.${ownProps.account}.${ownProps.token}`, 0);
+  const liquid = get(state, `balances.${ownProps.account}.${ownProps.token}`, false);
   const path = `accounts.${ownProps.account}.self_delegated_bandwidth`;
-  const cpuWeight = String(get(state, `${path}.cpu_weight`, '0.0000 TOKEN'))
-  const netWeight = String(get(state, `${path}.cpu_weight`, '0.0000 TOKEN'))
+  const cpuWeight = String(get(state, `${path}.cpu_weight`, false));
+  const netWeight = String(get(state, `${path}.net_weight`, false));
+  if (liquid === false || cpuWeight === false || netWeight === false) {
+    return ({
+      balance: false
+    });
+  }
   const cpu = parseFloat(cpuWeight.split(' ')[0]);
   const net = parseFloat(netWeight.split(' ')[0]);
   const delegated = get(state, `accounts.${ownProps.account}.delegated.total`, 0);
