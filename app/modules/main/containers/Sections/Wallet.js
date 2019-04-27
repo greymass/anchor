3ddeb865cdc3141116e7189b273456e80c0da492
Aaron Cox <aaron@greymass.com>
2019-04-27 18:38:06 -0400

    Started laying out wallet sections

--- app/modules/main/containers/Sections/Wallet.js
+++ app/modules/main/containers/Sections/Wallet.js
@@ -2,8 +2,6 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Placeholder, Segment } from 'semantic-ui-react';
-import { times } from 'lodash';
 import {
   HashRouter,
   Route,
@@ -11,73 +9,36 @@ import {
   withRouter
 } from 'react-router-dom';
 
-
-// import Wallet from '../../../../shared/components/Wallet';
-import WalletHomeContainer from './Wallet/Home';
-
-// import * as AccountsActions from '../../../../shared/actions/accounts';
-// import * as AppActions from '../../../../shared/actions/app';
-// import * as BlockExplorersActions from '../../../../shared/actions/blockexplorers';
-// import * as BuyRamBytesActions from '../../../../shared/actions/system/buyrambytes';
-// import * as BuyRamActions from '../../../../shared/actions/system/buyram';
-// import * as ChainActions from '../../../../shared/actions/chain';
-// import * as ConnectionActions from '../../../../shared/actions/connection';
-// import * as ContractsActions from '../../../../shared/actions/contracts';
-// import * as CreateAccountActions from '../../../../shared/actions/createaccount';
-// import * as GlobalsActions from '../../../../shared/actions/globals';
-// import * as ProducersActions from '../../../../shared/actions/producers';
-// import * as ProposalsActions from '../../../../shared/actions/governance/proposals';
-// import * as SellRamActions from '../../../../shared/actions/system/sellram';
-// import * as SettingsActions from '../../../../shared/actions/settings';
-// import * as StakeActions from '../../../../shared/actions/stake';
-// import * as TableActions from '../../../../shared/actions/table';
-// import * as TransactionActions from '../../../../shared/actions/transaction';
-// import * as TransferActions from '../../../../shared/actions/transfer';
-// import * as ValidateActions from '../../../../shared/actions/validate';
-// import * as VoteProducerActions from '../../../../shared/actions/system/voteproducer';
-// import * as WalletActions from '../../../../shared/actions/wallet';
-// import * as SystemStateActions from '../../../../shared/actions/system/systemstate';
-// import * as BEOSWithdrawActions from '../../../../shared/actions/blockchains/beos/withdraw';
+import WalletStakeContainer from './Wallet/Stake';
+import WalletTransferContainer from './Wallet/Transfer';
+import NavigationWalletContainer from '../Navigation/Wallet';
 
 class WalletContainer extends Component<Props> {
   render() {
     return (
-      <HashRouter>
-        <Switch>
-          <Route exact path="/wallet" component={WalletHomeContainer} />
-        </Switch>
-      </HashRouter>
+      <React.Fragment>
+        <NavigationWalletContainer />
+        <HashRouter>
+          <Switch>
+            <Route exact path="/wallet" component={WalletTransferContainer} />
+            <Route path="/wallet/stake" component={WalletStakeContainer} />
+          </Switch>
+        </HashRouter>
+      </React.Fragment>
     );
   }
 }
 
 function mapStateToProps(state) {
   return {
-    // accounts: state.accounts,
-    // actionHistories: state.actions,
-    // balances: state.balances,
-    // allBlockExplorers: state.blockexplorers,
-    // connection: state.connection,
-    // keys: state.keys,
-    // settings: state.settings,
-    // system: state.system,
-    // transaction: state.transaction,
-    // validate: state.validate,
-    // wallet: state.wallet,
+
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators({
-      // ...AccountsActions,
-      // ...BuyRamActions,
-      // ...BuyRamBytesActions,
-      // ...GlobalsActions,
-      // ...SellRamActions,
-      // ...SystemStateActions,
-      // ...TableActions,
-      // ...WalletActions,
+
     }, dispatch)
   };
 }
---
+++ app/modules/main/containers/Sections/Wallet/Transfer.js
@@ -0,0 +1,95 @@
+// @flow
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import {
+  HashRouter,
+  Route,
+  Switch,
+  withRouter
+} from 'react-router-dom';
+
+import { Grid, Header, Segment } from 'semantic-ui-react';
+
+import GlobalTransactionHandler from '../../../../../shared/components/Global/Transaction/Handler';
+import WalletPanelFormTransferSend from '../../../../../shared/components/Wallet/Panel/Form/Transfer/Send';
+
+import { clearSystemState } from '../../../../../shared/actions/system/systemstate';
+import { getContractHash } from '../../../../../shared/actions/accounts';
+import { transfer } from '../../../../../shared/actions/transfer';
+
+class WalletTransferContainer extends Component<Props> {
+  onClose = () => this.props.actions.clearSystemState()
+  render() {
+    const {
+      actions,
+      app,
+      balances,
+      blockExplorers,
+      connection,
+      settings,
+      system,
+    } = this.props;
+    const transaction = system.TRANSFER_LAST_TRANSACTION;
+    return (
+      <React.Fragment>
+        <Grid stackable>
+          <Grid.Row>
+            <Grid.Column width={10}>
+              <Segment color="blue" piled>
+                <Header>
+                  Transfer Tokens
+                  <Header.Subheader>
+                    Send tokens to another account, an exchange, or one of your contacts.
+                  </Header.Subheader>
+                </Header>
+                <GlobalTransactionHandler
+                  actionName="TRANSFER"
+                  actions={actions}
+                  blockExplorers={blockExplorers}
+                  content={(
+                    <WalletPanelFormTransferSend
+                      actions={actions}
+                      app={app}
+                      balances={balances}
+                      connection={connection}
+                      settings={settings}
+                      system={system}
+                    />
+                  )}
+                  icon="arrow circle up"
+                  onClose={actions.clearSystemState}
+                  settings={settings}
+                  system={system}
+                  transaction={transaction}
+                />
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    app: state.app,
+    balances: state.balances,
+    connection: state.connection,
+    settings: state.settings,
+    system: state.system,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      clearSystemState,
+      getContractHash,
+      transfer,
+    }, dispatch)
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(WalletTransferContainer));
