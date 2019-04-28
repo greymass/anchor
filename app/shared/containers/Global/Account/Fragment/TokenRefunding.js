9c9b85c8bb4cc1e417eb875bc033c0e4efe62f98
Aaron Cox <aaron@greymass.com>
2019-04-28 02:37:20 -0400

    Add refunding balances

---
+++ app/shared/containers/Global/Account/Fragment/TokenRefunding.js
@@ -0,0 +1,46 @@
+// @flow
+import { get } from 'dot-prop-immutable';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { translate } from 'react-i18next';
+import compose from 'lodash/fp/compose';
+import { Icon } from 'semantic-ui-react';
+import { isEmpty } from 'lodash';
+
+class GlobalAccountFragmentTokenRefunding extends PureComponent<Props> {
+  render() {
+    const {
+      balance,
+      lng,
+      precision,
+    } = this.props;
+    if (balance === false) return <Icon name="clock" />;
+    const formatter = new Intl.NumberFormat(lng, { minimumFractionDigits: precision });
+    return (
+      <React.Fragment>
+        {formatter.format(balance.toFixed(precision))}
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const loaded = !isEmpty(get(state, `accounts.${ownProps.account}`));
+  const defaultValue = loaded ? 0 : false;
+  let netRefunding = 0;
+  let cpuRefunding = 0;
+  const hasRefund = !isEmpty(get(state, `accounts.${ownProps.account}.refund_request`));
+  if (hasRefund) {
+    netRefunding = get(state, `accounts.${ownProps.account}.refund_request.net_amount`, defaultValue);
+    cpuRefunding = get(state, `accounts.${ownProps.account}.refund_request.cpu_amount`, defaultValue);
+  }
+  return {
+    balance: parseFloat(netRefunding) + parseFloat(cpuRefunding),
+    precision: get(state, `accounts.__contracts.${ownProps.token}.precision.${ownProps.token}`, 4),
+  };
+};
+
+export default compose(
+  translate('global'),
+  connect(mapStateToProps)
+)(GlobalAccountFragmentTokenRefunding);
