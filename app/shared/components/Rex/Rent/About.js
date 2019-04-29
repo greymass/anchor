3cc5659cc0d596e3f5a05684eb523a4a267c6682
dafuga <danielfugere28@gmail.com>
2019-04-29 12:54:33 +1000

    enhancement: split up the rex interface into two different sections for lendingh and renting

---
+++ app/shared/components/Rex/Rent/About.js
@@ -0,0 +1,35 @@
+// @flow
+import React, { PureComponent } from 'react';
+import { translate } from 'react-i18next';
+import {
+  Header,
+  Message,
+  Segment
+} from 'semantic-ui-react';
+
+class RexInterfaceAbout extends PureComponent<Props> {
+  render() {
+    const {
+      t
+    } = this.props;
+
+    return (
+      <Segment basic>
+        <Header
+          warning
+        >
+          <Header.Subheader>
+            {t('rex_interface_about_header')}
+          </Header.Subheader>
+        </Header>
+        <Message
+          warning
+        >
+          {t('rex_interface_about_warning')}
+        </Message>
+      </Segment>
+    );
+  }
+}
+
+export default translate('rex')(RexInterfaceAbout);
