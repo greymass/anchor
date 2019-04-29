d8c3db4e18fa898804bfa956f778a4018c5a6732
dafuga <danielfugere28@gmail.com>
2019-04-29 19:47:44 +1000

    enhancement: removed use of modals

--- app/shared/components/Rex/Lend/Manage.js
+++ app/shared/components/Rex/Lend/Manage.js
@@ -15,15 +15,18 @@ import GlobalFormFieldToken from '../../Global/Form/Field/Token';
 import GlobalFormMessageError from '../../Global/Form/Message/Error';
 import GlobalTransactionHandler from '../../Global/Transaction/Handler';
 
-class RexInterfaceManageRex extends PureComponent<Props> {
+class RexLendManage extends PureComponent<Props> {
   state = {
     confirming: false,
     transactionType: 'buy'
   };
   componentDidMount() {
-    const { actions } = this.props;
+    const { actions, settings } = this.props;
 
     actions.clearSystemState();
+
+    actions.getTable('eosio', settings.account, 'rexbal');
+    actions.getTable('eosio', settings.account, 'rexfund');
   }
   confirmTransaction = () => {
     const { actions } = this.props;
@@ -64,6 +67,7 @@ class RexInterfaceManageRex extends PureComponent<Props> {
       connection,
       settings,
       system,
+      tables,
       t
     } = this.props;
     const {
@@ -101,141 +105,156 @@ class RexInterfaceManageRex extends PureComponent<Props> {
       (!amountToSell && transactionType === 'sell');
     const displaySuccessMessage = !saveDisabled;
 
+    const fundedBalance = tables.eosio[settings.account].rexbal[0];
+
+    const confirmationPage = (
+      <React.Fragment>
+        <Header icon="cubes" content={t('rex_interface_manage_rex_confirmation_modal_header')} />
+        <GlobalTransactionHandler
+          actionName={transactionType === 'buy' ? 'BUYREX' : 'SELLREX'}
+          actions={actions}
+          blockExplorers={blockExplorers}
+          content={(
+            <React.Fragment>
+              { transactionType === 'buy' ? (
+                <p>
+                  {t('rex_interface_manage_rex_confirmation_modal_buy_rex', {
+                    amountToBuy,
+                    rexAmount: (amountToBuy.split(' ')[0] / priceOfRex)
+                  })}
+                </p>
+              ) : (
+                <p>
+                  {t('rex_interface_manage_rex_confirmation_modal_sell_rex', {
+                    amountToSell,
+                    rexAmount: (amountToSell.split(' ')[0] / priceOfRex)
+                  })}
+                </p>
+              )}
+            </React.Fragment>
+          )}
+          contract={contract}
+          onClose={this.onClose}
+          onSubmit={this.onSubmit}
+          settings={settings}
+          system={system}
+          transaction={transaction}
+        />
+        <Container>
+          <Button
+            content={t('common:close')}
+            onClick={() => this.setState({ confirming: false })}
+            textAlign="left"
+          />
+          <Button
+            color="green"
+            content={t('common:confirm')}
+            disabled={system.BUY_REX || system.SELL_REX}
+            onClick={this.confirmTransaction}
+            textAlign="right"
+          />
+        </Container>
+      </React.Fragment>
+    );
+
     return (
       <Segment basic>
-        {confirming && (
-          <Modal
-            open
-            size="small"
-          >
-            <Header icon="cubes" content={t('rex_interface_manage_rex_confirmation_modal_header')} />
-            <Modal.Content>
-              <GlobalTransactionHandler
-                actionName={transactionType === 'buy' ? 'BUY_REX' : 'SELL_REX'}
-                actions={actions}
-                blockExplorers={blockExplorers}
-                content={(
-                  <React.Fragment>
-                    { transactionType === 'buy' ? (
-                      <p>
-                        {t('rex_interface_manage_rex_confirmation_modal_buy_rex', {
-                          amountToBuy,
-                          rexAmount: (amountToBuy.split(' ')[0] / priceOfRex)
-                        })}
-                      </p>
-                    ) : (
-                      <p>
-                        {t('rex_interface_manage_rex_confirmation_modal_sell_rex', {
-                          amountToSell,
-                          rexAmount: (amountToSell.split(' ')[0] / priceOfRex)
-                        })}
-                      </p>
-                    )}
-                  </React.Fragment>
+        {confirming ? confirmationPage : (
+          <React.Fragment>
+            <Message
+              warning
+            >
+              {t('rex_interface_manage_rex_message', { chainSymbol: connection.chainSymbol })}
+            </Message>
+            <Message success>
+              <Header>
+                {
+                  t(
+                    'rex_interface_rent_funding_balance',
+                    {
+                      fundedBalance,
+                      chainSymbol: connection.chainSymbol
+                    }
+                  )
+                }
+              </Header>
+              <Header>
+                {t('rex_interface_rent_rex_balance', { rexBalance })}
+              </Header>
+            </Message>
+            <Form success={displaySuccessMessage}>
+              <Form.Group widths="equal">
+                <label>
+                  <strong>{t('rex_interface_transaction_type_label')}</strong>
+                  <br />
+                  <Dropdown
+                    autoFocus
+                    defaultValue="buy"
+                    name="transactionType"
+                    onChange={(e, props) => this.handleChange(e, { ...props, valid: true })}
+                    options={dropdownOptions}
+                    selection
+                    style={{ marginTop: '4px' }}
+                  />
+                </label>
+
+                {transactionType === 'buy' ? (
+                  <GlobalFormFieldToken
+                    connection={connection}
+                    key="amountToBuy"
+                    label={t('rex_interface_manage_rex_buy', { chainSymbol: connection.chainSymbol })}
+                    name="amountToBuy"
+                    onChange={this.handleChange}
+                  />
+                ) : (
+                  <GlobalFormFieldToken
+                    connection={connection}
+                    key="amountToSell"
+                    label={t('rex_interface_manage_rex_sell', { chainSymbol: connection.chainSymbol })}
+                    name="amountToSell"
+                    onChange={this.handleChange}
+                  />
                 )}
-                contract={contract}
-                onClose={this.onClose}
-                onSubmit={this.onSubmit}
-                settings={settings}
-                system={system}
-                transaction={transaction}
+              </Form.Group>
+              { displaySuccessMessage && transactionType === 'buy' && amountToBuy && (
+                <Message success>
+                  {t(
+                    'rex_interface_manage_rex_amount_to_buy',
+                    {
+                      amount: amountToBuy,
+                      rexAmount: amountToBuy.split(' ')[0] / priceOfRex
+                    }
+                  )}
+                </Message>
+              )}
+              { displaySuccessMessage && transactionType === 'sell' && amountToSell && (
+                <Message success>
+                  {t(
+                    'rex_interface_manage_rex_amount_to_sell',
+                    {
+                      amount: amountToSell,
+                      rexAmount: amountToSell.split(' ')[0] / priceOfRex
+                    }
+                  )}
+                </Message>
+              )}
+              {error && (
+                <GlobalFormMessageError
+                  error={error}
+                  style={{ marginTop: '20px', marginBottom: '20px' }}
+                />
+              )}
+              <Button
+                content={transactionType === 'buy' ? t('rex_interface_manage_rex_buy_button') : t('rex_interface_manage_rex_sell_button')}
+                disabled={saveDisabled}
+                onClick={() => this.setState({ confirming: true })}
               />
-            </Modal.Content>
-
-            <Modal.Actions>
-              <Container>
-                <Button
-                  content={t('common:close')}
-                  onClick={() => this.setState({ confirming: false })}
-                  textAlign="left"
-                />
-                <Button
-                  color="green"
-                  content={t('common:confirm')}
-                  disabled={system.BUY_REX || system.SELL_REX}
-                  onClick={this.confirmTransaction}
-                  textAlign="right"
-                />
-              </Container>
-            </Modal.Actions>
-          </Modal>
+            </Form>
+          </React.Fragment>
         )}
-        <Message
-          warning
-        >
-          {t('rex_interface_manage_rex_message', { chainSymbol: connection.chainSymbol })}
-        </Message>
-        <Form success={displaySuccessMessage}>
-          <Form.Group widths="equal">
-            <label>
-              <strong>{t('rex_interface_transaction_type_label')}</strong>
-              <br />
-              <Dropdown
-                autoFocus
-                defaultValue="buy"
-                name="transactionType"
-                onChange={(e, props) => this.handleChange(e, { ...props, valid: true })}
-                options={dropdownOptions}
-                selection
-                style={{ marginTop: '4px' }}
-              />
-            </label>
-
-            {transactionType === 'buy' ? (
-              <GlobalFormFieldToken
-                connection={connection}
-                key="amountToBuy"
-                label={t('rex_interface_manage_rex_buy', { chainSymbol: connection.chainSymbol })}
-                name="amountToBuy"
-                onChange={this.handleChange}
-              />
-            ) : (
-              <GlobalFormFieldToken
-                connection={connection}
-                key="amountToSell"
-                label={t('rex_interface_manage_rex_sell', { chainSymbol: connection.chainSymbol })}
-                name="amountToSell"
-                onChange={this.handleChange}
-              />
-            )}
-          </Form.Group>
-          { displaySuccessMessage && transactionType === 'buy' && amountToBuy && (
-            <Message success>
-              {t(
-                'rex_interface_manage_rex_amount_to_buy',
-                {
-                  amount: amountToBuy,
-                  rexAmount: amountToBuy.split(' ')[0] / priceOfRex
-                }
-              )}
-            </Message>
-          )}
-          { displaySuccessMessage && transactionType === 'sell' && amountToSell && (
-            <Message success>
-              {t(
-                'rex_interface_manage_rex_amount_to_sell',
-                {
-                  amount: amountToSell,
-                  rexAmount: amountToSell.split(' ')[0] / priceOfRex
-                }
-              )}
-            </Message>
-          )}
-          {error && (
-            <GlobalFormMessageError
-              error={error}
-              style={{ marginTop: '20px', marginBottom: '20px' }}
-            />
-          )}
-          <Button
-            content={transactionType === 'buy' ? t('rex_interface_manage_rex_buy_button') : t('rex_interface_manage_rex_sell_button')}
-            disabled={saveDisabled}
-            onClick={() => this.setState({ confirming: true })}
-          />
-        </Form>
       </Segment>
     );
   }
 }
 
-export default translate('rex')(RexInterfaceManageRex);
+export default translate('rex')(RexLendManage);
