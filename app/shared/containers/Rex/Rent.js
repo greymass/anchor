2767541c3dc75d03bcfa4e6e247093f571f00398
dafuga <danielfugere28@gmail.com>
2019-04-29 12:54:33 +1000

    enhancement: split up the rex interface into two different sections for lendingh and renting

---
+++ app/shared/containers/Rex/Rent.js
@@ -0,0 +1,113 @@
+// @flow
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Tab } from 'semantic-ui-react';
+import { translate } from 'react-i18next';
+
+import RexInterfaceAbout from '../../components/Rex/Rent/About';
+import RexInterfaceFund from '../../components/Rex/shared/Fund';
+import RexInterfaceRentManage from '../../components/Rex/Rent/Manage';
+
+type Props = {
+  actions: {},
+  accounts: {},
+  balance: {},
+  blockExplorers: {},
+  connection: {},
+  settings: {},
+  system: {},
+  t: {}
+};
+
+class RexRent extends Component<Props> {
+  props: Props;
+
+  render() {
+    const {
+      actions,
+      balance,
+      blockExplorers,
+      connection,
+      settings,
+      system,
+      t
+    } = this.props;
+
+    const panes = [
+      {
+        menuItem: {
+          key: 'about',
+          icon: 'info',
+          content: t('rex_interface_menu_about')
+        },
+        pane: {
+          key: 'about',
+          content: (
+            <RexInterfaceAbout />
+          )
+        }
+      },
+      {
+        menuItem: {
+          key: 'fund',
+          icon: 'arrow down',
+          content: t('rex_interface_menu_fund')
+        },
+        pane: {
+          key: 'fund',
+          content: (
+            <RexInterfaceFund
+              actions={actions}
+              balance={balance}
+              blockExplorers={blockExplorers}
+              connection={connection}
+              settings={settings}
+              system={system}
+            />
+          )
+        }
+      },
+      {
+        menuItem: {
+          key: 'manage_rex',
+          icon: 'arrow right',
+          content: t('rex_interface_menu_manage_rex')
+        },
+        pane: {
+          key: 'manage_rex',
+          content: (
+            <RexInterfaceRentManage
+              actions={actions}
+              blockExplorers={blockExplorers}
+              connection={connection}
+              settings={settings}
+              system={system}
+            />
+          )
+        }
+      }
+    ];
+    return (
+      <React.Fragment>
+        <Tab
+          defaultActiveIndex={0}
+          onTabChange={this.onTabChange}
+          panes={panes}
+          renderActiveOnly={false}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    accounts: state.accounts,
+    balance: state.balances[state.settings.account],
+    blockExplorers: (state.connection && state.blockexplorers[state.connection.chainKey]) || {},
+    connection: state.connection,
+    settings: state.settings
+  };
+}
+
+export default translate('rex')(connect(mapStateToProps)(RexRent));
