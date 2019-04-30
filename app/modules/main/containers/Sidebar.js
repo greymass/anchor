a7a5c1794b633d4008c1165413da187f96a3cc08
Aaron Cox <aaron@greymass.com>
2019-04-29 20:08:39 -0400

    Redesigned sidebar

--- app/modules/main/containers/Sidebar.js
+++ app/modules/main/containers/Sidebar.js
@@ -3,16 +3,25 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Icon, Image, Menu } from 'semantic-ui-react';
+import { Header, Icon, Image, Menu } from 'semantic-ui-react';
 import Logo from '../../../renderer/assets/images/anchor-shape.svg';
 
 import packageJson from '../../../package.json';
 import NavigationActions from '../actions/navigation';
+import * as SettingsActions from '../../../shared/actions/settings';
 
-import GreymassLogoHorizontal from '../../../renderer/assets/images/anchor-text.svg';
+
+import GreymassLogoHorizontal from '../../../renderer/assets/images/anchor-text-light.svg';
 
 class SidebarContainer extends Component<Props> {
   onClick = (e, data) => this.props.actions.changeModule(data.name)
+  toggleCollapsed = () => {
+    const {
+      actions,
+      settings,
+    } = this.props;
+    actions.setSetting('sidebarCollapsed', !settings.sidebarCollapsed);
+  }
   render() {
     const {
       navigation,
@@ -22,28 +31,41 @@ class SidebarContainer extends Component<Props> {
     const {
       module
     } = navigation;
+    const color = '#d1d5d8';
     return (
       <Menu
         animation="overlay"
         className="nav-sidebar"
-        icon="labeled"
+        inverted
+        icon={settings.sidebarCollapsed}
         style={{
           display: 'flex',
-          minWidth: '7.67em',
+          background: '#29242f',
+          borderRadius: 0,
+          borderRight: 'none',
           minHeight: '100vh',
         }}
         vertical
       >
-        <Menu.Item as="a" onClick={this.onClick} name="">
+        <Menu.Item
+          as="a"
+          onClick={this.onClick}
+          name=""
+          style={{
+            color,
+            textAlign: 'center'
+          }}
+        >
           <Image
             centered
             src={Logo}
             size="large"
             style={{
               marginBottom: '0.5rem',
+              width: ((settings.sidebarCollapsed) ? '2em' : '7em'),
             }}
           />
-          Home
+            Home
         </Menu.Item>
         {(
           settings.walletMode !== 'cold'
@@ -55,23 +77,17 @@ class SidebarContainer extends Component<Props> {
             <React.Fragment>
               <Menu.Item
                 as="a"
-                active={module === 'tests'}
-                onClick={this.onClick}
-                name="tests"
-                color="blue"
-              >
-                <Icon name="lab" />
-                TESTS
-              </Menu.Item>
-              <Menu.Item
-                as="a"
                 active={module && module.startsWith('wallet')}
                 onClick={this.onClick}
                 name="wallet"
                 color="blue"
+                style={{ color }}
               >
-                <Icon name="money bill" />
-                Wallet
+                <Icon name="money bill" size="large" />
+                {(!settings.sidebarCollapsed)
+                  ? <p>Wallet</p>
+                  : false
+                }
               </Menu.Item>
               <Menu.Item
                 as="a"
@@ -79,9 +95,13 @@ class SidebarContainer extends Component<Props> {
                 onClick={this.onClick}
                 name="governance/producers"
                 color="purple"
+                style={{ color }}
               >
-                <Icon name="balance" />
-                Governance
+                <Icon name="balance" size="large" />
+                {(!settings.sidebarCollapsed)
+                  ? <p>Governance</p>
+                  : false
+                }
               </Menu.Item>
             </React.Fragment>
           )
@@ -93,23 +113,71 @@ class SidebarContainer extends Component<Props> {
           onClick={this.onClick}
           name="tools"
           color="violet"
+          style={{ color }}
         >
-          <Icon name="wrench" />
-          Tools
+          <Icon
+            name="wrench"
+            size="large"
+          />
+          {(!settings.sidebarCollapsed)
+            ? <p>Tools</p>
+            : false
+          }
         </Menu.Item>
         <Menu.Header
           style={{
             flexGrow: 1
           }}
         />
+
+        <Menu.Item
+          as="a"
+          onClick={this.toggleCollapsed}
+          style={{
+            color,
+          }}
+        >
+          <Icon
+            name={(settings.sidebarCollapsed) ? 'list' : 'circle arrow left'}
+            size={(settings.sidebarCollapsed) ? 'small' : 'large'}
+            style={{
+              color,
+            }}
+          />
+          {(!settings.sidebarCollapsed)
+            ? <p>Minimize Sidebar</p>
+            : false
+          }
+        </Menu.Item>
         <Menu.Item
           as="a"
           name="version"
           onClick={this.onClick}
-          style={{ padding: '0.5em 0' }}
+          style={{
+            color,
+            padding: '0.5em 0',
+            textAlign: 'center',
+          }}
         >
-          {packageJson.version}
-          <Image centered src={GreymassLogoHorizontal} style={{ marginTop: '0.25em', maxWidth: '6em', width: '6em' }} />
+          <Header
+            content={packageJson.version}
+            size="small"
+            style={{
+              color,
+              margin: 0,
+            }}
+          />
+          <Image
+            centered
+            src={GreymassLogoHorizontal}
+            style={{
+              color: 'red',
+              fill: 'currentColor',
+              marginTop: '0.25em',
+              maxWidth: ((settings.sidebarCollapsed) ? '4em' : '7em'),
+              width: ((settings.sidebarCollapsed) ? '4em' : '7em')
+            }}
+          />
         </Menu.Item>
       </Menu>
     );
@@ -127,7 +195,8 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators({
-      ...NavigationActions
+      ...NavigationActions,
+      ...SettingsActions,
     }, dispatch)
   };
 }
