a41100fb374287571dcf72c32af5cfa240957689
Aaron Cox <aaron@greymass.com>
2019-04-30 15:23:26 -0400

    Rewrote key storage

--- app/shared/utils/Anchor/Wallet.js
+++ app/shared/utils/Anchor/Wallet.js
@@ -5,12 +5,12 @@ export default class EOSWallet {
 
   importProps(wallet, chainId = undefined) {
     this.wallet = {
-      schema: 'anchor.v1.wallet',
+      schema: 'anchor.v2.wallet',
       data: {
         account: wallet.account,
         authority: wallet.authorization,
         chainId: wallet.chainId || chainId,
-        data: wallet.data || undefined,
+        mode: wallet.mode,
         path: wallet.path || undefined,
         pubkey: wallet.pubkey,
         type: (wallet.path) ? 'ledger' : 'key',
@@ -28,6 +28,16 @@ export default class EOSWallet {
           chainId: wallet.chainId,
           data: wallet.data,
           mode: (wallet.path) ? 'ledger' : mode,
+          path: wallet.path,
+          pubkey: wallet.pubkey,
+        };
+      }
+      case 'anchor.v2.wallet': {
+        return {
+          account: wallet.account,
+          authorization: wallet.authority,
+          chainId: wallet.chainId,
+          mode: wallet.path,
           path: wallet.path,
           pubkey: wallet.pubkey,
         };
