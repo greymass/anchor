a5dfdd7fcf1dcf519aa9db1d974d92ef0e74225d
Aaron Cox <aaron@greymass.com>
2019-04-30 18:06:56 -0400

    Implement staking

--- app/modules/main/containers/Sections/Wallet/Stake.js
+++ app/modules/main/containers/Sections/Wallet/Stake.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { get } from 'dot-prop-immutable';
+import { Decimal } from 'decimal.js';
 import {
   HashRouter,
   Route,
@@ -12,16 +14,18 @@ import {
 import { Grid, Header, Segment } from 'semantic-ui-react';
 
 import GlobalTransactionHandler from '../../../../../shared/components/Global/Transaction/Handler';
-import WalletPanelFormTransferSend from '../../../../../shared/components/Wallet/Panel/Form/Transfer/Send';
+import WalletPanelFormStake from '../../../../../shared/components/Wallet/Panel/Form/Stake';
 
 import { clearSystemState } from '../../../../../shared/actions/system/systemstate';
 import { getContractHash } from '../../../../../shared/actions/accounts';
+import { setStake } from '../../../../../shared/actions/stake';
 import { transfer } from '../../../../../shared/actions/transfer';
 
 class WalletStakeContainer extends Component<Props> {
   onClose = () => this.props.actions.clearSystemState()
   render() {
     const {
+      account,
       actions,
       app,
       balances,
@@ -29,8 +33,13 @@ class WalletStakeContainer extends Component<Props> {
       connection,
       settings,
       system,
+      validate,
     } = this.props;
-    const transaction = system.TRANSFER_LAST_TRANSACTION;
+    const transaction = system.STAKE_LAST_TRANSACTION;
+    const {
+      cpu_weight,
+      net_weight
+    } = account.self_delegated_bandwidth;
     return (
       <React.Fragment>
         <Grid stackable>
@@ -40,10 +49,35 @@ class WalletStakeContainer extends Component<Props> {
                 <Header>
                   Stake Tokens
                   <Header.Subheader>
-
+                    Staking grants the accounts rights to resources on the network.
                   </Header.Subheader>
                 </Header>
-
+                <GlobalTransactionHandler
+                  actionName="STAKE"
+                  actions={actions}
+                  blockExplorers={blockExplorers}
+                  content={(
+                    <WalletPanelFormStake
+                      account={account}
+                      accountName={settings.account}
+                      actions={actions}
+                      balance={balances[settings.account]}
+                      connection={connection}
+                      cpuAmount={Decimal(String(cpu_weight).split(' ')[0])}
+                      key={`stake-form-${settings.account}`}
+                      netAmount={Decimal(String(net_weight).split(' ')[0])}
+                      onClose={this.onClose}
+                      settings={settings}
+                      system={system}
+                      validate={validate}
+                    />
+                  )}
+                  icon="microchip"
+                  title="Update Staked"
+                  settings={settings}
+                  system={system}
+                  transaction={transaction}
+                />
               </Segment>
             </Grid.Column>
           </Grid.Row>
@@ -55,11 +89,13 @@ class WalletStakeContainer extends Component<Props> {
 
 function mapStateToProps(state) {
   return {
+    account: get(state, `accounts.${state.settings.account}`),
     app: state.app,
     balances: state.balances,
     connection: state.connection,
     settings: state.settings,
     system: state.system,
+    validate: state.validate,
   };
 }
 
@@ -68,6 +104,7 @@ function mapDispatchToProps(dispatch) {
     actions: bindActionCreators({
       clearSystemState,
       getContractHash,
+      setStake,
       transfer,
     }, dispatch)
   };
