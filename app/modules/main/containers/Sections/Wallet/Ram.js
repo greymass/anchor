e16cdcce428866fb7bbc8658d08bac7bd82eda67
Aaron Cox <aaron@greymass.com>
2019-04-30 21:50:35 -0400

    Basic RAM Market panel

---
+++ app/modules/main/containers/Sections/Wallet/Ram.js
@@ -0,0 +1,187 @@
+// @flow
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { get } from 'dot-prop-immutable';
+import { translate } from 'react-i18next';
+import { withRouter } from 'react-router-dom';
+import compose from 'lodash/fp/compose';
+
+import { Grid, Header, Popup, Segment } from 'semantic-ui-react';
+
+import WalletPanelButtonRamBuy from '../../../../../shared/components/Wallet/Panel/Button/Ram/Buy';
+import WalletPanelButtonRamSell from '../../../../../shared/components/Wallet/Panel/Button/Ram/Sell';
+import GlobalAccountRequired from '../../../../../shared/containers/Global/Account/Required';
+
+import * as BuyRamBytesActions from '../../../../../shared/actions/system/buyrambytes';
+import * as BuyRamActions from '../../../../../shared/actions/system/buyram';
+import * as SellRamActions from '../../../../../shared/actions/system/sellram';
+import { clearSystemState } from '../../../../../shared/actions/system/systemstate';
+import { getContractHash } from '../../../../../shared/actions/accounts';
+import { getRamStats } from '../../../../../shared/actions/globals';
+import { setStake } from '../../../../../shared/actions/stake';
+import { transfer } from '../../../../../shared/actions/transfer';
+
+class WalletRamContainer extends Component<Props> {
+  onClose = () => this.props.actions.clearSystemState()
+  render() {
+    const {
+      account,
+      actions,
+      app,
+      balances,
+      blockExplorers,
+      chain,
+      connection,
+      globals,
+      settings,
+      system,
+      validate,
+    } = this.props;
+    const transaction = system.STAKE_LAST_TRANSACTION;
+    // Disable RAM markets on specific chains (Worbli)
+    const disableRamMarket = (connection.chainId === '73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f');
+    // Disable features based on distribution feature (BEOS)
+    const distributionPeriod = get(chain, 'distributionPeriodInfo.beosDistribution', false);
+    return (
+      <Grid stackable>
+        <Grid.Row>
+          <Grid.Column width={10}>
+            <Segment color="blue" piled>
+              <Header>
+                RAM Trading
+                <Header.Subheader>
+                  Buy or Sell RAM into the automated market.
+                </Header.Subheader>
+              </Header>
+              <GlobalAccountRequired>
+                {(distributionPeriod)
+                  ? (
+                    <Popup
+                      content={t('beos_withdraw:disabled_for_distribution_period')}
+                      inverted
+                      position="right center"
+                      trigger={(
+                        <Segment>
+                          <WalletPanelButtonRamBuy
+                            account={account}
+                            actions={actions}
+                            balances={balances}
+                            blockExplorers={blockExplorers}
+                            connection={connection}
+                            disabled
+                            globals={globals}
+                            settings={settings}
+                            system={system}
+                          />
+                        </Segment>
+                      )}
+                    />
+                  )
+                  : false
+                }
+                {(!distributionPeriod && !disableRamMarket)
+                  ? (
+                    <Segment>
+                      <WalletPanelButtonRamBuy
+                        account={account}
+                        actions={actions}
+                        balances={balances}
+                        blockExplorers={blockExplorers}
+                        connection={connection}
+                        globals={globals}
+                        settings={settings}
+                        system={system}
+                        disabled={false}
+                      />
+                    </Segment>
+                  )
+                  : false
+                }
+                {(distributionPeriod)
+                  ? (
+                    <Popup
+                      content={t('beos_withdraw:disabled_for_distribution_period')}
+                      inverted
+                      position="right center"
+                      trigger={(
+                        <Segment>
+                          <WalletPanelButtonRamSell
+                            account={account}
+                            actions={actions}
+                            balances={balances}
+                            blockExplorers={blockExplorers}
+                            connection={connection}
+                            disabled
+                            globals={globals}
+                            settings={settings}
+                            system={system}
+                          />
+                        </Segment>
+                      )}
+                    />
+                  )
+                  : false
+                }
+                {(!distributionPeriod && !disableRamMarket)
+                  ? (
+                    <Segment>
+                      <WalletPanelButtonRamSell
+                        account={account}
+                        actions={actions}
+                        balances={balances}
+                        blockExplorers={blockExplorers}
+                        connection={connection}
+                        globals={globals}
+                        settings={settings}
+                        system={system}
+                        disabled={false}
+                      />
+                    </Segment>
+                  )
+                  : false
+                }
+              </GlobalAccountRequired>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    account: get(state, `accounts.${state.settings.account}`),
+    app: state.app,
+    balances: state.balances,
+    chain: state.chain,
+    connection: state.connection,
+    globals: state.globals,
+    settings: state.settings,
+    system: state.system,
+    validate: state.validate,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+      ...BuyRamBytesActions,
+      ...BuyRamActions,
+      ...SellRamActions,
+      clearSystemState,
+      getContractHash,
+      getRamStats,
+      setStake,
+      transfer,
+    }, dispatch)
+  };
+}
+
+
+export default compose(
+  withRouter,
+  translate('global'),
+  connect(mapStateToProps, mapDispatchToProps)
+)(WalletRamContainer);
