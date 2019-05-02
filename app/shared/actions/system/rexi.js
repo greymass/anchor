676714ccdba4a41b8e9a7595ae2bf2aaf4d6341e
dafuga <danielfugere28@gmail.com>
2019-05-02 21:41:15 +1200

    chore: added a unstaketorex action in rex file
    also refactored rexActions method

--- app/shared/actions/system/rexi.js
+++ app/shared/actions/system/rexi.js
@@ -5,41 +5,88 @@ import eos from '../helpers/eos';
 
 export function buyrex(amount) {
   return (dispatch: () => void, getState) => {
-    rexAction('buyrex', 'BUYREX', amount, dispatch, getState);
+    const { settings } = getState();
+    const data = {
+      from: settings.account,
+      amount
+    };
+    rexAction('buyrex', 'BUYREX', data, dispatch, getState);
   };
 }
 
 export function sellrex(amount) {
   return (dispatch: () => void, getState) => {
-    rexAction('sellrex', 'SELLREX', amount, dispatch, getState);
+    const { settings } = getState();
+    const data = {
+      from: settings.account,
+      rex: amount
+    };
+    rexAction('sellrex', 'SELLREX', data, dispatch, getState);
   };
 }
 
 export function depositrex(amount) {
   return (dispatch: () => void, getState) => {
-    rexAction('deposit', 'DEPOSITREX', amount, dispatch, getState);
+    const { settings } = getState();
+    const data = {
+      owner: settings.account,
+      amount
+    };
+    rexAction('deposit', 'DEPOSITREX', data, dispatch, getState);
   };
 }
 
 export function withdrawrex(amount) {
   return (dispatch: () => void, getState) => {
-    rexAction('withdraw', 'WITHDRAWREX', amount, dispatch, getState);
+    const { settings } = getState();
+    const data = {
+      owner: settings.account,
+      amount
+    };
+    rexAction('withdraw', 'WITHDRAWREX', data, dispatch, getState);
   };
 }
 
 export function rentcpu(amount) {
   return (dispatch: () => void, getState) => {
-    rexAction('rentcpu', 'RENTCPUREX', amount, dispatch, getState);
+    const { settings } = getState();
+    const data = {
+      from: settings.account,
+      loan_payment: amount,
+      loan_fund: amount,
+      receiver: settings.account,
+    };
+    rexAction('rentcpu', 'RENTCPUREX', data, dispatch, getState);
   };
 }
 
 export function rentnet(amount) {
   return (dispatch: () => void, getState) => {
-    rexAction('rentnet', 'RENTNETREX', amount, dispatch, getState);
+    const { settings } = getState();
+    const data = {
+      from: settings.account,
+      loan_payment: amount,
+      loan_fund: amount,
+      receiver: settings.account,
+    };
+    rexAction('rentnet', 'RENTNETREX', data, dispatch, getState);
   };
 }
 
-async function rexAction(actionName, actionVariable, amount, dispatch, getState) {
+export function unstaketorex(amount, type) {
+  return (dispatch: () => void, getState) => {
+    const { settings } = getState();
+    const data = {
+      owner: settings.account,
+      receiver: settings.account,
+      from_cpu: (type === 'cpu' && amount),
+      from_net: (type === 'net' && amount),
+    };
+    rexAction('unstaketorex', 'UNSTAKETOREX', data, dispatch, getState);
+  };
+}
+
+async function rexAction(actionName, actionVariable, data, dispatch, getState) {
   const {
     settings,
     connection
@@ -49,27 +96,6 @@ async function rexAction(actionName, actionVariable, amount, dispatch, getState)
     type: types[`SYSTEM_${actionVariable}_PENDING`],
     payload: { connection }
   });
-
-  let accountField = 'owner';
-  let amountField = 'amount';
-  if (['BUYREX', 'SELLREX', 'RENTCPUREX', 'RENTNETREX'].includes(actionVariable)) {
-    accountField = 'from';
-  }
-  if (['SELLREX'].includes(actionVariable)) {
-    amountField = 'rex';
-  }
-  if (['RENTCPUREX', 'RENTNETREX'].includes(actionVariable)) {
-    amountField = 'loan_payment';
-  }
-
-  const data = {
-    [accountField]: settings.account,
-    [amountField]: amount,
-  }
-  if (['RENTCPUREX', 'RENTNETREX'].includes(actionVariable)) {
-    data.receiver = settings.account;
-    data.loan_fund = amount;
-  }
 
   // hack to get signing working on ledger - needs to be refactored
   const eosobj = eos(connection, true, true);
