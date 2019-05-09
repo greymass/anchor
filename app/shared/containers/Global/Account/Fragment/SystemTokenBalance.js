0f2af2271cbe771e75e26dbe71f9ddd0ed3fefc6
Aaron Cox <aaron@greymass.com>
2019-05-08 20:35:40 -0400

    Proper escaping of account names with dots.

--- app/shared/containers/Global/Account/Fragment/SystemTokenBalance.js
+++ app/shared/containers/Global/Account/Fragment/SystemTokenBalance.js
@@ -10,10 +10,8 @@ import { isEmpty, sum } from 'lodash';
 class GlobalAccountFragmentSystemTokenBalance extends PureComponent<Props> {
   render() {
     const {
-      lng
-    } = this.props;
-    let {
       balance,
+      lng,
     } = this.props;
     if (balance === false) return <Icon color="grey" name="clock outline" />;
     const formatter = new Intl.NumberFormat(lng, { minimumFractionDigits: 4 });
@@ -26,10 +24,11 @@ class GlobalAccountFragmentSystemTokenBalance extends PureComponent<Props> {
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const loaded = !isEmpty(get(state, `balances.${ownProps.account}`));
+  const account = ownProps.account.replace('.', '\\.');
+  const loaded = !isEmpty(get(state, `balances.${account}`));
   const defaultValue = loaded ? 0 : false;
-  const liquid = get(state, `balances.${ownProps.account}.${ownProps.token}`, defaultValue);
-  const path = `accounts.${ownProps.account}.self_delegated_bandwidth`;
+  const liquid = get(state, `balances.${account}.${ownProps.token}`, defaultValue);
+  const path = `accounts.${account}.self_delegated_bandwidth`;
   const cpuWeight = String(get(state, `${path}.cpu_weight`, defaultValue));
   const netWeight = String(get(state, `${path}.net_weight`, defaultValue));
   if (liquid === false || cpuWeight === false || netWeight === false) {
@@ -37,14 +36,14 @@ const mapStateToProps = (state, ownProps) => {
       balance: false
     });
   }
-  const delegated = get(state, `accounts.${ownProps.account}.delegated.total`, 0);
+  const delegated = get(state, `accounts.${account}.delegated.total`, 0);
   // Get refunding balances for total
   let netRefunding = 0;
   let cpuRefunding = 0;
-  const hasRefund = !isEmpty(get(state, `accounts.${ownProps.account}.refund_request`));
+  const hasRefund = !isEmpty(get(state, `accounts.${account}.refund_request`));
   if (hasRefund) {
-    netRefunding = get(state, `accounts.${ownProps.account}.refund_request.net_amount`, 0);
-    cpuRefunding = get(state, `accounts.${ownProps.account}.refund_request.cpu_amount`, 0);
+    netRefunding = get(state, `accounts.${account}.refund_request.net_amount`, 0);
+    cpuRefunding = get(state, `accounts.${account}.refund_request.cpu_amount`, 0);
   }
   return ({
     balance: sum([
