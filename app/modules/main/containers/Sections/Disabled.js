5696e9c323403efb6121aefd643fffeeba067ced
Aaron Cox <aaron@greymass.com>
2019-05-14 17:16:06 -0400

    Disabled Section component

---
+++ app/modules/main/containers/Sections/Disabled.js
@@ -0,0 +1,33 @@
+// @flow
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import { Segment } from 'semantic-ui-react';
+
+class VersionContainer extends Component<Props> {
+  render() {
+    return (
+      <Segment>
+        This section is not yet enabled.
+      </Segment>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators({
+
+    }, dispatch)
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(VersionContainer));
