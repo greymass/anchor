950be8092576293a759ade5c44cc9d6a6ff6cb60
Aaron Cox <aaron@greymass.com>
2019-05-16 20:51:07 -0400

    updated name in shasum checks

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-eos-voter-0.7.12-amd64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.12-amd64.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.12-amd64.snap"
-# shasum -b -a 512 linux-eos-voter-0.7.12-amd64.snap
-echo "shasum -b -a 512 linux-eos-voter-0.7.12-arm64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.12-arm64.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.12-armv7l.deb"
-shasum -b -a 512 linux-eos-voter-0.7.12-armv7l.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.12-i386.deb"
-# shasum -b -a 512 linux-eos-voter-0.7.12-i386.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.12-x86_64.AppImage"
-shasum -b -a 512 linux-eos-voter-0.7.12-x86_64.AppImage
-echo "shasum -b -a 512 mac-eos-voter-0.7.12.dmg"
-shasum -b -a 512 mac-eos-voter-0.7.12.dmg
-echo "shasum -b -a 512 mac-eos-voter-0.7.12.zip"
-shasum -b -a 512 mac-eos-voter-0.7.12.zip
-echo "shasum -b -a 512 win-eos-voter-0.7.12.exe"
-shasum -b -a 512 win-eos-voter-0.7.12.exe
+echo "shasum -b -a 512 linux-anchor-beta-0.9.0-amd64.deb"
+shasum -b -a 512 linux-anchor-beta-0.9.0-amd64.deb
+# echo "shasum -b -a 512 linux-anchor-beta-0.9.0-amd64.snap"
+# shasum -b -a 512 linux-anchor-beta-0.9.0-amd64.snap
+echo "shasum -b -a 512 linux-anchor-beta-0.9.0-arm64.deb"
+shasum -b -a 512 linux-anchor-beta-0.9.0-arm64.deb
+echo "shasum -b -a 512 linux-anchor-beta-0.9.0-armv7l.deb"
+shasum -b -a 512 linux-anchor-beta-0.9.0-armv7l.deb
+# echo "shasum -b -a 512 linux-anchor-beta-0.9.0-i386.deb"
+# shasum -b -a 512 linux-anchor-beta-0.9.0-i386.deb
+echo "shasum -b -a 512 linux-anchor-beta-0.9.0-x86_64.AppImage"
+shasum -b -a 512 linux-anchor-beta-0.9.0-x86_64.AppImage
+echo "shasum -b -a 512 mac-anchor-beta-0.9.0.dmg"
+shasum -b -a 512 mac-anchor-beta-0.9.0.dmg
+echo "shasum -b -a 512 mac-anchor-beta-0.9.0.zip"
+shasum -b -a 512 mac-anchor-beta-0.9.0.zip
+echo "shasum -b -a 512 win-anchor-beta-0.9.0.exe"
+shasum -b -a 512 win-anchor-beta-0.9.0.exe
 
 cd ..
