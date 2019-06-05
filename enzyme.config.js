66aae53fb654ca7cac55449a8e7e36ae4fbdebc7
dafuga <danielfugere28@gmail.com>
2019-06-05 13:27:11 +0800

    setup: got unit testing setup

---
+++ enzyme.config.js
@@ -0,0 +1,8 @@
+/** Used in jest.config.js */
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'babel-polyfill';
+
+configure({
+  adapter: new Adapter(),
+});
