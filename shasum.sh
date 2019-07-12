0b99bbb4c43626b201376f966483b521d73b4ae3
Aaron Cox <aaron@greymass.com>
2019-07-12 17:36:25 -0400

    0.7.10 version bump

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-eos-voter-0.7.9-amd64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.9-amd64.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.9-amd64.snap"
-# shasum -b -a 512 linux-eos-voter-0.7.9-amd64.snap
-echo "shasum -b -a 512 linux-eos-voter-0.7.9-arm64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.9-arm64.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.9-armv7l.deb"
-shasum -b -a 512 linux-eos-voter-0.7.9-armv7l.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.9-i386.deb"
-# shasum -b -a 512 linux-eos-voter-0.7.9-i386.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.9-x86_64.AppImage"
-shasum -b -a 512 linux-eos-voter-0.7.9-x86_64.AppImage
-echo "shasum -b -a 512 mac-eos-voter-0.7.9.dmg"
-shasum -b -a 512 mac-eos-voter-0.7.9.dmg
-echo "shasum -b -a 512 mac-eos-voter-0.7.9.zip"
-shasum -b -a 512 mac-eos-voter-0.7.9.zip
-echo "shasum -b -a 512 win-eos-voter-0.7.9.exe"
-shasum -b -a 512 win-eos-voter-0.7.9.exe
+echo "shasum -b -a 512 linux-eos-voter-0.7.10-amd64.deb"
+shasum -b -a 512 linux-eos-voter-0.7.10-amd64.deb
+# echo "shasum -b -a 512 linux-eos-voter-0.7.10-amd64.snap"
+# shasum -b -a 512 linux-eos-voter-0.7.10-amd64.snap
+echo "shasum -b -a 512 linux-eos-voter-0.7.10-arm64.deb"
+shasum -b -a 512 linux-eos-voter-0.7.10-arm64.deb
+echo "shasum -b -a 512 linux-eos-voter-0.7.10-armv7l.deb"
+shasum -b -a 512 linux-eos-voter-0.7.10-armv7l.deb
+# echo "shasum -b -a 512 linux-eos-voter-0.7.10-i386.deb"
+# shasum -b -a 512 linux-eos-voter-0.7.10-i386.deb
+echo "shasum -b -a 512 linux-eos-voter-0.7.10-x86_64.AppImage"
+shasum -b -a 512 linux-eos-voter-0.7.10-x86_64.AppImage
+echo "shasum -b -a 512 mac-eos-voter-0.7.10.dmg"
+shasum -b -a 512 mac-eos-voter-0.7.10.dmg
+echo "shasum -b -a 512 mac-eos-voter-0.7.10.zip"
+shasum -b -a 512 mac-eos-voter-0.7.10.zip
+echo "shasum -b -a 512 win-eos-voter-0.7.10.exe"
+shasum -b -a 512 win-eos-voter-0.7.10.exe
 
 cd ..
