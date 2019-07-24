da65752123bb307fd3fbbb588dd5e460de353ad0
dafuga <danielfugere28@gmail.com>
2019-07-23 21:50:50 -0700

    fix: receiving maturities date as UTC

--- app/shared/components/Rex/utils/fetchMaturedBalance.js
+++ app/shared/components/Rex/utils/fetchMaturedBalance.js
@@ -3,11 +3,13 @@ import { get } from 'dot-prop-immutable';
 export default function fetchMaturedBalance(tables, account) {
   const rexMaturities = get(tables, `eosio.eosio.rexbal.${account}.rows.0.rex_maturities`, []);
   let maturedMaturitiesTotal = 0;
+
   rexMaturities.forEach(maturity => {
-    if (new Date(maturity.first) < new Date()) {
+    if (new Date(`${maturity.first}Z`) < new Date()) {
       maturedMaturitiesTotal += Number(maturity.second);
     }
   });
+
   const maturedRexNumber = Number(get(tables, `eosio.eosio.rexbal.${account}.rows.0.matured_rex`, 0));
 
   return `${(maturedRexNumber + maturedMaturitiesTotal) / 10000} REX`;
