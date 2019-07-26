7dd6b39e59cba0596d823a0d1892478e9c313b83
Aaron Cox <aaron@greymass.com>
2019-07-26 01:08:00 -0400

    Adding path storage and comments

--- app/shared/reducers/storage.js
+++ app/shared/reducers/storage.js
@@ -1,8 +1,12 @@
 import * as types from '../actions/types';
 
 const initialState = {
+  // data storage
   data: null,
+  // all usable keys
   keys: [],
+  // a mapping of public key -> path for hardware devices
+  paths: {},
 };
 
 export default function storage(state = initialState, action = {}) {
