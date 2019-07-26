dbe1f36e97e5c87ab200042db945f819c4bdb99e
Aaron Cox <aaron@greymass.com>
2019-07-26 01:11:02 -0400

    Use new Import panel for both UIs

--- app/modules/main/containers/Sections/Home/Accounts.js
+++ app/modules/main/containers/Sections/Home/Accounts.js
@@ -10,10 +10,7 @@ import compose from 'lodash/fp/compose';
 import { setSetting } from '../../../../../shared/actions/settings';
 import { swapBlockchain } from '../../../../../shared/actions/blockchains';
 
-import GlobalModalAccountImportCold from '../../../../../shared/containers/Global/Account/Import/Cold';
-import GlobalModalAccountImportHot from '../../../../../shared/containers/Global/Account/Import/Hot';
-import GlobalModalAccountImportLedger from '../../../../../shared/containers/Global/Account/Import/Ledger';
-import GlobalModalAccountImportWatch from '../../../../../shared/containers/Global/Account/Import/Watch';
+import GlobalAccountImport from '../../../../../shared/containers/Global/Account/Import';
 
 class HomeAccountsContainer extends Component<Props> {
   onClose = () => {
@@ -22,86 +19,9 @@ class HomeAccountsContainer extends Component<Props> {
     } = this.props;
     history.push('/');
   }
-  getPanes = () => {
-    const {
-      settings,
-      t
-    } = this.props;
-    const panes = [];
-
-    const ledgerWallet = {
-      menuItem: {
-        key: 'ledger',
-        icon: 'usb',
-        content: t('global_modal_account_import_ledger_wallet'),
-      },
-      render: () => (
-        <Tab.Pane>
-          <GlobalModalAccountImportLedger onClose={this.onClose} />
-        </Tab.Pane>
-      )
-    };
-    const hotWallet = {
-      menuItem: {
-        key: 'hot',
-        icon: 'id card',
-        content: t('global_modal_account_import_hot_wallet'),
-      },
-      render: () => <GlobalModalAccountImportHot onClose={this.onClose} />
-    };
-    const watchWallet = {
-      menuItem: {
-        key: 'watch',
-        icon: 'eye',
-        content: t('global_modal_account_import_watch_wallet'),
-      },
-      render: () => <GlobalModalAccountImportWatch onClose={this.onClose} />
-    };
-    const coldWallet = {
-      menuItem: {
-        key: 'cold',
-        icon: 'snowflake',
-        content: t('global_modal_account_import_cold_wallet'),
-      },
-      render: () => <GlobalModalAccountImportCold onClose={this.onClose} />
-    };
-
-    switch (settings.walletMode) {
-      case 'cold': {
-        panes.push(coldWallet);
-        break;
-      }
-      default: {
-        panes.push(hotWallet, watchWallet, ledgerWallet);
-      }
-    }
-
-    return panes;
-  }
   render() {
-    const panes = this.getPanes();
     return (
-      <React.Fragment>
-        <Segment attached="top" style={{ marginTop: 0 }}>
-          <Header
-            content="Setup an account on this blockchain."
-            subheader="Choose one of the following methods to import or create an account."
-          />
-        </Segment>
-        <Tab
-          menu={{
-            attached: true,
-            inverted: true,
-            fluid: true,
-            pointing: true,
-            style: {
-              margin: 0
-            }
-          }}
-          panes={panes}
-          defaultActiveIndex={0}
-        />
-      </React.Fragment>
+      <GlobalAccountImport onClose={this.onClose} />
     );
   }
 }
