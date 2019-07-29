d2f931432b85da52e7ac038cdc11225926b2c212
dafuga <danielfugere28@gmail.com>
2019-07-29 12:51:04 -0700

    refactor: using componentWillReceiveProps instead of componentWillUpdate

--- app/shared/components/Wallet/Panel/Unlocked.js
+++ app/shared/components/Wallet/Panel/Unlocked.js
@@ -35,13 +35,13 @@ class WalletPanelUnlocked extends Component<Props> {
     }
   }
 
-  componentDidUpdate(prevProps) {
+  componentWillReceiveProps(nextProps) {
     const { connection } = this.props;
 
-    if (connection.chainId !== prevProps.connection.chainId) {
+    if (connection.chainId !== nextProps.connection.chainId) {
       this.setState({
         isWaxChain:
-          connection.chainId ===
+          nextProps.connection.chainId ===
            '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4'
       });
     }
