c28776aaf73faeafc24c40c5453cae7a9180c80d
Aaron Cox <aaron@greymass.com>
2019-07-30 01:55:55 -0400

    Fixed quotes

--- config/jest/fileTransform.js
+++ config/jest/fileTransform.js
@@ -1,7 +1,7 @@
-const path = require(‘path’);
+const path = require(`path`);
 
 module.exports = {
   process(src, filename) {
-    return ‘module.exports = ’ + JSON.stringify(path.basename(filename)) + ‘;’;
+    return `module.exports = ` + JSON.stringify(path.basename(filename)) + `;`;
   },
 };
