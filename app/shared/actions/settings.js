085005d9b5fe6586cd7be0fb6a6bd010eb175eea
Aaron Cox <aaron@greymass.com>
2019-09-16 18:32:59 -0400

    Clear balances on swap

--- app/shared/actions/settings.js
+++ app/shared/actions/settings.js
@@ -63,6 +63,7 @@ export function setSettingWithValidation(key, value) {
       case 'node': {
         // If nodes are changing, force clear any locally cached data
         dispatch({ type: types.CLEAR_ACCOUNT_CACHE });
+        dispatch({ type: types.CLEAR_BALANCE_CACHE });
         dispatch({ type: types.CLEAR_PRODUCER_CACHE });
         dispatch(validate.validateNode(value, false, true, true, true));
         break;
