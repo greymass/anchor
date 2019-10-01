8032211df37ee332f3e187f0c1b56f4f162b0296
Aaron Cox <aaron@greymass.com>
2019-10-01 18:10:12 -0400

    Stake/Unstake controls

---
+++ app/shared/components/Global/Form/Token/Unstake.js
@@ -0,0 +1,122 @@
+// @flow
+import React, { Component } from 'react';
+import { Button, Header, Form, Grid, Table } from 'semantic-ui-react';
+import { translate } from 'react-i18next';
+
+import GlobalAccountFragmentResourceStakedSelf from '../../../../containers/Global/Account/Fragment/Resource/Staked/Self';
+import GlobalFormFieldToken from '../../../../components/Global/Form/Field/Token';
+
+export class GlobalFormTokenUnstake extends Component<Props> {
+  state = {
+    value: false,
+    valid: false,
+  }
+  onChange = (e, { value, valid }) => this.setState({ value, valid });
+  onCancel = () => {
+    if (this.props.onClose) {
+      this.props.onClose();
+    }
+  }
+  onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      this.onSubmit(e);
+      e.preventDefault();
+      return false;
+    }
+  }
+  onSubmit = () => {
+    const { account, resource } = this.props;
+    const { undelegatebw } = this.props.actions;
+    const { valid, value } = this.state;
+    if (valid) {
+      switch (resource) {
+        default:
+        case 'CPU':
+          undelegatebw(account, account, false, value);
+          break;
+        case 'NET':
+          undelegatebw(account, account, value, false);
+          break;
+      }
+    }
+  }
+  render() {
+    const {
+      account,
+      connection,
+      resource,
+    } = this.props;
+    const {
+      valid,
+    } = this.state;
+    return (
+      <Form onKeyPress={this.onKeyPress}>
+        <Grid padded="horizontally">
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <GlobalFormFieldToken
+                autoFocus
+                connection={connection}
+                key="amount"
+                label={(
+                  <Header>
+                    Remove Tokens
+                    <Header.Subheader>
+                      Enter the amount of tokens you would like to remove
+                      {' '}
+                      from your staked {resource.toUpperCase()} balance.
+                    </Header.Subheader>
+                  </Header>
+                )}
+                name="amount"
+                onChange={this.onChange}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Table definition textAlign="right">
+                <Table.Row>
+                  <Table.Cell>Account</Table.Cell>
+                  <Table.Cell>
+                    {account}
+                  </Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Staked {resource.toUpperCase()} Tokens</Table.Cell>
+                  <Table.Cell>
+                    <GlobalAccountFragmentResourceStakedSelf
+                      account={account}
+                      chainId={connection.chainId}
+                      contract="eosio"
+                      token={connection.chainSymbol}
+                      type={resource}
+                    />
+                  </Table.Cell>
+                </Table.Row>
+              </Table>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Button
+                content="Cancel"
+                onClick={this.onCancel}
+              />
+            </Grid.Column>
+            <Grid.Column>
+              <Button
+                content="Stake"
+                disabled={!valid}
+                floated="right"
+                icon="circle plus"
+                primary
+                onClick={this.onSubmit}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Form>
+    );
+  }
+}
+
+export default translate('global')(GlobalFormTokenUnstake);
