b389fe2166db37fb263bfeca7aa714ed62c9cc42
Aaron Cox <aaron@greymass.com>
2019-10-15 17:43:16 -0400

    Altering cold wallet signer to support new key structure

--- app/modules/main/containers/Sections/ColdWallet.js
+++ app/modules/main/containers/Sections/ColdWallet.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import { map } from 'lodash';
 
 import ColdWallet from '../../../../shared/components/ColdWallet/Wallet';
 import * as TransactionActions from '../../../../shared/actions/transaction';
@@ -17,6 +18,10 @@ class ColdWalletContainer extends Component<Props> {
 
 function mapStateToProps(state) {
   return {
+    pubkeys: {
+      available: state.storage.keys,
+      unlocked: map(state.auths.keystore, 'pubkey')
+    },
     settings: state.settings,
     system: state.system,
     transaction: state.transaction,
