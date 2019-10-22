b6ad8deab98025561c947ce42f5ab75caad25853
Aaron Cox <aaron@greymass.com>
2019-10-22 16:31:22 -0400

    Bumping to 1.0.0-rc1

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-eos-voter-0.7.11-amd64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.11-amd64.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.11-amd64.snap"
-# shasum -b -a 512 linux-eos-voter-0.7.11-amd64.snap
-echo "shasum -b -a 512 linux-eos-voter-0.7.11-arm64.deb"
-shasum -b -a 512 linux-eos-voter-0.7.11-arm64.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.11-armv7l.deb"
-shasum -b -a 512 linux-eos-voter-0.7.11-armv7l.deb
-# echo "shasum -b -a 512 linux-eos-voter-0.7.11-i386.deb"
-# shasum -b -a 512 linux-eos-voter-0.7.11-i386.deb
-echo "shasum -b -a 512 linux-eos-voter-0.7.11-x86_64.AppImage"
-shasum -b -a 512 linux-eos-voter-0.7.11-x86_64.AppImage
-echo "shasum -b -a 512 mac-eos-voter-0.7.11.dmg"
-shasum -b -a 512 mac-eos-voter-0.7.11.dmg
-echo "shasum -b -a 512 mac-eos-voter-0.7.11.zip"
-shasum -b -a 512 mac-eos-voter-0.7.11.zip
-echo "shasum -b -a 512 win-eos-voter-0.7.11.exe"
-shasum -b -a 512 win-eos-voter-0.7.11.exe
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-amd64.deb"
+shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-amd64.deb
+# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-amd64.snap"
+# shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-amd64.snap
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-arm64.deb"
+shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-arm64.deb
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-armv7l.deb"
+shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-armv7l.deb
+# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-i386.deb"
+# shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-i386.deb
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-x86_64.AppImage"
+shasum -b -a 512 linux-anchor-beta-1.0.0-rc1-x86_64.AppImage
+echo "shasum -b -a 512 mac-anchor-beta-1.0.0-rc1.dmg"
+shasum -b -a 512 mac-anchor-beta-1.0.0-rc1.dmg
+echo "shasum -b -a 512 mac-anchor-beta-1.0.0-rc1.zip"
+shasum -b -a 512 mac-anchor-beta-1.0.0-rc1.zip
+echo "shasum -b -a 512 win-anchor-beta-1.0.0-rc1.exe"
+shasum -b -a 512 win-anchor-beta-1.0.0-rc1.exe
 
 cd ..
