2db5b5dd528b1fd3bdfa933b2fe35153d3fb0398
Marcin Imielski <mimielski@syncad.com>
2019-10-31 13:44:24 +0100

    add else statement

--- app/shared/actions/stake.js
+++ app/shared/actions/stake.js
@@ -52,94 +52,110 @@ export function setStake(accountName, netAmount, cpuAmount) {
           increaseInStake.cpuAmount
         )
       });
-      if (decreaseInStake.netAmount > 0 || decreaseInStake.cpuAmount > 0) {
-        data.push({
-          name: 'undelegatebw',
-          data: undelegatebwParams(
-            connection.chainSymbol,
-            currentAccount.account_name,
-            accountName,
-            decreaseInStake.netAmount,
-            decreaseInStake.cpuAmount
-          )
-        });
-      }
+    }
+    if (decreaseInStake.netAmount > 0 || decreaseInStake.cpuAmount > 0) {
+      data.push({
+        name: 'undelegatebw',
+        data: undelegatebwParams(
+          connection.chainSymbol,
+          currentAccount.account_name,
+          accountName,
+          decreaseInStake.netAmount,
+          decreaseInStake.cpuAmount
+        )
+      });
+    }
 
-      let serializedArray = [];
-      if (checkForBeos(connection)) {
-        const temp = jurisdictions.choosenJurisdictions.map(obj => obj.code);
-        serializedArray = serializer.serialize(temp);
-      }
-
-      if (data.length > 0) {
-        eos(connection, true).transaction({
-          actions: [{
-            account: 'eosio',
-            name: data[0].name,
-            authorization: [{
-              actor: currentAccount.account_name,
-              permission: 'active'
-            }],
-            data: data[0].data
+    let serializedArray = [];
+    if (checkForBeos(connection)) {
+      const temp = jurisdictions.choosenJurisdictions.map(obj => obj.code);
+      serializedArray = serializer.serialize(temp);
+    }
+    console.log(data);
+    if (data.length > 0) {
+      eos(connection, true).transaction({
+        actions: [{
+          account: 'eosio',
+          name: data[0].name,
+          authorization: [{
+            actor: currentAccount.account_name,
+            permission: 'active'
           }],
-          transaction_extensions: serializedArray
-        }, {
-          broadcast: connection.broadcast,
-          expireInSeconds: connection.expireInSeconds,
-          sign: connection.sign
-        }).then((tx) => {
-          if (data.length > 1) {
-            eos(connection, true).transaction({
-              actions: [{
-                account: 'eosio',
-                name: data[1].name,
-                authorization: [{
-                  actor: currentAccount.account_name,
-                  permission: 'active'
-                }],
-                data: data[1].data
+          data: data[0].data
+        }],
+        transaction_extensions: serializedArray
+      }, {
+        broadcast: connection.broadcast,
+        expireInSeconds: connection.expireInSeconds,
+        sign: connection.sign
+      }).then((tx) => {
+        if (data.length > 1) {
+          eos(connection, true).transaction({
+            actions: [{
+              account: 'eosio',
+              name: data[1].name,
+              authorization: [{
+                actor: currentAccount.account_name,
+                permission: 'active'
               }],
-              transaction_extensions: serializedArray
-            }, {
-              broadcast: connection.broadcast,
-              expireInSeconds: connection.expireInSeconds,
-              sign: connection.sign
-            }).then((tx) => {
-              setTimeout(() => {
-                if (accountName === settings.account) {
-                  dispatch(AccountActions.getAccount(accountName));
-                }
+              data: data[1].data
+            }],
+            transaction_extensions: serializedArray
+          }, {
+            broadcast: connection.broadcast,
+            expireInSeconds: connection.expireInSeconds,
+            sign: connection.sign
+          }).then((tx2) => {
+            setTimeout(() => {
+              if (accountName === settings.account) {
+                dispatch(AccountActions.getAccount(accountName));
+              }
 
-                dispatch(TableActions.getTable('eosio', settings.account, 'delband'));
-              }, 500);
+              dispatch(TableActions.getTable('eosio', settings.account, 'delband'));
+            }, 500);
 
-              return dispatch({
-                payload: {
-                  connection,
-                  tx
-                },
-                type: types.SYSTEM_STAKE_SUCCESS
-              });
-            }).catch((err) => {
-              dispatch({
-                payload: {
-                  connection,
-                  err
-                },
-                type: types.SYSTEM_STAKE_FAILURE
-              });
+            return dispatch({
+              payload: {
+                connection,
+                tx
+              },
+              type: types.SYSTEM_STAKE_SUCCESS
             });
-          }
-        }).catch((err) => {
-          dispatch({
+          }).catch((err) => {
+            dispatch({
+              payload: {
+                connection,
+                err
+              },
+              type: types.SYSTEM_STAKE_FAILURE
+            });
+          });
+        } else {
+          setTimeout(() => {
+            if (accountName === settings.account) {
+              dispatch(AccountActions.getAccount(accountName));
+            }
+
+            dispatch(TableActions.getTable('eosio', settings.account, 'delband'));
+          }, 500);
+
+          return dispatch({
             payload: {
               connection,
-              err
+              tx
             },
-            type: types.SYSTEM_STAKE_FAILURE
+            type: types.SYSTEM_STAKE_SUCCESS
           });
+        }
+      }).catch((err) => {
+        dispatch({
+          payload: {
+            connection,
+            err
+          },
+          type: types.SYSTEM_STAKE_FAILURE
         });
-      }
+      });
     }
   };
 }
