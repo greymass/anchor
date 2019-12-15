8da99a7f7d067857c3b93578b063ca50889e4575
Aaron Cox <aaron@greymass.com>
2019-12-15 14:23:52 -0500

    Improved Fuel support, still not complete

--- app/shared/containers/Global/Fuel/Dropdown.js
+++ app/shared/containers/Global/Fuel/Dropdown.js
@@ -4,12 +4,14 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { translate } from 'react-i18next';
 import compose from 'lodash/fp/compose';
-import { Header, Image, Menu, Popup } from 'semantic-ui-react';
+import { Button, Grid, Header, Image, Menu, Popup, Progress, Segment } from 'semantic-ui-react';
 
 import * as FuelActions from '../../../actions/fuel';
 import * as SettingsActions from '../../../actions/settings';
 
-import FuelLogo from '../../../../renderer/assets/images/fuel/greymassfuel-logo.png';
+import FuelFullLogo from '../../../../renderer/assets/images/fuel/greymassfuel-horizontal.png';
+import FuelLogo from '../../../../renderer/assets/images/fuel/fuel-square-logo.png';
+import FuelLogoDisabled from '../../../../renderer/assets/images/fuel/fuel-square-logo-grey.png';
 
 class GlobalFuelDropdown extends Component<Props> {
   constructor(props) {
@@ -19,27 +21,47 @@ class GlobalFuelDropdown extends Component<Props> {
     };
   }
   componentDidMount = () => {
-    this.props.actions.getFuelQuotaStatus();
+    const { settings } = this.props;
+    if (settings.greymassFuel) {
+      this.props.actions.getFuelQuotaStatus();
+    }
   }
   onClose = () => this.setState({ open: false })
-  onOpen = () => this.setState({ open: true })
+  onOpen = () => {
+    this.setState({ open: true })
+    this.props.actions.getFuelQuotaStatus();
+  }
+  enable = () => this.props.actions.setSetting('greymassFuel', true)
+  disable = () => this.props.actions.setSetting('greymassFuel', false)
   render() {
     const {
+      available,
       enabled,
+      fuel,
     } = this.props;
     const {
       open
     } = this.state;
     // Remove menu item for unsupported chains
-    if (!enabled) {
+    if (!available) {
       return false;
+    }
+    let netpercent = 0;
+    let cpupercent = 0;
+    let quota = false;
+    if (fuel) {
+      ({ quota } = fuel);
+      if (quota) {
+        netpercent = 100 - Math.round((quota.net / quota.quota_net) * 100);
+        cpupercent = 100 - Math.round((quota.cpu / quota.quota_cpu) * 100);
+      }
     }
     return (
       <Popup
         trigger={(
           <Menu.Item>
             <Image
-              src={FuelLogo}
+              src={(enabled) ? FuelLogo : FuelLogoDisabled}
               style={{
                 height: '32px',
                 margin: 0
@@ -48,11 +70,69 @@ class GlobalFuelDropdown extends Component<Props> {
           </Menu.Item>
         )}
         content={(
-          <React.Fragment>
-            <Header>
-              Greymass Fuel
-            </Header>
-          </React.Fragment>
+          <Segment basic>
+            <Image
+              fluid
+              src={FuelFullLogo}
+              style={{
+
+                margin: 0
+              }}
+            />
+            <Segment basic textAlign="center">
+              <p>Enable this feature to take advantage transactions powered by Greymass Fuel.</p>
+              {(enabled)
+                ? (
+                  <Button
+                    basic
+                    content="Disable Fuel"
+                    color="orange"
+                    onClick={this.disable}
+                  />
+
+                )
+                : (
+                  <Button
+                    content="Enable Fuel"
+                    color="green"
+                    onClick={this.enable}
+                  />
+                )
+              }
+            </Segment>
+            {(quota)
+              ? (
+                <Grid>
+                  <Grid.Row columns={1}>
+                    <Grid.Column textAlign="center">
+                      <p>Your quotas remaining are as follows:</p>
+                    </Grid.Column>
+                  </Grid.Row>
+                  <Grid.Row columns={2}>
+                    <Grid.Column>
+                      <Progress
+                        indicating
+                        percent={cpupercent}
+                        progress="percent"
+                      >
+                        CPU Quota
+                      </Progress>
+                    </Grid.Column>
+                    <Grid.Column>
+                      <Progress
+                        indicating
+                        percent={netpercent}
+                        progress="percent"
+                      >
+                        NET Quota
+                      </Progress>
+                    </Grid.Column>
+                  </Grid.Row>
+                </Grid>
+              )
+              : false
+            }
+          </Segment>
         )}
         flowing
         on="click"
@@ -70,7 +150,7 @@ class GlobalFuelDropdown extends Component<Props> {
   }
 }
 
-function isEnabled(connection) {
+function isAvailable(connection) {
   return (
     connection
     && connection.supportedContracts
@@ -78,9 +158,18 @@ function isEnabled(connection) {
   );
 }
 
+function isEnabled(settings) {
+  return (
+    settings
+    && settings.greymassFuel
+  );
+}
+
 function mapStateToProps(state) {
   return {
-    enabled: isEnabled(state.connection),
+    available: isAvailable(state.connection),
+    enabled: (isAvailable(state.connection) && isEnabled(state.settings)),
+    fuel: state.fuel,
     settings: state.settings,
   };
 }
