ff23fcf33827c512d782e39780e5f94228eadca0
dafuga <danielfugere28@gmail.com>
2019-12-17 16:43:11 +0900

    chore: cleaned up the system/rexi.js file

--- app/shared/actions/system/rexi.js
+++ app/shared/actions/system/rexi.js
@@ -162,58 +162,39 @@ async function rexAction(actionName, actionVariable, data, dispatch, getState) {
     connection
   } = getState();
 
+  const { account, authorization } = settings;
+
   dispatch({
     type: types[`SYSTEM_${actionVariable}_PENDING`],
     payload: { connection }
   });
 
-  // hack to get signing working on ledger - needs to be refactored
-  const eosobj = eos(connection, true, true);
-  let method = 'transact';
-  let params = {
-    blocksBehind: 3,
-    expireSeconds: 30,
-  };
-  let contract;
-  if (!eosobj[method]) {
-    contract = await eosobj.getAbi('eosio');
-    if (contract
-      && contract.account_name
-      && contract.abi
-    ) {
-      eosobj.fc.abiCache.abi(contract.account_name, contract.abi);
-    }
-    method = 'transaction';
-    params = {
-      broadcast: connection.broadcast,
-      expireInSeconds: connection.expireInSeconds,
-      sign: connection.sign
-    };
-  }
-
-  return eosobj[method]({
+  return eos(connection, true, true).transact({
     actions: [{
       account: 'eosio',
       name: actionName,
       authorization: [{
-        actor: settings.account,
-        permission: settings.authorization,
+        actor: account,
+        permission: authorization,
       }],
       data,
     }]
-  }, params).then((tx) => {
+  }, {
+    broadcast: connection.broadcast,
+    expireInSeconds: connection.expireInSeconds,
+    sign: connection.sign
+  }).then((tx) => {
     setTimeout(() => {
-      dispatch(getAccount(settings.account));
-      dispatch(getCurrencyBalance(settings.account));
-      dispatch(getTableByBounds('eosio', 'eosio', 'rexbal', settings.account, settings.account));
-      dispatch(getTableByBounds('eosio', 'eosio', 'rexfund', settings.account, settings.account));
+      dispatch(getAccount(account));
+      dispatch(getCurrencyBalance(account));
+      dispatch(getTableByBounds('eosio', 'eosio', 'rexbal', account, account));
+      dispatch(getTableByBounds('eosio', 'eosio', 'rexfund', account, account));
       dispatch(getCPULoans());
       dispatch(getNETLoans());
     }, 1000);
     return dispatch({
       payload: {
         connection,
-        contract,
         tx,
       },
       type: types[`SYSTEM_${actionVariable}_SUCCESS`]
