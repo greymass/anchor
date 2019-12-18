1cca28a39b820618926313af3ace1146c988e4c4
dafuga <danielfugere28@gmail.com>
2019-12-18 16:13:25 +0900

    fix: handling case were proposal vote_json is empty string

--- app/shared/components/Tools/Governance/Proposals/Proposal.js
+++ app/shared/components/Tools/Governance/Proposals/Proposal.js
@@ -57,7 +57,8 @@ class ToolsGovernanceProposalsProposal extends Component<Props> {
     const vote = find(votes, ['proposal_name', proposal_name]);
     const voted = !!(vote);
     const approved = (voted) ? !!(vote.vote) : false;
-    const comment = vote && JSON.parse(vote.vote_json).comment;
+    console.log({vote})
+    const comment = vote && vote.vote_json.length > 0 && JSON.parse(vote.vote_json).comment;
     const isVotePending = !!(system.GOVERNANCE_VOTE_PROPOSAL === 'PENDING' || system.GOVERNANCE_UNVOTE_PROPOSAL === 'PENDING');
     const isSupporting = (voted && approved);
     const isAgainst = (voted && !approved);
