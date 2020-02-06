65c00512ad4dbc56429c17574dc0fdbf3e9cd04f
Aaron Cox <aaron@greymass.com>
2020-02-06 15:58:50 -0500

    RC6 version bump

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-amd64.deb"
-shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-amd64.deb
-# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-amd64.snap"
-# shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-amd64.snap
-echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-arm64.deb"
-shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-arm64.deb
-echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-armv7l.deb"
-shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-armv7l.deb
-# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-i386.deb"
-# shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-i386.deb
-echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-x86_64.AppImage"
-shasum -b -a 512 linux-anchor-beta-1.0.0-rc5-x86_64.AppImage
-echo "shasum -b -a 512 mac-anchor-beta-1.0.0-rc5.dmg"
-shasum -b -a 512 mac-anchor-beta-1.0.0-rc5.dmg
-echo "shasum -b -a 512 mac-anchor-beta-1.0.0-rc5.zip"
-shasum -b -a 512 mac-anchor-beta-1.0.0-rc5.zip
-echo "shasum -b -a 512 win-anchor-beta-1.0.0-rc5.exe"
-shasum -b -a 512 win-anchor-beta-1.0.0-rc5.exe
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-amd64.deb"
+shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-amd64.deb
+# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-amd64.snap"
+# shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-amd64.snap
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-arm64.deb"
+shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-arm64.deb
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-armv7l.deb"
+shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-armv7l.deb
+# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-i386.deb"
+# shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-i386.deb
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-x86_64.AppImage"
+shasum -b -a 512 linux-anchor-beta-1.0.0-rc6-x86_64.AppImage
+echo "shasum -b -a 512 mac-anchor-beta-1.0.0-rc6.dmg"
+shasum -b -a 512 mac-anchor-beta-1.0.0-rc6.dmg
+echo "shasum -b -a 512 mac-anchor-beta-1.0.0-rc6.zip"
+shasum -b -a 512 mac-anchor-beta-1.0.0-rc6.zip
+echo "shasum -b -a 512 win-anchor-beta-1.0.0-rc6.exe"
+shasum -b -a 512 win-anchor-beta-1.0.0-rc6.exe
 
 cd ..
