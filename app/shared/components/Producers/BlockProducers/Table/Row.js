0711406f0107cfb607ab3d63a894258dfd28eada
dafuga <danielfugere28@gmail.com>
2020-02-19 20:59:38 +0900

    cleanup: code cleanup

--- app/shared/components/Producers/BlockProducers/Table/Row.js
+++ app/shared/components/Producers/BlockProducers/Table/Row.js
@@ -33,9 +33,6 @@ class ProducersTableRow extends Component<Props> {
       totalVoteWeight
     } = this.props;
 
-    console.log({producer})
-
-
     const epoch = 946684800000;
     const lastProduced = (producer.last_produced_block_time * 500) + epoch;
     const isActive = (Date.now() - lastProduced) < 1000;
