64684dc7cfcf16ac64e5624bd68397e8add6f8f5
Aaron Cox <aaron@greymass.com>
2020-03-30 17:05:05 -0400

    Updating version to 1.0.0

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-amd64.deb"
-shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-amd64.deb
-# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-amd64.snap"
-# shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-amd64.snap
-echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-arm64.deb"
-shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-arm64.deb
-echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-armv7l.deb"
-shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-armv7l.deb
-# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-i386.deb"
-# shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-i386.deb
-echo "shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-x86_64.AppImage"
-shasum -b -a 512 linux-anchor-beta-1.0.0-rc8-x86_64.AppImage
-echo "shasum -b -a 512 mac-anchor-beta-1.0.0-rc8.dmg"
-shasum -b -a 512 mac-anchor-beta-1.0.0-rc8.dmg
-echo "shasum -b -a 512 mac-anchor-beta-1.0.0-rc8.zip"
-shasum -b -a 512 mac-anchor-beta-1.0.0-rc8.zip
-echo "shasum -b -a 512 win-anchor-beta-1.0.0-rc8.exe"
-shasum -b -a 512 win-anchor-beta-1.0.0-rc8.exe
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-amd64.deb"
+shasum -b -a 512 linux-anchor-beta-1.0.0-amd64.deb
+# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-amd64.snap"
+# shasum -b -a 512 linux-anchor-beta-1.0.0-amd64.snap
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-arm64.deb"
+shasum -b -a 512 linux-anchor-beta-1.0.0-arm64.deb
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-armv7l.deb"
+shasum -b -a 512 linux-anchor-beta-1.0.0-armv7l.deb
+# echo "shasum -b -a 512 linux-anchor-beta-1.0.0-i386.deb"
+# shasum -b -a 512 linux-anchor-beta-1.0.0-i386.deb
+echo "shasum -b -a 512 linux-anchor-beta-1.0.0-x86_64.AppImage"
+shasum -b -a 512 linux-anchor-beta-1.0.0-x86_64.AppImage
+echo "shasum -b -a 512 mac-anchor-beta-1.0.0.dmg"
+shasum -b -a 512 mac-anchor-beta-1.0.0.dmg
+echo "shasum -b -a 512 mac-anchor-beta-1.0.0.zip"
+shasum -b -a 512 mac-anchor-beta-1.0.0.zip
+echo "shasum -b -a 512 win-anchor-beta-1.0.0.exe"
+shasum -b -a 512 win-anchor-beta-1.0.0.exe
 
 cd ..
