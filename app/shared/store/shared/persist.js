7115aaffcb849887e7a9e169fb52919cc4f192a2
dafuga <danielfugere28@gmail.com>
2020-03-31 22:08:38 +0900

    chore: added migration to add blockchain fields

--- app/shared/store/shared/persist.js
+++ app/shared/store/shared/persist.js
@@ -2,12 +2,40 @@ import { createMigrate } from 'redux-persist';
 import createElectronStorage from 'redux-persist-electron-storage';
 
 const migrations = {
+  2: (state) => {
+    let { blockchains } = state;
 
+    blockchains = updateBlockchainData(blockchains, 'fio', {
+      votePrecision: 4,
+    });
+
+    blockchains = updateBlockchainData(blockchains, 'fio-testnet-3', {
+      votePrecision: 4,
+    });
+
+    blockchains = updateBlockchainData(blockchains, 'wax-mainnet', {
+      voteDecayPeriod: 13,
+    });
+
+    blockchains = updateBlockchainData(blockchains, 'telos-mainnet', {
+      oneTokenOneVote: true,
+    });
+
+    blockchains = updateBlockchainData(blockchains, 'telos-testnet-2', {
+      oneTokenOneVote: true,
+    });
+
+    // Update the blockchains state.
+    return {
+      ...state,
+      blockchains,
+    };
+  },
 };
 
 const persistConfig = {
   key: 'anchor-config',
-  version: 1,
+  version: 2,
   migrate: createMigrate(migrations, { debug: true }),
   storage: createElectronStorage(),
   timeout: 0, // The code base checks for falsy, so 0 disables
@@ -21,3 +49,24 @@ const persistConfig = {
 };
 
 export default persistConfig;
+
+function updateBlockchainData(blockchains, chainId, keysToUpdate) {
+  const chainToUpdate = blockchains.find((blockchain) => {
+    return blockchain._id === chainId;
+  });
+
+  if (!chainToUpdate) {
+    return blockchains;
+  }
+
+  const updatedBlockchain = {
+    ...chainToUpdate,
+    ...keysToUpdate,
+  };
+
+  const allOtherChains = blockchains.filter((blockchain) => {
+    return blockchain._id !== chainId;
+  });
+
+  return allOtherChains.concat(updatedBlockchain);
+}
