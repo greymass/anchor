58683e0f41a8d06b24d8715ccb47d71bd0990b35
Aaron Cox <aaron@greymass.com>
2020-04-10 18:28:38 -0400

    Updated a few more i18next components

--- app/shared/components/Producers/BlockProducers/Selector/Item/Empty.js
+++ app/shared/components/Producers/BlockProducers/Selector/Item/Empty.js
@@ -1,37 +1,34 @@
 // @flow
 import React, { Component } from 'react';
-import { I18n } from 'react-i18next';
+import { withTranslation } from 'react-i18next';
 import { List, Message } from 'semantic-ui-react';
 
-export default class ProducersSelectorItemEmpty extends Component<Props> {
+class ProducersSelectorItemEmpty extends Component<Props> {
   render() {
     const {
       modified,
+      t,
     } = this.props;
     return (
-      <I18n ns="producers">
-        {
-          (t) => (
-            <List.Item>
-              {(modified)
-                ? (
-                  <Message
-                    header={t('producer_voter_remove_all_votes_header')}
-                    content={t('producer_voter_remove_all_votes_subheader')}
-                  />
-                )
-                : (
-                  <Message
-                    header={t('producer_voter_no_producers_votes_cast_header')}
-                    content={t('producer_voter_no_producers_votes_cast_subheader')}
-                    size="small"
-                  />
-                )
-              }
-            </List.Item>
+      <List.Item>
+        {(modified)
+          ? (
+            <Message
+              header={t('producer_voter_remove_all_votes_header')}
+              content={t('producer_voter_remove_all_votes_subheader')}
+            />
+          )
+          : (
+            <Message
+              header={t('producer_voter_no_producers_votes_cast_header')}
+              content={t('producer_voter_no_producers_votes_cast_subheader')}
+              size="small"
+            />
           )
         }
-      </I18n>
+      </List.Item>
     );
   }
 }
+
+export default withTranslation()(ProducersSelectorItemEmpty)
