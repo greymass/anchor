b89f7a2bad99d5effd5f84284dd7d0fe8769a034
Aaron Cox <aaron@greymass.com>
2020-04-10 19:01:18 -0400

    More i18n updates

--- app/shared/components/Wallet/Panel/Form/Modal/Confirm.js
+++ app/shared/components/Wallet/Panel/Form/Modal/Confirm.js
@@ -1,9 +1,9 @@
 // @flow
 import React, { Component } from 'react';
-import { I18n } from 'react-i18next';
+import { withTranslation } from 'react-i18next';
 import { Button, Form, Header, Icon, Input, Modal } from 'semantic-ui-react';
 
-export default class WalletPanelFormModalConfirm extends Component<Props> {
+class WalletPanelFormModalConfirm extends Component<Props> {
   state = {
     password: ''
   }
@@ -30,67 +30,63 @@ export default class WalletPanelFormModalConfirm extends Component<Props> {
       onCancel,
       onConfirm,
       onSubmit,
-      password
+      password,
+      t
     } = this.props;
     const matches = (this.state.password === password);
     return (
-      <I18n ns="wallet">
-        {
-          (t) => (
-            <Modal
-              centered={false}
-              trigger={
-                button ||
-                (
-                  <Button
-                    color="blue"
-                    content={buttonText || t('wallet_panel_password_confirm_button')}
-                    floated={floated}
-                    disabled={disabled}
-                    onClick={onConfirm}
-                    size="small"
-                  />
-                )
-              }
-              onClose={onCancel}
-              open={open}
-              size="tiny"
-              style={{ height: 'auto' }}
-            >
-              <Header icon="lock" content={t('wallet_panel_password_confirm_title')} />
-              <Modal.Content>
-                <h3>{t('wallet_panel_password_confirm_description')}</h3>
-                <Form>
-                  <Form.Field
-                    autoFocus
-                    control={Input}
-                    icon={matches ? 'checkmark' : 'x'}
-                    label={t('wallet_panel_password_label')}
-                    onChange={this.compare}
-                    onKeyPress={this.onKeyPress}
-                    type="password"
-                  />
-                </Form>
-              </Modal.Content>
-              <Modal.Actions>
-                <Button
-                  onClick={onCancel}
-                >
-                  <Icon name="x" /> {t('cancel')}
-                </Button>
-                <Button
-                  color="green"
-                  disabled={!matches}
-                  onClick={onSubmit}
-                >
-                  <Icon name="checkmark" /> {buttonText || t('wallet_panel_wallet_save')}
-                </Button>
-              </Modal.Actions>
-            </Modal>
-
+      <Modal
+        centered={false}
+        trigger={
+          button ||
+          (
+            <Button
+              color="blue"
+              content={buttonText || t('wallet_panel_password_confirm_button')}
+              floated={floated}
+              disabled={disabled}
+              onClick={onConfirm}
+              size="small"
+            />
           )
         }
-      </I18n>
+        onClose={onCancel}
+        open={open}
+        size="tiny"
+        style={{ height: 'auto' }}
+      >
+        <Header icon="lock" content={t('wallet_panel_password_confirm_title')} />
+        <Modal.Content>
+          <h3>{t('wallet_panel_password_confirm_description')}</h3>
+          <Form>
+            <Form.Field
+              autoFocus
+              control={Input}
+              icon={matches ? 'checkmark' : 'x'}
+              label={t('wallet_panel_password_label')}
+              onChange={this.compare}
+              onKeyPress={this.onKeyPress}
+              type="password"
+            />
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            onClick={onCancel}
+          >
+            <Icon name="x" /> {t('cancel')}
+          </Button>
+          <Button
+            color="green"
+            disabled={!matches}
+            onClick={onSubmit}
+          >
+            <Icon name="checkmark" /> {buttonText || t('wallet_panel_wallet_save')}
+          </Button>
+        </Modal.Actions>
+      </Modal>
     );
   }
 }
+
+export default withTranslation('wallet')(WalletPanelFormModalConfirm)
