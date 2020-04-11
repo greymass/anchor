cfbb42e6dc92f35d2eabe220061043093f5b2cb6
Aaron Cox <aaron@greymass.com>
2020-04-10 20:03:28 -0400

    Added suspense to prompt root

--- app/modules/handler/containers/Root.js
+++ app/modules/handler/containers/Root.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 
@@ -18,7 +18,9 @@ export default class Root extends Component<Props> {
   render = () => (
     <I18nextProvider i18n={i18n}>
       <Provider store={store}>
-        <Prompt />
+        <Suspense fallback={<div>Loading</div>}>
+          <Prompt />
+        </Suspense>
       </Provider>
     </I18nextProvider>
   );
