6920e15cb19f9e6f34acba8d8bb844cef6223799
dafuga <danielfugere28@gmail.com>
2020-05-03 11:35:59 +0900

    cleanup: code cleanup

--- app/shared/components/Global/Message/App/Error.js
+++ app/shared/components/Global/Message/App/Error.js
@@ -1,16 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 
-const { withTranslation, translate } = require('react-i18next')
-const i18n = require('react-i18next')
-
-// import i18n from 'react-i18next';
-// console.log({withTranslation});
-// console.log({translate})
-// console.log({ json: JSON.stringify(i18n)})
-// console.log({version: require('react-i18next').version})
-console.dir(Object.keys(require('react-i18next')));
-console.log(require.resolve('react-i18next'));
+import { withTranslation } from 'react-i18next';
 
 import { Message, Header, Segment } from 'semantic-ui-react';
 
@@ -18,26 +9,29 @@ class FormMessageError extends Component<Props> {
   render() {
     const {
       error,
-    } = this.props
+    } = this.props;
 
     return (
       <Segment>
         <Header>An error occured while loading the app:</Header>
-        {typeof error === 'object' ? (
-          <Message negative>
-            <Header>{error.message}</Header>
-            <p>{error.stack}</p>
-          </Message>
-        ) : (
-          <Message negative>
-            <Header>
-              {error}
-            </Header>
-          </Message>
-        )}
+        {typeof error === 'object'
+          ? (
+            <Message negative>
+              <Header>{error.message}</Header>
+              <p>{error.stack}</p>
+            </Message>
+          )
+          : (
+            <Message negative>
+              <Header>
+                {error}
+              </Header>
+            </Message>
+          )
+        }
       </Segment>
     );
   }
 }
 
-export default FormMessageError;
+export default withTranslation('global')(FormMessageError);
