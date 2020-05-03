99253bf1970d6fa524def14a19c47ba705568ae1
dafuga <danielfugere28@gmail.com>
2020-05-03 11:20:48 +0900

    enhancement: using Trans component instead of dangerously set html for locales with tags

--- app/shared/utils/htmlDecode.js
+++
@@ -1,5 +0,0 @@
-module.exports = function htmlDecode(content) {
-  const e = document.createElement('div');
-  e.innerHTML = content;
-  return e.childNodes.length === 0 ? '' : e.childNodes[0].nodeValue;
-};
