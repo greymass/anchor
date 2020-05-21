a74afb81fa1042596505c133af66549b65e397bf
Aaron Cox <aaron@greymass.com>
2020-05-21 11:22:37 -0400

    Added QR code to private keys for mobile wallet imports

--- app/shared/components/Global/Form/Field/Key/Private.js
+++ app/shared/components/Global/Form/Field/Key/Private.js
@@ -1,7 +1,8 @@
 // @flow
 import React, { Component } from 'react';
 import { withTranslation } from 'react-i18next';
-import { Checkbox, Form, Input } from 'semantic-ui-react';
+import { Button, Checkbox, Form, Header, Icon, Input, Modal, Segment } from 'semantic-ui-react';
+import QRCode from 'qrcode';
 
 import debounce from 'lodash/debounce';
 
@@ -9,9 +10,27 @@ const ecc = require('eosjs-ecc');
 
 class GlobalFormFieldKeyPrivate extends Component<Props> {
   state = {
+    qrcode: false,
+    qrvisible: false,
     value: '',
     visible: false
   };
+  qrCodePrompt = () => {
+    this.setState({ qrcode: true });
+  }
+  qrCodeVisible = () => {
+    this.setState({ qrvisible: true }, () => {
+      this.renderQRCode();
+    });
+  }
+  async renderQRCode() {
+    const { canvas } = this;
+    QRCode.toCanvas(canvas, this.props.value, {
+      scale: 6
+    }, (error) => {
+      if (error) console.error(error);
+    });
+  }
   onChange = debounce((e, { name, value }) => {
     const parsed = value.trim();
     this.setState({
@@ -48,16 +67,79 @@ class GlobalFormFieldKeyPrivate extends Component<Props> {
       loading,
       name,
       placeholder,
+      qr,
       t,
       value
     } = this.props;
     const {
-      visible
+      qrcode,
+      visible,
     } = this.state;
     return (
       <React.Fragment>
+        {(qrcode)
+          ? (
+            <Modal
+              closeIcon
+              open
+              onClose={() => this.setState({ qrcode: false })}
+            >
+              <Modal.Header>
+                Private Key Export
+              </Modal.Header>
+              <Modal.Content>
+                <Segment
+                  basic
+                  textAlign="center"
+                >
+                  {(this.state.qrvisible)
+                    ? (
+                      <React.Fragment>
+                        <Header>
+                          Private Key as QR Code
+                          <Header.Subheader>
+                            Use this QR Code to import your private keys into a mobile wallet.
+                          </Header.Subheader>
+                        </Header>
+                        <canvas ref={(node) => { this.canvas = node; }} />
+                      </React.Fragment>
+                    )
+                    : (
+                      <React.Fragment>
+                        <Header icon size="large">
+                          <Icon name="eye" />
+                          <Header.Content>
+                            Before proceeding, ensure your screen is not visible to anyone else
+                            <Header.Subheader>
+                              The QR Code that will be displayed here contains your private key. Anyone else who manages to view this QR code could gain access to your private keys and compromise your account.
+                            </Header.Subheader>
+                          </Header.Content>
+                        </Header>
+                        <Button
+                          color="orange"
+                          content="Show QR Code"
+                          icon="privacy"
+                          onClick={this.qrCodeVisible}
+                        />
+                      </React.Fragment>
+                    )
+                  }
+                </Segment>
+              </Modal.Content>
+            </Modal>
+          )
+          : false
+        }
         <p>
           <Form.Field
+            action={(qr)
+              ? {
+                icon: 'qrcode',
+                onClick: this.qrCodePrompt
+              }
+              : undefined
+            }
+            actionPosition="right"
             autoFocus={autoFocus}
             control={Input}
             defaultValue={value}
