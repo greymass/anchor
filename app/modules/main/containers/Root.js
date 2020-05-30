36ec629a4b43b233ce43495120e059fb7bc6b173
Aaron Cox <aaron@greymass.com>
2020-05-30 01:32:11 -0400

    Reorganized index/root container

--- app/modules/main/containers/Root.js
+++ app/modules/main/containers/Root.js
@@ -1,18 +1,15 @@
 // @flow
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux';
 
-import { configureStore, history } from '../../../shared/store/renderer/configureStore';
 import i18n from '../../../shared/i18n';
 import ScrollToTop from '../../../shared/components/Global/ScrollToTop';
 
 import MessageAppError from '../../../shared/components/Global/Message/App/Error';
 
 import '../../../shared/app.global.css';
-
-const { store } = configureStore();
 
 export default class Root extends Component<Props> {
   state = {};
@@ -29,23 +26,22 @@ export default class Root extends Component<Props> {
 
   render() {
     const { error } = this.state;
-
     const Routes = this.props.routes;
-
+    const { store } = this.props;
     return (error) ? (
       <I18nextProvider i18n={i18n}>
         <MessageAppError error={error} />
       </I18nextProvider>
     ) : (
-      <I18nextProvider i18n={i18n}>
-        <Provider store={store}>
-          <ConnectedRouter history={history} store={store}>
+      <Provider store={store}>
+        <I18nextProvider i18n={i18n}>
+          <ConnectedRouter history={this.props.history} store={this.props.store}>
             <ScrollToTop>
               <Routes />
             </ScrollToTop>
           </ConnectedRouter>
-        </Provider>
-      </I18nextProvider>
+        </I18nextProvider>
+      </Provider>
     );
   }
 }
