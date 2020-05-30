b08e3d944fac96f1edccf8dcf9f6f02939040962
Aaron Cox <aaron@greymass.com>
2020-05-30 01:32:11 -0400

    Reorganized index/root container

--- app/modules/main/index.js
+++ app/modules/main/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
+import { configureStore, history } from '../../shared/store/renderer/configureStore';
 import Root from './containers/Root';
 import Routes from './routes';
 
+const { store } = configureStore();
+
+class Index extends Component<Props> {
+  render() {
+    return (
+      <AppContainer>
+        <Root
+          history={history}
+          routes={this.props.routes}
+          store={store}
+        />
+      </AppContainer>
+    );
+  }
+}
+
+Index.childContextTypes = {
+  persistor: PropTypes.object
+};
+
 const renderApp = routes => {
   render(
-    <AppContainer>
-      <Root routes={routes} />
-    </AppContainer>,
+    <Index routes={routes} />,
     document.getElementById('root')
   );
 };
