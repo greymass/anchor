d3bd609bf4bee188e4959bd121ec9e79126ec1b9
Aaron Cox <aaron@greymass.com>
2020-05-30 01:31:55 -0400

    Fixed persistor

--- app/shared/actions/storage.js
+++ app/shared/actions/storage.js
@@ -1,14 +1,11 @@
 import * as types from './types';
-import { configureStore } from '../store/renderer/configureStore';
-
-const { persistor } = configureStore();
 
 export function setStorage(data) {
   return async (dispatch: () => void) => {
-    // flush persisted state to disk in a few moments
-    setTimeout(() => {
-      persistor.flush();
-    }, 2000);
+    // flush to disk
+    if (window && window.persistor) {
+      setTimeout(window.persistor.flush, 2000);
+    }
     // update store
     return dispatch({
       type: types.WALLET_STORAGE_UPDATE,
