ffd193da10541899b528e37bc5738e59fb6358f5
Aaron Cox <aaron@greymass.com>
2020-06-23 13:57:18 -0400

    Attempted fixes for Scatter experimental imports

--- app/shared/containers/Welcome/Import.js
+++ app/shared/containers/Welcome/Import.js
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import compose from 'lodash/fp/compose';
 import { withTranslation } from 'react-i18next';
-import { Button, Form, Label, Modal, Segment } from 'semantic-ui-react';
+import { Button, Form, Label, Message, Modal, Segment } from 'semantic-ui-react';
 
 import * as AccountsActions from '../../actions/accounts';
 import * as BlockchainsActions from '../../actions/blockchains';
@@ -157,39 +157,8 @@ class WelcomeImportContainer extends Component<Props> {
         accounts,
         keypairs,
       } = keychain;
-      // Restore all defined networks
-      let config = {};
-      networks.forEach((network) => {
-        if (network.blockchain === 'eos') {
-          config = {
-            _id: network.id,
-            chainId: network.chainId,
-            name: network.name,
-            node: `${network.protocol}://${network.host}`,
-            symbol: (network.token) ? network.token.symbol : undefined,
-            testnet: false,
-          };
-          actions.importBlockchainFromBackup(config);
-        }
-      });
-      // Restore all accounts
-      accounts.forEach((account) => {
-        const keypair = find(keypairs, { id: account.keypairUnique });
-        // console.log(account, keypair)
-        const [, , chainId] = account.networkUnique.split(':');
-        const converted = {
-          account: account.name,
-          authority: account.authority,
-          chainId,
-          mode: (keypair.external) ? 'ledger' : 'hot',
-          path: (keypair.external) ? `44'/194'/0'/0/${keypair.external.addressIndex}` : undefined,
-          pubkey: account.publicKey,
-          type: (keypair.external) ? 'ledger' : 'key',
-        };
-        chainIds.push(chainId);
-        // commit to storage
-        actions.importWalletFromBackup(converted);
-      });
+      // Set the wallet hash based on the password to settings
+      actions.setWalletHash(password);
       // convert key storage
       const keyStore = [];
       const keys = [];
@@ -224,20 +193,69 @@ class WelcomeImportContainer extends Component<Props> {
               break;
             }
             default: {
-              console.log('unknown format from scatter', keypair.id);
+              console.log('unknown format from scatter', privateKey.type);
               break;
             }
           }
         }
+      });
+      // Restore all defined networks
+      let config = {};
+      networks.forEach((network) => {
+        if (network.blockchain === 'eos') {
+          const node = blockchains[network.chainId];
+          config = {
+            _id: network.id,
+            chainId: network.chainId,
+            name: network.name,
+            node: node || `${network.protocol}://${network.host}`,
+            symbol: (network.token) ? network.token.symbol : undefined,
+            testnet: false,
+          };
+          actions.importBlockchainFromBackup(config);
+        }
+      });
+      // Restore all accounts
+      accounts.forEach((account) => {
+        const keypair = find(keypairs, { id: account.keypairUnique });
+        const [, , chainId] = account.networkUnique.split(':');
+        const converted = {
+          account: account.name,
+          authority: account.authority,
+          chainId,
+          mode: (keypair.external) ? 'ledger' : 'hot',
+          path: (keypair.external) ? `44'/194'/0'/0/${keypair.external.addressIndex}` : undefined,
+          pubkey: account.publicKey,
+          type: (keypair.external) ? 'ledger' : 'key',
+        };
+        chainIds.push(chainId);
+        // commit to storage
+        actions.importWalletFromBackup(converted);
       });
       // grab the first account as the most recent
       const [recentAccount] = accounts;
       // Initialize Anchor
       actions.setSetting('walletInit', true);
       actions.setSetting('blockchains', chainIds);
-      actions.setWalletHash(password);
-      actions.validateNode(config.node, config.chainId, true, true);
-      actions.useWallet(recentAccount.chainId, recentAccount.name, recentAccount.authorization);
+      let { chainId } = recentAccount;
+      if (!chainId && recentAccount.networkUnique) {
+        const [networkType, , networkId] = recentAccount.networkUnique.split(':');
+        if (networkType === 'eos') {
+          chainId = networkId;
+        }
+      }
+      const auth = recentAccount.authorization || recentAccount.authority;
+      const node = blockchains[chainId];
+      actions.validateNode(node, chainId, true, true);
+      actions.setSettings({
+        account: recentAccount.name,
+        authorization: auth,
+        blockchains: chainIds,
+        chainId,
+        node,
+        walletInit: true,
+      });
+      actions.useWallet(chainId, recentAccount.name, auth);
     } catch (e) {
       this.setState({
         loading: false,
@@ -311,6 +329,12 @@ class WelcomeImportContainer extends Component<Props> {
           </Modal.Header>
           <Modal.Content>
             <Segment basic loading={loading}>
+              <Message
+                color="red"
+                content="Importing Scatter backups is a feature in development and may not work properly. If one of these imports does not work properly, you can reset your wallet through Tools -> Reset Application, and either try again or import your accounts manually by using the private keys."
+                header="PLEASE READ: This is an experimental feature."
+                icon="warning sign"
+              />
               <p>{t('welcome:welcome_import_wallets_scatter_subheader')}</p>
               <Form.Input
                 autoFocus
@@ -387,3 +411,27 @@ export default compose(
   withTranslation('welcome'),
   connect(mapStateToProps, mapDispatchToProps)
 )(WelcomeImportContainer);
+
+const blockchains = {
+  'cbef47b0b26d2b8407ec6a6f91284100ec32d288a39d4b4bbd49655f7c484112': 'https://api.beos.world',
+  'b912d19a6abd2b1b05611ae5be473355d64d95aeff0c09bedc8c166cd6468fe4': 'https://api.testnet.beos.world',
+  'd5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86': 'https://bos.eosn.io',
+  'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906': 'https://eos.greymass.com',
+  'e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473': 'https://jungle.greymass.com',
+  '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840': 'https://jungle3.greymass.com',
+  '5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191': 'https://kylin.eosn.io',
+  '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c': 'https://fio.greymass.com',
+  'b20901380af44ef59c5918439a1f9a41d83669020319a80574b804a5f95cbd7e': 'https://fiotestnet.greymass.com',
+  'b042025541e25a472bffde2d62edd457b7e70cee943412b1ea0f044f88591664': 'https://instar.greymass.com',
+  'b62febe5aadff3d5399090b9565cb420387d3c66f2ccd7c7ac1f532c4f50f573': 'https://lynx.greymass.com/',
+  '0fea517bbfb5b51c564b5c59bcf7f02cf934cfff895f59d0d5cd7079c06fd978': 'https://lynxtestnet.greymass.com/',
+  'cfe6486a83bad4962f232d48003b1824ab5665c36778141034d75e57b956e422': 'https://meetone.eosn.io',
+  '93ece941df27a5787a405383a66a7c26d04e80182adf504365710331ac0625a7': 'https://testchain.remme.io/',
+  '384da888112027f0321850a169f737c33e53b388aad48b5adace4bab97f437e0': 'https://proton.greymass.com/',
+  '71ee83bcf52142d61019d95f9cc5427ba6a0d7ff8accd9e2088ae2abeaf3d3dd': 'https://protontestnet.greymass.com/',
+  '4667b205c6838ef70ff7988f6e8257e8be0e1284a2f59699054a018f743b1d11': 'https://telos.greymass.com',
+  '1eaa0824707c8c16bd25145493bf062aecddfeb56c736f6ba6397f3195f33c9f': 'https://testnet.eos.miami',
+  '73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f': 'https://api.worbli.io',
+  '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4': 'https://wax.greymass.com',
+  'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12': 'https://waxtestnet.greymass.com',
+};
