583454f71e25883a9942ed61093e7351e338e894
Aaron Cox <aaron@greymass.com>
2020-08-25 17:34:55 -0400

    Interval cleanup

--- app/renderer/tray/index.js
+++ app/renderer/tray/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
-
 import Root from '../../shared/containers/Tray';
 import Routes from './routes';
-
 
 const renderApp = routes => {
   render(
