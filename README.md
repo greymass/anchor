1142aee1fb1bc91f015adb9deb97f9e7e4e5d5f6
Aaron Cox <aaron@greymass.com>
2020-12-29 15:55:23 -0500

    Version bump

--- README.md
+++ README.md
@@ -33,12 +33,12 @@ When downloading from either location, check the URL bar of your browser to ensu
 
 ### Current Release
 
-The most recent (v1.1.9) release can downloaded using these links:
+The most recent (v1.1.10) release can downloaded using these links:
 
-- [Windows Installer](https://github.com/greymass/anchor/releases/download/v1.1.9/win-anchor-wallet-1.1.9.exe)
-- [macOS Package](https://github.com/greymass/anchor/releases/download/v1.1.9/mac-anchor-wallet-1.1.9.dmg)
-- [Linux (deb)](https://github.com/greymass/anchor/releases/download/v1.1.9/linux-anchor-wallet-1.1.9-amd64.deb)
-- [Linux (AppImage)](https://github.com/greymass/anchor/releases/download/v1.1.9/linux-anchor-wallet-1.1.9-x86_64.AppImage)
+- [Windows Installer](https://github.com/greymass/anchor/releases/download/v1.1.10/win-anchor-wallet-1.1.10.exe)
+- [macOS Package](https://github.com/greymass/anchor/releases/download/v1.1.10/mac-anchor-wallet-1.1.10.dmg)
+- [Linux (deb)](https://github.com/greymass/anchor/releases/download/v1.1.10/linux-anchor-wallet-1.1.10-amd64.deb)
+- [Linux (AppImage)](https://github.com/greymass/anchor/releases/download/v1.1.10/linux-anchor-wallet-1.1.10-x86_64.AppImage)
 
 The latest release will always be available on the releases page of this repository:
 
--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-anchor-wallet-1.1.9-amd64.deb"
-shasum -b -a 512 linux-anchor-wallet-1.1.9-amd64.deb
-# echo "shasum -b -a 512 linux-anchor-wallet-1.1.9-amd64.snap"
-# shasum -b -a 512 linux-anchor-wallet-1.1.9-amd64.snap
-echo "shasum -b -a 512 linux-anchor-wallet-1.1.9-arm64.deb"
-shasum -b -a 512 linux-anchor-wallet-1.1.9-arm64.deb
-echo "shasum -b -a 512 linux-anchor-wallet-1.1.9-armv7l.deb"
-shasum -b -a 512 linux-anchor-wallet-1.1.9-armv7l.deb
-# echo "shasum -b -a 512 linux-anchor-wallet-1.1.9-i386.deb"
-# shasum -b -a 512 linux-anchor-wallet-1.1.9-i386.deb
-echo "shasum -b -a 512 linux-anchor-wallet-1.1.9-x86_64.AppImage"
-shasum -b -a 512 linux-anchor-wallet-1.1.9-x86_64.AppImage
-echo "shasum -b -a 512 mac-anchor-wallet-1.1.9.dmg"
-shasum -b -a 512 mac-anchor-wallet-1.1.9.dmg
-echo "shasum -b -a 512 mac-anchor-wallet-1.1.9.zip"
-shasum -b -a 512 mac-anchor-wallet-1.1.9.zip
-echo "shasum -b -a 512 win-anchor-wallet-1.1.9.exe"
-shasum -b -a 512 win-anchor-wallet-1.1.9.exe
+echo "shasum -b -a 512 linux-anchor-wallet-1.1.10-amd64.deb"
+shasum -b -a 512 linux-anchor-wallet-1.1.10-amd64.deb
+# echo "shasum -b -a 512 linux-anchor-wallet-1.1.10-amd64.snap"
+# shasum -b -a 512 linux-anchor-wallet-1.1.10-amd64.snap
+echo "shasum -b -a 512 linux-anchor-wallet-1.1.10-arm64.deb"
+shasum -b -a 512 linux-anchor-wallet-1.1.10-arm64.deb
+echo "shasum -b -a 512 linux-anchor-wallet-1.1.10-armv7l.deb"
+shasum -b -a 512 linux-anchor-wallet-1.1.10-armv7l.deb
+# echo "shasum -b -a 512 linux-anchor-wallet-1.1.10-i386.deb"
+# shasum -b -a 512 linux-anchor-wallet-1.1.10-i386.deb
+echo "shasum -b -a 512 linux-anchor-wallet-1.1.10-x86_64.AppImage"
+shasum -b -a 512 linux-anchor-wallet-1.1.10-x86_64.AppImage
+echo "shasum -b -a 512 mac-anchor-wallet-1.1.10.dmg"
+shasum -b -a 512 mac-anchor-wallet-1.1.10.dmg
+echo "shasum -b -a 512 mac-anchor-wallet-1.1.10.zip"
+shasum -b -a 512 mac-anchor-wallet-1.1.10.zip
+echo "shasum -b -a 512 win-anchor-wallet-1.1.10.exe"
+shasum -b -a 512 win-anchor-wallet-1.1.10.exe
 
 cd ..
