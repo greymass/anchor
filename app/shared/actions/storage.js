4237570a50c93b19a2bf44152a57cb976affa2a0
Aaron Cox <aaron@greymass.com>
2021-02-22 16:04:30 -0500

    Implemented key searching and removal

--- app/shared/actions/storage.js
+++ app/shared/actions/storage.js
@@ -1,4 +1,9 @@
+import { find, forEach, partition, pluck, uniq } from 'lodash';
+
 import * as types from './types';
+import { decrypt, encrypt } from './wallet';
+
+const CryptoJS = require('crypto-js');
 
 export function setStorage(data) {
   return async (dispatch: () => void) => {
@@ -14,6 +19,40 @@ export function setStorage(data) {
   };
 }
 
+export function removeKeyFromStorageByPublic(password, pubkey) {
+  return async (dispatch: () => void, getState) => {
+    const { storage } = getState();
+    if (storage.data) {
+      // Decrypt storage
+      const decrypted = JSON.parse(decrypt(storage.data, password).toString(CryptoJS.enc.Utf8));
+
+      // Determine which keys to remove and which to keep
+      const [, keep] = partition(decrypted, (e) => e.pubkey === pubkey);
+
+      // Clone our paths
+      const paths = Object.assign({}, storage.paths);
+
+      // Remove the associated path to this key if it exists
+      if (paths[pubkey]) {
+        delete paths[pubkey];
+      }
+
+      // Determine available public keys based on stored keys and derivation paths
+      const keys = uniq([
+        ...keep.map(k => k.pubkey),
+        ...Object.keys(paths),
+      ]);
+
+      // Encrypt new storage
+      const data = encrypt(JSON.stringify(keep), password);
+
+      // Set the wallet storage to match the new configuration
+      dispatch(setStorage({ data, keys, paths }));
+    }
+  };
+}
+
 export default {
+  removeKeyFromStorageByPublic,
   setStorage
 };
--- app/shared/components/Tools/Keys.js
+++ app/shared/components/Tools/Keys.js
@@ -8,6 +8,7 @@ import {
   Dropdown,
   Header,
   Icon,
+  Input,
   List,
   Message,
   Modal,
@@ -21,22 +22,27 @@ import ToolsKeyGeneratorComponent from './KeyGenerator';
 import ToolsKeyImportComponent from './KeyImport';
 import GlobalFragmentChainLogo from '../Global/Fragment/ChainLogo';
 import GlobalAccountKey from '../../containers/Global/Account/Key';
+import GlobalButtonElevate from '../../containers/Global/Button/Elevate';
 
 const defaultState = {
   closable: true,
   confirm: false,
+  confirmRemove: false,
   open: false,
   openImport: false,
   openKey: false,
   pubkeys: [],
+  searchFilter: '',
 };
 
 class ToolsKeys extends Component<Props> {
   constructor(props) {
     super(props);
-    this.state = Object.assign({}, defaultState)
+    this.state = Object.assign({}, defaultState);
   }
   onCancelConfirm = () => this.setState({ confirm: false })
+  onRemoveConfirm = (pubkey) => this.setState({ confirmRemove: pubkey })
+  onRemoveCancel = () => this.setState({ confirmRemove: false })
   onClose = () => {
     if (this.state.closable) {
       this.setState({ open: false });
@@ -62,16 +68,31 @@ class ToolsKeys extends Component<Props> {
   onHideKey = () => this.setState({ openKey: false })
   onShowKey = (pubkey) => this.setState({ openKey: pubkey })
   onSave = () => this.setState({ closable: true })
+  onRemoveKey = (password, pubkey) => {
+    const { actions } = this.props;
+    actions.removeKeyFromStorageByPublic(password, pubkey);
+    this.setState({ confirmRemove: false });
+  }
+  filterKeys = (e, { value }) => this.setState({ searchFilter: value })
   render() {
     const {
       paths,
       pubkeys,
       t,
+      settings,
+      validate,
+      wallet,
       wallets,
     } = this.props;
     const {
-      openKey
+      confirmRemove,
+      openKey,
+      searchFilter,
     } = this.state;
+    let filtered = [...pubkeys.available];
+    if (searchFilter !== '') {
+      filtered = [...pubkeys.available].filter(k => k.toLowerCase().includes(searchFilter.toLowerCase()));
+    }
     return (
       <Segment color="violet" piled style={{ margin: 0 }}>
         {(openKey)
@@ -80,6 +101,43 @@ class ToolsKeys extends Component<Props> {
               onClose={this.onHideKey}
               pubkey={openKey}
             />
+          )
+          : false
+        }
+        {(confirmRemove)
+          ? (
+            <Modal
+              onClose={() => this.onRemoveCancel()}
+              open={confirmRemove}
+              size="tiny"
+            >
+              <Modal.Header>{t('global:global_account_key_remove_confirm_title')}</Modal.Header>
+              <Modal.Content>
+                <Modal.Description>
+                  <Header>{t('global:global_account_key_remove_confirm_header')}</Header>
+                  <p>{t('global:global_account_key_remove_confirm_desc')}</p>
+                  <p>{confirmRemove}</p>
+                </Modal.Description>
+              </Modal.Content>
+              <Modal.Actions>
+                <Button floated="left" onClick={() => this.onRemoveCancel()}>
+                  {t('cancel')}
+                </Button>
+                <GlobalButtonElevate
+                  onSuccess={(password) => this.onRemoveKey(password, confirmRemove)}
+                  settings={settings}
+                  trigger={(
+                    <Button
+                      color="orange"
+                      content={t('global_account_key_remove_confirm_button')}
+                      icon="warning sign"
+                    />
+                  )}
+                  validate={validate}
+                  wallet={wallet}
+                />
+              </Modal.Actions>
+            </Modal>
           )
           : false
         }
@@ -145,19 +203,22 @@ class ToolsKeys extends Component<Props> {
           <Table.Header>
             <Table.Row>
               <Table.HeaderCell>
-                {t('tools_keys_management_header_cell_one')}
+                <Input
+                  onChange={this.filterKeys}
+                  placeholder="Search by Public Key"
+                />
               </Table.HeaderCell>
-              <Table.HeaderCell>
+              <Table.HeaderCell collapsing>
                 {t('tools_keys_management_header_cell_two')}
               </Table.HeaderCell>
-              <Table.HeaderCell textAlign="center">
+              <Table.HeaderCell textAlign="center" collapsing>
                 {t('tools_keys_management_header_cell_three')}
               </Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {pubkeys.available.map((pubkey) => {
-              const matching = wallets.filter((wallet) => wallet.pubkey === pubkey);
+            {filtered.map((pubkey) => {
+              const matching = wallets.filter((w) => w.pubkey === pubkey);
               return (
                 <Table.Row>
                   <Table.Cell>
@@ -170,6 +231,27 @@ class ToolsKeys extends Component<Props> {
                           onClick={() => this.onShowKey(pubkey)}
                           text={t('global:global_account_key_modal_header')}
                         />
+                        {(matching.length > 0)
+                          ? (
+                            <Popup
+                              content={(
+                                <p>{t('global:global_account_key_remove_unable')}</p>
+                              )}
+                              position="left center"
+                              trigger={(
+                                <Dropdown.Item
+                                  text={t('global:global_account_key_remove')}
+                                />
+                              )}
+                            />
+                          )
+                          : (
+                            <Dropdown.Item
+                              onClick={() => this.onRemoveConfirm(pubkey)}
+                              text={t('global:global_account_key_remove')}
+                            />
+                          )
+                        }
                       </Dropdown.Menu>
                     </Dropdown>
                   </Table.Cell>
@@ -179,17 +261,17 @@ class ToolsKeys extends Component<Props> {
                       <Popup
                         content={(
                           <Table definition size="small">
-                            {matching.map((wallet) => (
+                            {matching.map((w) => (
                               <Table.Row>
                                 <Table.Cell collapsing>
                                   <GlobalFragmentChainLogo
-                                    chainId={wallet.chainId}
+                                    chainId={w.chainId}
                                     noPopup
                                     size="avatar"
                                   />
                                 </Table.Cell>
                                 <Table.Cell>
-                                  {wallet.account}@{wallet.authorization}
+                                  {w.account}@{w.authorization}
                                 </Table.Cell>
                               </Table.Row>
                             ))}
