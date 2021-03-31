dd583afafecbd1b8f7365c93d59bcdd18e456c94
Aaron Cox <aaron@greymass.com>
2021-03-31 12:18:37 -0400

    Only notarize if the values are defined.
    Fixes #1051

--- internals/macos/notarize.js
+++ internals/macos/notarize.js
@@ -9,10 +9,13 @@ exports.default = async function notarizing(context) {
 
   const appName = context.packager.appInfo.productFilename;
 
-  return await notarize({
-    appBundleId: 'com.greymass.anchordesktop.release',
-    appPath: `${appOutDir}/${appName}.app`,
-    appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
-  });
+  if (process.env.APPLEID && process.env.APPLEIDPASS) {
+    return notarize({
+      appBundleId: 'com.greymass.anchordesktop.release',
+      appPath: `${appOutDir}/${appName}.app`,
+      appleId: process.env.APPLEID,
+      appleIdPassword: process.env.APPLEIDPASS,
+    });
+  }
+  console.log('No APPLEID or APPLEIDPASS defined, skipping notarization.');
 };
