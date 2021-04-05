9087efe5244036567f74e88a548b68ef06d86f1f
Aaron Cox <aaron@greymass.com>
2021-04-05 17:50:52 -0400

    Adjustments to use RAM units

---
+++ app/shared/containers/Global/Account/Fragment/Ram/Units.js
@@ -0,0 +1,55 @@
+// @flow
+import { get } from 'dot-prop-immutable';
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { withTranslation } from 'react-i18next';
+import compose from 'lodash/fp/compose';
+import { Icon, Popup } from 'semantic-ui-react';
+
+class GlobalAccountFragmentResourceUnits extends PureComponent<Props> {
+  render() {
+    const {
+      max,
+      settings,
+      used,
+    } = this.props;
+    if (!max || !used) return false;
+    const units = ((max - used) / 1000).toFixed(3);
+    const el = (
+      <span>
+        {units} kb
+      </span>
+    );
+    return (
+      <React.Fragment>
+        {(units < 1)
+          ? (
+            <Popup
+              content="This resource is running low. Consider buying more RAM in order to increase the capacity of this account."
+              trigger={(
+                <span>
+                  <Icon color="yellow" name="warning sign" />
+                  {el}
+                </span>
+              )}
+            />
+          )
+          : el
+        }
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  const account = ownProps.account.replace('.', '\\.');
+  return {
+    used: get(state.accounts, `${account}.ram_usage`),
+    max: get(state.accounts, `${account}.ram_quota`),
+  };
+};
+
+export default compose(
+  withTranslation('global'),
+  connect(mapStateToProps)
+)(GlobalAccountFragmentResourceUnits);
