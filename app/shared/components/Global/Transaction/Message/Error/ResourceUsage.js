7a954bdf107ac955ab57c4f3885b29f8e3f8d0c4
Aaron Cox <aaron@greymass.com>
2021-04-16 12:57:43 -0400

    Fixed reset logic and errors for fee-based transactions

--- app/shared/components/Global/Transaction/Message/Error/ResourceUsage.js
+++ app/shared/components/Global/Transaction/Message/Error/ResourceUsage.js
@@ -14,8 +14,19 @@ import { setSetting } from '../../../../../actions/settings';
 class GlobalTransactionMessageErrorResourceUsage extends PureComponent<Props> {
   componentDidMount = () => {
     const { fuel, settings, system } = this.props;
-    if (settings.transactionFees) {
+    if (fuel.alternative && settings.transactionFees) {
       this.props.actions.retryWithFee(system.latestFailure, fuel.alternative.request);
+    }
+  }
+  componentDidUpdate(prevProps) {
+    const { fuel, settings, system } = this.props;
+    if (
+      !prevProps.fuel.alternative
+      && fuel.alternative
+    ) {
+      if (settings.transactionFees) {
+        this.props.actions.retryWithFee(system.latestFailure, fuel.alternative.request);
+      }
     }
   }
   toggle = (e, { checked }) => {
@@ -32,34 +43,42 @@ class GlobalTransactionMessageErrorResourceUsage extends PureComponent<Props> {
       t
     } = this.props;
     const canProceedWithFee = (fuel && fuel.alternative && fuel.alternative.request && fuel.alternative.request.code === 402);
+    const loading = !fuel || !fuel.alternative;
     return (
-      <Segment basic>
+      <Segment basic loading={loading}>
         <Header
           content="Transaction Fee"
           icon="stop"
           size="large"
           subheader="Your account lacks the network resources for this transaction to succeed."
         />
-        <Segment stacked size="large" textAlign="center">
-          <p>To perform this transaction, you will either need to pay a fee or use the Resources section of the wallet to manage your account resources.</p>
-          <Header
-            content={fuel.alternative.request.data.fee}
-            subheader="Transaction Fee"
-          />
-          <Button
-            content={`Proceed with ${fuel.alternative.request.data.fee} fee`}
-            onClick={this.prompt}
-            primary
-            size="large"
-          />
-          <div style={{ margin: '1em 0' }}>
-            <Checkbox
-              label="Automatically display fee-based transactions."
-              onChange={this.toggle}
-              size="large"
-            />
-          </div>
-        </Segment>
+        {(loading)
+          ? (
+            <p>Loading...</p>
+          )
+          : (
+            <Segment stacked size="large" textAlign="center">
+              <p>To perform this transaction, you will either need to pay a fee or use the Resources section of the wallet to manage your account resources.</p>
+              <Header
+                content={fuel.alternative.request.data.fee}
+                subheader="Transaction Fee"
+              />
+              <Button
+                content={`Proceed with ${fuel.alternative.request.data.fee} fee`}
+                onClick={this.prompt}
+                primary
+                size="large"
+              />
+              <div style={{ margin: '1em 0' }}>
+                <Checkbox
+                  label="Automatically display fee-based transactions."
+                  onChange={this.toggle}
+                  size="large"
+                />
+              </div>
+            </Segment>
+          )
+        }
       </Segment>
     );
   }
