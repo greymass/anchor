ee57ffd429ac397afbc91bc31b3d56ce13949cb0
Aaron Cox <aaron@greymass.com>
2021-07-27 18:23:43 -0400

    Upgrade webpack from 4 to 5

--- babel.config.js
+++ babel.config.js
@@ -28,14 +28,44 @@ module.exports = (api) => {
         modules: 'commonjs',
         debug: true
       }),
+      require('@babel/preset-flow'),
       require('@babel/preset-typescript'),
       [require('@babel/preset-react'), { development }],
-      require('@babel/preset-flow')
     ],
     plugins: [
       require('babel-plugin-add-module-exports'),
       require('@babel/plugin-transform-modules-commonjs'),
+
+      // Stage 0
       require('@babel/plugin-proposal-function-bind'),
+
+      // Stage 1
+      require('@babel/plugin-proposal-export-default-from'),
+      require('@babel/plugin-proposal-logical-assignment-operators'),
+      [require('@babel/plugin-proposal-optional-chaining'), { loose: false }],
+      [
+        require('@babel/plugin-proposal-pipeline-operator'),
+        { proposal: 'minimal' },
+      ],
+      [
+        require('@babel/plugin-proposal-nullish-coalescing-operator'),
+        { loose: false },
+      ],
+      require('@babel/plugin-proposal-do-expressions'),
+
+      // Stage 2
+      [require('@babel/plugin-proposal-decorators'), { legacy: true }],
+      require('@babel/plugin-proposal-function-sent'),
+      require('@babel/plugin-proposal-export-namespace-from'),
+      require('@babel/plugin-proposal-numeric-separator'),
+      require('@babel/plugin-proposal-throw-expressions'),
+
+      // Stage 3
+      require('@babel/plugin-syntax-dynamic-import'),
+      require('@babel/plugin-syntax-import-meta'),
+      require('@babel/plugin-proposal-class-properties'),
+      require('@babel/plugin-proposal-json-strings'),
+
       ...(development ? developmentPlugins : productionPlugins),
     ],
   };
