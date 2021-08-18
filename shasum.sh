7cc67fcacb86c64342cec8053e1a95a5761c1f83
Aaron Cox <aaron@greymass.com>
2021-08-18 17:50:04 -0400

    Updating shasum script since filenames changed.

--- shasum.sh
+++ shasum.sh
@@ -14,10 +14,10 @@ shasum -b -a 512 linux-anchor-wallet-1.2.7-armv7l.deb
 # shasum -b -a 512 linux-anchor-wallet-1.2.7-i386.deb
 echo "shasum -b -a 512 linux-anchor-wallet-1.2.7-x86_64.AppImage"
 shasum -b -a 512 linux-anchor-wallet-1.2.7-x86_64.AppImage
-echo "shasum -b -a 512 mac-anchor-wallet-1.2.7.dmg"
-shasum -b -a 512 mac-anchor-wallet-1.2.7.dmg
-echo "shasum -b -a 512 mac-anchor-wallet-1.2.7.zip"
-shasum -b -a 512 mac-anchor-wallet-1.2.7.zip
+echo "shasum -b -a 512 mac-anchor-wallet-1.2.7-x64.dmg"
+shasum -b -a 512 mac-anchor-wallet-1.2.7-x64.dmg
+echo "shasum -b -a 512 mac-anchor-wallet-1.2.7-x64.zip"
+shasum -b -a 512 mac-anchor-wallet-1.2.7-x64.zip
 echo "shasum -b -a 512 win-anchor-wallet-1.2.7.exe"
 shasum -b -a 512 win-anchor-wallet-1.2.7.exe
 
