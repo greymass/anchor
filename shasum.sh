a6f4c7295781836d6b329e55129224127f4ab840
Aaron Cox <aaron@greymass.com>
2022-04-06 18:36:54 -0400

    version bump

--- shasum.sh
+++ shasum.sh
@@ -2,23 +2,23 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-anchor-wallet-1.3.3-amd64.deb"
-shasum -b -a 512 linux-anchor-wallet-1.3.3-amd64.deb
-# echo "shasum -b -a 512 linux-anchor-wallet-1.3.3-amd64.snap"
-# shasum -b -a 512 linux-anchor-wallet-1.3.3-amd64.snap
-echo "shasum -b -a 512 linux-anchor-wallet-1.3.3-arm64.deb"
-shasum -b -a 512 linux-anchor-wallet-1.3.3-arm64.deb
-echo "shasum -b -a 512 linux-anchor-wallet-1.3.3-armv7l.deb"
-shasum -b -a 512 linux-anchor-wallet-1.3.3-armv7l.deb
-# echo "shasum -b -a 512 linux-anchor-wallet-1.3.3-i386.deb"
-# shasum -b -a 512 linux-anchor-wallet-1.3.3-i386.deb
-echo "shasum -b -a 512 linux-anchor-wallet-1.3.3-x86_64.AppImage"
-shasum -b -a 512 linux-anchor-wallet-1.3.3-x86_64.AppImage
-echo "shasum -b -a 512 mac-anchor-wallet-1.3.3-x64.dmg"
-shasum -b -a 512 mac-anchor-wallet-1.3.3-x64.dmg
-echo "shasum -b -a 512 mac-anchor-wallet-1.3.3-x64.zip"
-shasum -b -a 512 mac-anchor-wallet-1.3.3-x64.zip
-echo "shasum -b -a 512 win-anchor-wallet-1.3.3.exe"
-shasum -b -a 512 win-anchor-wallet-1.3.3.exe
+echo "shasum -b -a 512 linux-anchor-wallet-1.3.4-amd64.deb"
+shasum -b -a 512 linux-anchor-wallet-1.3.4-amd64.deb
+# echo "shasum -b -a 512 linux-anchor-wallet-1.3.4-amd64.snap"
+# shasum -b -a 512 linux-anchor-wallet-1.3.4-amd64.snap
+echo "shasum -b -a 512 linux-anchor-wallet-1.3.4-arm64.deb"
+shasum -b -a 512 linux-anchor-wallet-1.3.4-arm64.deb
+echo "shasum -b -a 512 linux-anchor-wallet-1.3.4-armv7l.deb"
+shasum -b -a 512 linux-anchor-wallet-1.3.4-armv7l.deb
+# echo "shasum -b -a 512 linux-anchor-wallet-1.3.4-i386.deb"
+# shasum -b -a 512 linux-anchor-wallet-1.3.4-i386.deb
+echo "shasum -b -a 512 linux-anchor-wallet-1.3.4-x86_64.AppImage"
+shasum -b -a 512 linux-anchor-wallet-1.3.4-x86_64.AppImage
+echo "shasum -b -a 512 mac-anchor-wallet-1.3.4-x64.dmg"
+shasum -b -a 512 mac-anchor-wallet-1.3.4-x64.dmg
+echo "shasum -b -a 512 mac-anchor-wallet-1.3.4-x64.zip"
+shasum -b -a 512 mac-anchor-wallet-1.3.4-x64.zip
+echo "shasum -b -a 512 win-anchor-wallet-1.3.4.exe"
+shasum -b -a 512 win-anchor-wallet-1.3.4.exe
 
 cd ..
