c7276f3406fae5a918ec330fb87f5190f25306a2
Aaron Cox <aaron@greymass.com>
2022-09-20 14:13:52 -0400

    Enable broadcast when advanced settings enabled.

--- app/modules/main/containers/Sidebar.js
+++ app/modules/main/containers/Sidebar.js
@@ -322,7 +322,7 @@ class SidebarContainer extends Component<Props> {
         {(
           settings.walletInit
           && settings.chainId
-          && !inSetup
+          && (!inSetup || (inSetup && settings.advancedOptions))
         )
           ? (
             <WalletPanelButtonBroadcast
