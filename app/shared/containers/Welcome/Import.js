a94fc94695c96a674c64f14b80eff5955ef7ac66
Ben Sigman <bensig@gmail.com>
2022-10-11 15:32:51 -0700

    add libre mainnet and testnet

--- app/shared/containers/Welcome/Import.js
+++ app/shared/containers/Welcome/Import.js
@@ -446,4 +446,6 @@ const blockchains = {
   '73647cde120091e0a4b85bced2f3cfdb3041e266cbbe95cee59b73235a1b3b6f': 'https://api.worbli.io',
   '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4': 'https://wax.greymass.com',
   'f16b1833c747c43682f4386fca9cbb327929334a762755ebec17f6f23c9b8a12': 'https://waxtestnet.greymass.com',
+  'b64646740308df2ee06c6b72f34c0f7fa066d940e831f752db2006fcc2b78dee': 'https://testnet.libre.org',
+  '38b1d7815474d0c60683ecbea321d723e83f5da6ae5f1c1f9fecc69d9ba96465': 'https://lb.libre.org',
 };
