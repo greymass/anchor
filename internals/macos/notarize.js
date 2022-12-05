01223cb6826c93c3aa17a146f6e36e551cb11b35
Aaron Cox <aaron@greymass.com>
2022-12-05 17:39:05 -0500

    formatting

--- internals/macos/notarize.js
+++ internals/macos/notarize.js
@@ -1,24 +1,26 @@
-require('dotenv').config();
-const { notarize } = require('electron-notarize');
+require("dotenv").config();
+const { notarize } = require("electron-notarize");
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== 'darwin') {
+  if (electronPlatformName !== "darwin") {
     return;
   }
 
-  if (!('APPLEID' in process.env && 'APPLEIDPASS' in process.env)) {
-    console.warn('Skipping notarizing step. APPLEID and APPLEIDPASS env variables must be set');
-    console.log(process.env)
+  if (!("APPLEID" in process.env && "APPLEIDPASS" in process.env)) {
+    console.warn(
+      "Skipping notarizing step. APPLEID and APPLEIDPASS env variables must be set"
+    );
+    console.log(process.env);
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
   await notarize({
-    appBundleId: 'com.greymass.anchordesktop.release',
+    appBundleId: "com.greymass.anchordesktop.release",
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEIDPASS,
+    appleIdPassword: process.env.APPLEIDPASS
   });
 };
