f6fbc3dcf6488758fa1220579f28273484e252d4
Aaron Cox <aaron@greymass.com>
2024-02-19 12:17:56 -0800

    Version bump

--- README.md
+++ README.md
@@ -33,12 +33,12 @@ When downloading from either location, check the URL bar of your browser to ensu
 
 ### Current Release
 
-The most recent (v1.3.12) release can downloaded using these links:
+The most recent (v1.3.13) release can downloaded using these links:
 
-- [Windows Installer](https://github.com/greymass/anchor/releases/download/v1.3.12/win-anchor-wallet-1.3.12.exe)
-- [macOS Package](https://github.com/greymass/anchor/releases/download/v1.3.12/mac-anchor-wallet-1.3.12-x64.dmg)
-- [Linux (deb)](https://github.com/greymass/anchor/releases/download/v1.3.12/linux-anchor-wallet-1.3.12-amd64.deb)
-- [Linux (AppImage)](https://github.com/greymass/anchor/releases/download/v1.3.12/linux-anchor-wallet-1.3.12-x86_64.AppImage)
+- [Windows Installer](https://github.com/greymass/anchor/releases/download/v1.3.13/win-anchor-wallet-1.3.13.exe)
+- [macOS Package](https://github.com/greymass/anchor/releases/download/v1.3.13/mac-anchor-wallet-1.3.13-x64.dmg)
+- [Linux (deb)](https://github.com/greymass/anchor/releases/download/v1.3.13/linux-anchor-wallet-1.3.13-amd64.deb)
+- [Linux (AppImage)](https://github.com/greymass/anchor/releases/download/v1.3.13/linux-anchor-wallet-1.3.13-x86_64.AppImage)
 
 The latest release will always be available on the releases page of this repository:
 
--- shasum.sh
+++ shasum.sh
@@ -2,25 +2,25 @@
 
 cd release
 
-echo "shasum -b -a 512 linux-anchor-wallet-1.3.12-amd64.deb"
-shasum -b -a 512 linux-anchor-wallet-1.3.12-amd64.deb
-# echo "shasum -b -a 512 linux-anchor-wallet-1.3.12-amd64.snap"
-# shasum -b -a 512 linux-anchor-wallet-1.3.12-amd64.snap
-echo "shasum -b -a 512 linux-anchor-wallet-1.3.12-arm64.deb"
-shasum -b -a 512 linux-anchor-wallet-1.3.12-arm64.deb
-echo "shasum -b -a 512 linux-anchor-wallet-1.3.12-x86_64.rpm"
-shasum -b -a 512 linux-anchor-wallet-1.3.12-x86_64.rpm
-echo "shasum -b -a 512 linux-anchor-wallet-1.3.12-armv7l.deb"
-shasum -b -a 512 linux-anchor-wallet-1.3.12-armv7l.deb
-# echo "shasum -b -a 512 linux-anchor-wallet-1.3.12-i386.deb"
-# shasum -b -a 512 linux-anchor-wallet-1.3.12-i386.deb
-echo "shasum -b -a 512 linux-anchor-wallet-1.3.12-x86_64.AppImage"
-shasum -b -a 512 linux-anchor-wallet-1.3.12-x86_64.AppImage
-echo "shasum -b -a 512 mac-anchor-wallet-1.3.12-x64.dmg"
-shasum -b -a 512 mac-anchor-wallet-1.3.12-x64.dmg
-echo "shasum -b -a 512 mac-anchor-wallet-1.3.12-x64.zip"
-shasum -b -a 512 mac-anchor-wallet-1.3.12-x64.zip
-echo "shasum -b -a 512 win-anchor-wallet-1.3.12.exe"
-shasum -b -a 512 win-anchor-wallet-1.3.12.exe
+echo "shasum -b -a 512 linux-anchor-wallet-1.3.13-amd64.deb"
+shasum -b -a 512 linux-anchor-wallet-1.3.13-amd64.deb
+# echo "shasum -b -a 512 linux-anchor-wallet-1.3.13-amd64.snap"
+# shasum -b -a 512 linux-anchor-wallet-1.3.13-amd64.snap
+echo "shasum -b -a 512 linux-anchor-wallet-1.3.13-arm64.deb"
+shasum -b -a 512 linux-anchor-wallet-1.3.13-arm64.deb
+echo "shasum -b -a 512 linux-anchor-wallet-1.3.13-x86_64.rpm"
+shasum -b -a 512 linux-anchor-wallet-1.3.13-x86_64.rpm
+echo "shasum -b -a 512 linux-anchor-wallet-1.3.13-armv7l.deb"
+shasum -b -a 512 linux-anchor-wallet-1.3.13-armv7l.deb
+# echo "shasum -b -a 512 linux-anchor-wallet-1.3.13-i386.deb"
+# shasum -b -a 512 linux-anchor-wallet-1.3.13-i386.deb
+echo "shasum -b -a 512 linux-anchor-wallet-1.3.13-x86_64.AppImage"
+shasum -b -a 512 linux-anchor-wallet-1.3.13-x86_64.AppImage
+echo "shasum -b -a 512 mac-anchor-wallet-1.3.13-x64.dmg"
+shasum -b -a 512 mac-anchor-wallet-1.3.13-x64.dmg
+echo "shasum -b -a 512 mac-anchor-wallet-1.3.13-x64.zip"
+shasum -b -a 512 mac-anchor-wallet-1.3.13-x64.zip
+echo "shasum -b -a 512 win-anchor-wallet-1.3.13.exe"
+shasum -b -a 512 win-anchor-wallet-1.3.13.exe
 
 cd ..
