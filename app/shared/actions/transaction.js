e87e29ac6db27cbccaaab323bc51286fec087a95
aaroncox <aaron.cox@greymass.com>
2026-09-17 14:10:01 -0700

    Deduplicate merged transaction signatures

--- app/shared/actions/transaction.js
+++ app/shared/actions/transaction.js
@@ -191,8 +191,10 @@ export function signTransaction(tx, contracts = []) {
         if (tx.transaction.signatures && tx.transaction.signatures.length) {
           // Merge signatures
           modified.transaction.signatures = [
-            ...signed.transaction.signatures,
-            ...tx.transaction.signatures
+            ...new Set([
+              ...signed.transaction.signatures,
+              ...tx.transaction.signatures
+            ])
           ];
         }
         if (modified.broadcast) {
